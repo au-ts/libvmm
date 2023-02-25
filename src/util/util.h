26780bc4203fca63810ba27c3e83f8e728b83112
Jingyao Zhou <jingyao.zhou@unsw.edu.au>
2023-02-26 02:09:41 +1100

    util: add MIN and MAX

--- src/util/util.h
+++ src/util/util.h
@@ -11,6 +11,16 @@
 #include <sel4cp.h>
 #include "printf.h"
 
+#define MIN(a,b) \
+    ({ typeof (a) _a = (a); \
+       typeof (b) _b = (b); \
+       _a < _b ? _a : _b; })
+
+#define MAX(a,b) \
+    ({ typeof (a) _a = (a); \
+       typeof (b) _b = (b); \
+       _a > _b ? _a : _b; })
+
 // @ivanv: these are here for convience, should not be here though
 #define VM_ID 1
 #define VCPU_ID 0
