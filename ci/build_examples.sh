739af6d56d312ae238c688255c4dad3aee6a1756
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2023-02-26 22:26:57 +1100

    Add OdroidC2 to CI building, clean up CI script

--- ci/build_examples.sh
+++ ci/build_examples.sh
@@ -4,7 +4,9 @@ set -e
 
 SDK_PATH=$1
 
-examples=("qemu_arm_virt_hyp/systems/simple.system" "qemu_arm_virt_hyp/systems/two_vms.system")
+# @ivanv This is weird and needs to be done better. We should probably
+# just give a path to the makefile of the system file along with images
+examples=("qemu_arm_virt_hyp/systems/simple.system" "qemu_arm_virt_hyp/systems/two_vms.system" "odroidc2/systems/simple.system")
 
 for example in ${examples[@]}; do
     echo "Building example: $example"
@@ -12,5 +14,11 @@ for example in ${examples[@]}; do
     SYSTEM=$(echo "$example" | cut -d "/" -f3)
     SYSTEM_NAME=$(basename $SYSTEM .system)
     BUILD_DIR="build_${SYSTEM_NAME}_${BOARD}"
-    make -B BUILD_DIR=$BUILD_DIR SEL4CP_SDK=$SDK_PATH SEL4CP_CONFIG=debug SEL4CP_BOARD=$BOARD SYSTEM=$SYSTEM
+
+    make -j$(nproc) -B \
+        BUILD_DIR=$BUILD_DIR \
+        SEL4CP_SDK=$SDK_PATH \
+        SEL4CP_CONFIG=debug \
+        SEL4CP_BOARD=$BOARD \
+        SYSTEM=$SYSTEM
 done
