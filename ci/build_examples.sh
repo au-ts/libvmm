d48b7d0462ad92018e5d22230a6f1400fced65c5
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2023-03-29 15:52:24 +1100

    Add basic runtime check to CI

--- ci/build_examples.sh
+++ ci/build_examples.sh
@@ -3,10 +3,19 @@
 set -e
 
 SDK_PATH=$1
+QEMU=qemu-system-aarch64
+
+if ! command -v qemu-system-aarch64 &> /dev/null
+then
+    echo "qemu-system-aarch64 could not be found, it must be available to run the script."
+    exit
+fi
+
+[ ! -d $SDK_PATH ] && echo "The path to SDK provided does not exist: $SDK_PATH" && exit
 
 # @ivanv This is weird and needs to be done better. We should probably
 # just give a path to the makefile of the system file along with images
-examples=("qemu_arm_virt_hyp/systems/simple.system" "qemu_arm_virt_hyp/systems/two_vms.system" "odroidc2_hyp/systems/simple.system")
+examples=("qemu_arm_virt_hyp/systems/simple.system")
 
 for example in ${examples[@]}; do
     echo "Building example: $example"
@@ -21,4 +30,18 @@ for example in ${examples[@]}; do
         SEL4CP_CONFIG=debug \
         SEL4CP_BOARD=$BOARD \
         SYSTEM=$SYSTEM
+done
+
+for example in ${examples[@]}; do
+    BOARD=$(echo "$example" | cut -d "/" -f1)
+    BUILD_DIR="build_${SYSTEM_NAME}_${BOARD}"
+    SYSTEM=$(echo "$example" | cut -d "/" -f3)
+    SYSTEM_NAME=$(basename $SYSTEM .system)
+    BUILD_DIR="build_${SYSTEM_NAME}_${BOARD}"
+    # For now we will only do runtime tests via simulation
+    if [[ $BOARD == "qemu_arm_virt_hyp" ]]; then
+        echo "Running example: $example"
+        IMAGE="$BUILD_DIR/loader.img"
+        ./ci/buildroot_login.exp $IMAGE
+    fi
 done
