98227aa57c7812a1afb9746f3b6235fcc6b77efd
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2023-04-10 21:16:56 +1000

    Cleanup CI script and also build/test release config

--- ci/examples.sh
+++ ci/examples.sh
@@ -3,45 +3,41 @@
 set -e
 
 SDK_PATH=$1
-QEMU=qemu-system-aarch64
-
-if ! command -v qemu-system-aarch64 &> /dev/null
-then
-    echo "qemu-system-aarch64 could not be found, it must be available to run the script."
-    exit
-fi
 
 [ ! -d $SDK_PATH ] && echo "The path to SDK provided does not exist: $SDK_PATH" && exit
 
-# @ivanv This is weird and needs to be done better. We should probably
-# just give a path to the makefile of the system file along with images
-examples=("qemu_arm_virt_hyp/systems/simple.system")
+EXAMPLES=("qemu_arm_virt_hyp/systems/simple.system")
+CONFIGS=("debug release")
 
-for example in ${examples[@]}; do
-    echo "Building example: $example"
-    BOARD=$(echo "$example" | cut -d "/" -f1)
-    SYSTEM=$(echo "$example" | cut -d "/" -f3)
-    SYSTEM_NAME=$(basename $SYSTEM .system)
-    BUILD_DIR="build_${SYSTEM_NAME}_${BOARD}"
+for EXAMPLE in ${EXAMPLES[@]}; do
+    for CONFIG in ${CONFIGS[@]}; do
+        echo "Building example: $EXAMPLE in config: $CONFIG"
+        BOARD=$(echo "$EXAMPLE" | cut -d "/" -f1)
+        SYSTEM=$(echo "$EXAMPLE" | cut -d "/" -f3)
+        SYSTEM_NAME=$(basename $SYSTEM .system)
+        BUILD_DIR="build_${SYSTEM_NAME}_${BOARD}_${CONFIG}"
 
-    make -j$(nproc) -B \
-        BUILD_DIR=$BUILD_DIR \
-        SEL4CP_SDK=$SDK_PATH \
-        SEL4CP_CONFIG=debug \
-        SEL4CP_BOARD=$BOARD \
-        SYSTEM=$SYSTEM
+        make -j$(nproc) -B \
+            BUILD_DIR=$BUILD_DIR \
+            SEL4CP_SDK=$SDK_PATH \
+            CONFIG=$CONFIG \
+            BOARD=$BOARD \
+            SYSTEM=$SYSTEM
+    done
 done
 
-for example in ${examples[@]}; do
-    BOARD=$(echo "$example" | cut -d "/" -f1)
-    BUILD_DIR="build_${SYSTEM_NAME}_${BOARD}"
-    SYSTEM=$(echo "$example" | cut -d "/" -f3)
-    SYSTEM_NAME=$(basename $SYSTEM .system)
-    BUILD_DIR="build_${SYSTEM_NAME}_${BOARD}"
-    # For now we will only do runtime tests via simulation
-    if [[ $BOARD == "qemu_arm_virt_hyp" ]]; then
-        echo "Running example: $example"
-        IMAGE="$BUILD_DIR/loader.img"
-        ./ci/buildroot_login.exp $IMAGE
-    fi
+for EXAMPLE in ${EXAMPLES[@]}; do
+    for CONFIG in ${CONFIGS[@]}; do
+        BOARD=$(echo "$EXAMPLE" | cut -d "/" -f1)
+        BUILD_DIR="build_${SYSTEM_NAME}_${BOARD}"
+        SYSTEM=$(echo "$EXAMPLE" | cut -d "/" -f3)
+        SYSTEM_NAME=$(basename $SYSTEM .system)
+        BUILD_DIR="build_${SYSTEM_NAME}_${BOARD}_${CONFIG}"
+        # For now we will only do runtime tests via simulation
+        if [[ $BOARD == "qemu_arm_virt_hyp" ]]; then
+            echo "Running EXAMPLE: $EXAMPLE"
+            IMAGE="$BUILD_DIR/loader.img"
+            ./ci/buildroot_login.exp $IMAGE
+        fi
+    done
 done
