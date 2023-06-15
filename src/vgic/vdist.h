au-ts/libvmm

commit 25ff3f9a1b6798e982a1c4b21310af740d9498ca

Cleaning

src/vgic/vdist.h
@@ -242,7 +242,7 @@ static void vgic_dist_clr_pending_irq(struct gic_dist_map *dist, uint32_t vcpu_i
 
 static bool vgic_dist_reg_read(uint64_t vcpu_id, vgic_t *vgic, uint64_t offset, uint64_t fsr, seL4_UserContext *regs)
 {
-    int err = 0;
+    bool success = false;
     struct gic_dist_map *gic_dist = vgic_get_dist(vgic->registers);
     uint32_t reg = 0;
     int reg_offset = 0;
@@ -395,20 +395,17 @@ static bool vgic_dist_reg_read(uint64_t vcpu_id, vgic_t *vgic, uint64_t offset, 
     default:
         LOG_VMM_ERR("Unknown register offset 0x%x", offset);
         // err = ignore_fault(fault);
-        err = fault_advance_vcpu(regs);
+        success = !fault_advance_vcpu(regs);
         goto fault_return;
     }
     uint32_t mask = fault_get_data_mask(GIC_DIST_PADDR + offset, fsr);
     // fault_set_data(fault, reg & mask);
     // @ivanv: interesting, when we don't call fault_Set_data in the CAmkES VMM, everything works fine?...
-    err = fault_advance(regs, GIC_DIST_PADDR + offset, fsr, reg & mask);
+    success = !fault_advance(regs, GIC_DIST_PADDR + offset, fsr, reg & mask);
 
 fault_return:
     // @ivanv: revisit, also make fault_return consistint. it's called something else in vgic_dist_reg_write
-    if (err) {
-        return false;
-    }
-    return true;
+    return success;
 }
 
 static inline void emulate_reg_write_access(seL4_UserContext *regs, uint64_t addr, uint64_t fsr, uint32_t *reg)

src/vgic/vgic.c
@@ -19,7 +19,7 @@ extern vgic_t vgic;
 bool handle_vgic_maintenance()
 {
     // @ivanv: reivist, also inconsistency between int and bool
-    int err;
+    bool success = true;
     int idx = sel4cp_mr_get(seL4_VGICMaintenance_IDX);
     /* Currently not handling spurious IRQs */
     // @ivanv: wtf, this comment seems irrelevant to the code.
@@ -51,12 +51,12 @@ bool handle_vgic_maintenance()
 #endif
 
     if (virq) {
-        err = vgic_vcpu_load_list_reg(&vgic, VCPU_ID, idx, group, virq);
+        success = vgic_vcpu_load_list_reg(&vgic, VCPU_ID, idx, group, virq);
     }
-    if (!err) {
+    if (success) {
         reply_to_fault();
     } else {
-        printf("VGIC|ERROR: maintenance handler failed (error %d)", err);
+        printf("VGIC|ERROR: maintenance handler failed\n");
         assert(0);
         return false;
     }

src/arch/aarch64/linux.h
@@ -16,5 +16,5 @@ struct linux_image_header {
     uint64_t res3;        // reserved
     uint64_t res4;        // reserved
     uint32_t magic;       // Magic number, little endian, "ARM\x64"
-    uint32_t res5;        // reserved (used for PE COFF offset
+    uint32_t res5;        // reserved (used for PE COFF offset)
 };

src/fault.c
@@ -25,47 +25,43 @@ bool fault_advance_vcpu(seL4_UserContext *regs) {
     // For now we just ignore it and continue
     // Assume 64-bit instruction
     regs->pc += 4;
-    // @ivanv: should this be true or false?
     int err = seL4_TCB_WriteRegisters(BASE_VM_TCB_CAP + VM_ID, true, 0, SEL4_USER_CONTEXT_SIZE, regs);
     assert(err == seL4_NoError);
     /* Reply to thread */
     reply_to_fault();
 
-    return (err != 0);
+    return (err == seL4_NoError);
 }
 
 enum fault_width {
-    WIDTH_DOUBLEWORD,
-    WIDTH_WORD,
-    WIDTH_HALFWORD,
-    WIDTH_BYTE
+    WIDTH_DOUBLEWORD = 0,
+    WIDTH_WORD = 1,
+    WIDTH_HALFWORD = 2,
+    WIDTH_BYTE = 3,
 };
 
-enum fault_width fault_get_width(uint64_t fsr)
+static enum fault_width fault_get_width(uint64_t fsr)
 {
-    // @ivanv: what is HSR syndrome??
     if (HSR_IS_SYNDROME_VALID(fsr)) {
         switch (HSR_SYNDROME_WIDTH(fsr)) {
-        case 0: return WIDTH_BYTE;
-        case 1: return WIDTH_HALFWORD;
-        case 2: return WIDTH_WORD;
-        case 3: return WIDTH_DOUBLEWORD;
-        default:
-            // @ivanv: reviist
-            // print_fault(f);
-            assert(0);
-            return 0;
+            case 0: return WIDTH_BYTE;
+            case 1: return WIDTH_HALFWORD;
+            case 2: return WIDTH_WORD;
+            case 3: return WIDTH_DOUBLEWORD;
+            default:
+                // @ivanv: reviist
+                // print_fault(f);
+                assert(0);
+                return 0;
         }
     } else {
+        LOG_VMM_ERR("Received invalid FSR: 0x%lx\n", fsr);
         // @ivanv: reviist
         // int rt;
         // rt = decode_instruction(f);
         // assert(rt >= 0);
+        return -1;
     }
-
-    assert(0);
-    // @ivanv: come back to
-    return 0;
 }
 
 uint64_t fault_get_data_mask(uint64_t addr, uint64_t fsr)
@@ -88,8 +84,8 @@ uint64_t fault_get_data_mask(uint64_t addr, uint64_t fsr)
             mask = ~mask;
             break;
         default:
-            /* Should never get here... Keep the compiler happy */
-            LOG_VMM_ERR("unknown width: 0x%lx, from FSR: 0x%lx\n", fault_get_width(fsr), fsr);
+            LOG_VMM_ERR("unknown width: 0x%lx, from FSR: 0x%lx, addr: 0x%lx\n",
+                fault_get_width(fsr), fsr, addr);
             assert(0);
             return 0;
     }
@@ -158,6 +154,7 @@ static int get_rt(uint64_t fsr)
         rt = HSR_SYNDROME_RT(fsr);
     } else {
         printf("decode_insturction for arm64 not implemented\n");
+        assert(0);
         // @ivanv: implement decode instruction for aarch64
         // rt = decode_instruction(f);
     }

src/psci.c
@@ -70,8 +70,8 @@ bool handle_psci(uint64_t vcpu_id, seL4_UserContext *regs, uint64_t fn_number, u
             return false;
     }
 
-    int err = fault_advance_vcpu(regs);
-    assert(!err);
+    bool success = fault_advance_vcpu(regs);
+    assert(success);
 
-    return err == 0;
+    return success;
 }

src/smc.h
@@ -16,4 +16,6 @@ bool handle_smc(uint32_t hsr);
 
 // Helper functions
 void smc_set_return_value(seL4_UserContext *u, uint64_t val);
+
+/* Gets the value of x1-x6 */
 uint64_t smc_get_arg(seL4_UserContext *u, uint64_t arg);

src/util/util.h
@@ -16,7 +16,9 @@
 #define VCPU_ID 0
 #define NUM_VCPUS 1
 // Note that this is AArch64 specific
-#define SEL4_USER_CONTEXT_SIZE 0x24
+#if defined(CONFIG_ARCH_AARCH64)
+    #define SEL4_USER_CONTEXT_SIZE 0x24
+#endif
 
 #define PAGE_SIZE_4K 4096
 

src/vmm.c
@@ -42,11 +42,6 @@ static bool handle_unknown_syscall(sel4cp_msginfo msginfo)
     uint64_t syscall = sel4cp_mr_get(seL4_UnknownSyscall_Syscall);
     uint64_t fault_ip = sel4cp_mr_get(seL4_UnknownSyscall_FaultIP);
 
-    seL4_UserContext regs;
-    int err = seL4_TCB_ReadRegisters(BASE_VM_TCB_CAP + VM_ID, false, 0, SEL4_USER_CONTEXT_SIZE, &regs);
-    assert(!err);
-    regs.pc += 4;
-
     LOG_VMM("Received syscall 0x%lx\n", syscall);
     switch (syscall) {
         case SYSCALL_PA_TO_IPA:
@@ -61,10 +56,12 @@ static bool handle_unknown_syscall(sel4cp_msginfo msginfo)
             LOG_VMM_ERR("Unknown syscall: syscall number: 0x%lx, PC: 0x%lx\n", syscall, fault_ip);
             return false;
     }
-    err = seL4_TCB_WriteRegisters(BASE_VM_TCB_CAP + VM_ID, false, 0, SEL4_USER_CONTEXT_SIZE, &regs);
+
+    seL4_UserContext regs;
+    seL4_Error err = seL4_TCB_ReadRegisters(BASE_VM_TCB_CAP + VM_ID, false, 0, SEL4_USER_CONTEXT_SIZE, &regs);
     assert(!err);
 
-    return true;
+    return fault_advance_vcpu(&regs);
 }
 
 static bool handle_vppi_event()
@@ -89,8 +86,7 @@ static bool handle_vppi_event()
 
 static bool handle_vcpu_fault(sel4cp_msginfo msginfo)
 {
-    // @ivanv: should this be uint32_t?
-    uint64_t hsr = sel4cp_mr_get(seL4_VCPUFault_HSR);
+    uint32_t hsr = sel4cp_mr_get(seL4_VCPUFault_HSR);
     uint64_t hsr_ec_class = HSR_EXCEPTION_CLASS(hsr);
     switch (hsr_ec_class) {
         case HSR_SMC_64_EXCEPTION:
@@ -98,13 +94,11 @@ static bool handle_vcpu_fault(sel4cp_msginfo msginfo)
         case HSR_WFx_EXCEPTION:
             // If we get a WFI exception, we just do nothing in the VMM.
             reply_to_fault();
-            break;
+            return true;
         default:
             LOG_VMM_ERR("unknown SMC exception, EC class: 0x%lx, HSR: 0x%lx\n", hsr_ec_class, hsr);
             return false;
     }
-
-    return true;
 }
 
 static int handle_user_exception(sel4cp_msginfo msginfo)
@@ -239,7 +233,8 @@ void guest_start(void) {
     // @ivanv: Note that remove this line causes the VMM to fault if we
     // actually get the interrupt. This should be avoided by making the VGIC driver more stable.
     err = vgic_register_irq(VCPU_ID, SERIAL_IRQ, &serial_ack, NULL);
-    // @ivanv: comment why we ack it
+    // Just in case there is already an interrupt available to handle, we ack it here.
+    // @ivanv: not sure if this is necessary? is this the right place to do this
     sel4cp_irq_ack(SERIAL_IRQ_CH);
     seL4_UserContext regs = {0};
     regs.x0 = GUEST_DTB_VADDR;
@@ -332,10 +327,6 @@ bool guest_restart(void) {
     // Now we need to re-initialise all the VMM state
     guest_start();
     LOG_VMM("Restarted guest\n");
-    // Finally, we can to restart the VM.
-    // uint64_t guest_init_pc = guest_ram_vaddr + 0x80000; // @ivanv: get rid of
-    // LOG_VMM("restarting guest at 0x%lx, DTB at 0x%lx\n", guest_init_pc, GUEST_DTB_VADDR);
-    // sel4cp_vm_restart(VM_ID, guest_init_pc);
     return true;
 }
 

commit b4ff398c0f21c5231efd37688d36bdd3dda4eb49

Fixing bug introduced by 25ff3f9a1b6798e982a1c4b21310af740d9498ca
I didn't test my refactor enough and a simple `bool success = false`
instead of `bool success = true` messed things up.

Overall this VGIC driver code needs to be cleaned up, the whole design
of the VGIC driver ought to be redone and audited.

src/vgic/vdist.h
@@ -398,12 +398,14 @@ static bool vgic_dist_reg_read(uint64_t vcpu_id, vgic_t *vgic, uint64_t offset, 
         LOG_VMM_ERR("Unknown register offset 0x%x", offset);
         // err = ignore_fault(fault);
         success = fault_advance_vcpu(regs);
+        assert(success);
         goto fault_return;
     }
     uint32_t mask = fault_get_data_mask(GIC_DIST_PADDR + offset, fsr);
     // fault_set_data(fault, reg & mask);
     // @ivanv: interesting, when we don't call fault_Set_data in the CAmkES VMM, everything works fine?...
     success = fault_advance(regs, GIC_DIST_PADDR + offset, fsr, reg & mask);
+    assert(success);
 
 fault_return:
     if (!success) {
@@ -420,7 +422,7 @@ static inline void emulate_reg_write_access(seL4_UserContext *regs, uint64_t add
 
 static bool vgic_dist_reg_write(uint64_t vcpu_id, vgic_t *vgic, uint64_t offset, uint64_t fsr, seL4_UserContext *regs)
 {
-    bool success = false;
+    bool success = true;
     struct gic_dist_map *gic_dist = vgic_get_dist(vgic->registers);
     uint64_t addr = GIC_DIST_PADDR + offset;
     uint32_t mask = fault_get_data_mask(addr, fsr);
@@ -580,6 +582,7 @@ static bool vgic_dist_reg_write(uint64_t vcpu_id, vgic_t *vgic, uint64_t offset,
         // we're also blindly injectnig the given IRQ to that vCPU.
         // @ivanv: come back to this, do we have two writes to the TCB registers?
         success = vgic_inject_irq(vcpu_id, virq);
+        assert(success);
         break;
     case RANGE32(0xF04, 0xF0C):
         /* Reserved */
@@ -605,9 +608,17 @@ static bool vgic_dist_reg_write(uint64_t vcpu_id, vgic_t *vgic, uint64_t offset,
 #endif
     default:
         LOG_VMM_ERR("Unknown register offset 0x%x", offset);
+        assert(0);
     }
 ignore_fault:
+    assert(success);
+    if (!success) {
+        return false;
+    }
+
     success = fault_advance_vcpu(regs);
+    assert(success);
+
     return success;
 }
 

src/vgic/vgic.c
@@ -106,11 +106,16 @@ bool handle_vgic_dist_fault(uint64_t vcpu_id, uint64_t fault_addr, uint64_t fsr,
     assert(fault_addr - GIC_DIST_PADDR < GIC_DIST_SIZE);
 
     uint64_t offset = fault_addr - GIC_DIST_PADDR;
+    bool success = false;
     if (fault_is_read(fsr)) {
         // printf("VGIC|INFO: Read dist\n");
-        return vgic_dist_reg_read(vcpu_id, &vgic, offset, fsr, regs);
+        success = vgic_dist_reg_read(vcpu_id, &vgic, offset, fsr, regs);
+        assert(success);
     } else {
         // printf("VGIC|INFO: Write dist\n");
-        return vgic_dist_reg_write(vcpu_id, &vgic, offset, fsr, regs);
+        success = vgic_dist_reg_write(vcpu_id, &vgic, offset, fsr, regs);
+        assert(success);
     }
+
+    return success;
 }