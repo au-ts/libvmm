e5080cd9545b17e3fde6252e92b24d13c316b012
Ivan Velickovic <i.velickovic@unsw.edu.au>
2023-08-03 00:18:30 +1000

    Getting CI back into a working state
    Note that I did have to no longer test rebooting a guest since I
    have to get that functionality reworking with the restructure

--- ci/examples.sh
+++ ci/examples.sh
@@ -4,40 +4,27 @@ set -e
 
 SDK_PATH=$1
 
-[ ! -d $SDK_PATH ] && echo "The path to SDK provided does not exist: $SDK_PATH" && exit
+[[ -z $SDK_PATH ]] && echo "usage: examples.sh [PATH TO SDK]" && exit 1
+[[ ! -d $SDK_PATH ]] && echo "The path to SDK provided does not exist: '$SDK_PATH'" && exit 1
 
-EXAMPLES=("qemu_arm_virt_hyp/systems/simple.system")
-CONFIGS=("debug release")
+build_simple() {
+    BOARD=$1
+    CONFIG=$2
+    BUILD_DIR="build_simple_${CONFIG}_${BOARD}"
+    make -C examples/simple -B BUILD_DIR=${BUILD_DIR} CONFIG=${CONFIG} BOARD=${BOARD} SEL4CP_SDK=${SDK_PATH}
+}
 
-for EXAMPLE in ${EXAMPLES[@]}; do
-    for CONFIG in ${CONFIGS[@]}; do
-        echo "Building example: $EXAMPLE in config: $CONFIG"
-        BOARD=$(echo "$EXAMPLE" | cut -d "/" -f1)
-        SYSTEM=$(echo "$EXAMPLE" | cut -d "/" -f3)
-        SYSTEM_NAME=$(basename $SYSTEM .system)
-        BUILD_DIR="build_${SYSTEM_NAME}_${BOARD}_${CONFIG}"
+simulate_simple() {
+    BOARD=$1
+    CONFIG=$2
+    BUILD_DIR="build_simple_${CONFIG}_${BOARD}"
+    ./ci/buildroot_login.exp examples/simple/${BUILD_DIR}/loader.img
+}
 
-        make -j$(nproc) -B \
-            BUILD_DIR=$BUILD_DIR \
-            SEL4CP_SDK=$SDK_PATH \
-            CONFIG=$CONFIG \
-            BOARD=$BOARD \
-            SYSTEM=$SYSTEM
-    done
-done
+build_simple "qemu_arm_virt_hyp" "debug"
+simulate_simple "qemu_arm_virt_hyp" "debug"
+build_simple "qemu_arm_virt_hyp" "release"
+simulate_simple "qemu_arm_virt_hyp" "release"
 
-for EXAMPLE in ${EXAMPLES[@]}; do
-    for CONFIG in ${CONFIGS[@]}; do
-        BOARD=$(echo "$EXAMPLE" | cut -d "/" -f1)
-        BUILD_DIR="build_${SYSTEM_NAME}_${BOARD}"
-        SYSTEM=$(echo "$EXAMPLE" | cut -d "/" -f3)
-        SYSTEM_NAME=$(basename $SYSTEM .system)
-        BUILD_DIR="build_${SYSTEM_NAME}_${BOARD}_${CONFIG}"
-        # For now we will only do runtime tests via simulation
-        if [[ $BOARD == "qemu_arm_virt_hyp" ]]; then
-            echo "Running example: $BOARD $CONFIG $SYSTEM"
-            IMAGE="$BUILD_DIR/loader.img"
-            ./ci/buildroot_login.exp $IMAGE
-        fi
-    done
-done
+build_simple "odroidc4_hyp" "debug"
+build_simple "odroidc4_hyp" "release"
