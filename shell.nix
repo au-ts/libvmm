994596ed1c5a0d371f800919ab3a4ff718baa7c6
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2023-08-03 14:55:06 +1000

    Nix shell formatting/comment

--- shell.nix
+++ shell.nix
@@ -5,10 +5,12 @@ in
     buildInputs = with pkgs.buildPackages; [
         qemu
         gnumake
-		dtc
-		expect
+        dtc
         llvmPackages_16.clang
         llvmPackages_16.lld
+        # expect is only needed for CI testing but we include it for
+        # completeness
+        expect
     ];
 }
 
