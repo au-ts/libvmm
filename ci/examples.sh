173e6f5bd1274b74a518cc7282317f581547d6a9
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2023-08-03 14:55:52 +1000

    Further fixes in CI and testing script

--- ci/examples.sh
+++ ci/examples.sh
@@ -7,24 +7,30 @@ SDK_PATH=$1
 [[ -z $SDK_PATH ]] && echo "usage: examples.sh [PATH TO SDK]" && exit 1
 [[ ! -d $SDK_PATH ]] && echo "The path to SDK provided does not exist: '$SDK_PATH'" && exit 1
 
-build_simple() {
+build_simple_make() {
     BOARD=$1
     CONFIG=$2
-    BUILD_DIR="build_simple_${CONFIG}_${BOARD}"
-    make -C examples/simple -B BUILD_DIR=${BUILD_DIR} CONFIG=${CONFIG} BOARD=${BOARD} SEL4CP_SDK=${SDK_PATH}
+    BUILD_DIR="${PWD}/build/simple/make/${BOARD}/${CONFIG}"
+    mkdir -p ${BUILD_DIR}
+    make -C examples/simple -B \
+        BUILD_DIR=${BUILD_DIR} \
+        CONFIG=${CONFIG} \
+        BOARD=${BOARD} \
+        SEL4CP_SDK=${SDK_PATH} \
+        -j$(nproc)
 }
 
-simulate_simple() {
+simulate_simple_make() {
     BOARD=$1
     CONFIG=$2
-    BUILD_DIR="build_simple_${CONFIG}_${BOARD}"
-    ./ci/buildroot_login.exp examples/simple/${BUILD_DIR}/loader.img
+    BUILD_DIR="${PWD}/build/simple/make/${BOARD}/${CONFIG}"
+    ./ci/buildroot_login.exp ${BUILD_DIR}/loader.img
 }
 
-build_simple "qemu_arm_virt_hyp" "debug"
-simulate_simple "qemu_arm_virt_hyp" "debug"
-build_simple "qemu_arm_virt_hyp" "release"
-simulate_simple "qemu_arm_virt_hyp" "release"
+build_simple_make "qemu_arm_virt_hyp" "debug"
+simulate_simple_make "qemu_arm_virt_hyp" "debug"
+build_simple_make "qemu_arm_virt_hyp" "release"
+simulate_simple_make "qemu_arm_virt_hyp" "release"
 
-build_simple "odroidc4_hyp" "debug"
-build_simple "odroidc4_hyp" "release"
+build_simple_make "odroidc4_hyp" "debug"
+build_simple_make "odroidc4_hyp" "release"
