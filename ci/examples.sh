9e14faf7f89ca8d029e83e945158c054a15bcb02
Ivan Velickovic <i.velickovic@unsw.edu.au>
2023-08-09 23:58:18 +1000

    ci: adding testing for examples/simple built via Zig build system

--- ci/examples.sh
+++ ci/examples.sh
@@ -16,8 +16,35 @@ build_simple_make() {
         BUILD_DIR=${BUILD_DIR} \
         CONFIG=${CONFIG} \
         BOARD=${BOARD} \
-        SEL4CP_SDK=${SDK_PATH} \
-        -j$(nproc)
+        SEL4CP_SDK=${SDK_PATH}
+}
+
+build_simple_zig() {
+    BOARD=$1
+    CONFIG=$2
+    # @ivanv Ideally we'd have Makefile and Zig output in same
+    # directory structure
+    EXAMPLE_DIR="${PWD}/examples/simple"
+    pushd ${EXAMPLE_DIR}
+    # @ivanv: for some reason Zig compiling the C VMM only
+    # works with ReleaseFast optimisations, debug does not work
+    # and causes a fault to happen during Linux booting. More
+    # investigation required.... sigh
+    zig build \
+        -Dtarget="aarch64-freestanding" \
+        -Dsdk=${SDK_PATH} \
+        -Dboard=${BOARD} \
+        -Dconfig=${CONFIG} \
+        -Doptimize=ReleaseFast
+    popd
+}
+
+simulate_simple_zig() {
+    BOARD=$1
+    CONFIG=$2
+    EXAMPLE_DIR="${PWD}/examples/simple"
+    BUILD_DIR="${EXAMPLE_DIR}/zig-out/bin"
+    ./ci/buildroot_login.exp ${BUILD_DIR}/loader.img
 }
 
 simulate_simple_make() {
@@ -31,6 +58,11 @@ build_simple_make "qemu_arm_virt_hyp" "debug"
 simulate_simple_make "qemu_arm_virt_hyp" "debug"
 build_simple_make "qemu_arm_virt_hyp" "release"
 simulate_simple_make "qemu_arm_virt_hyp" "release"
+
+build_simple_zig "qemu_arm_virt_hyp" "debug"
+simulate_simple_zig "qemu_arm_virt_hyp" "debug"
+build_simple_zig "qemu_arm_virt_hyp" "release"
+simulate_simple_zig "qemu_arm_virt_hyp" "release"
 
 build_simple_make "odroidc4_hyp" "debug"
 build_simple_make "odroidc4_hyp" "release"
