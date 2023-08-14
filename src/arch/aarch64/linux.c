2649f1f136d00e0e2d42a4c92992c3c799ec015e
Ivan Velickovic <i.velickovic@unsw.edu.au>
2023-08-14 12:23:19 +1000

    Expose linux_image_header struct definition
    The pointer passed to linux_setup_images needs to have the same
    alignment as this header, this signifies that the API probably
    needs to be improved as well (or at least document this quirk).
    Either way, this struct definition should be exposed outside the
    VMM library.

--- src/arch/aarch64/linux.c
+++ src/arch/aarch64/linux.c
@@ -7,28 +7,6 @@
 #include "../../util/util.h"
 #include "linux.h"
 
-/*
- * The structure of the kernel image header and the magic value comes from the
- * Linux kernel documentation that can be found in the Linux source code. The
- * path is Documentation/arm64/booting.rst or Documentation/arch/arm64/booting.rst
- * depending on the version of the source code.
- */
-
-#define LINUX_IMAGE_MAGIC 0x644d5241
-
-struct linux_image_header {
-    uint32_t code0;       // Executable code
-    uint32_t code1;       // Executable code
-    uint64_t text_offset; // Image load offset, little endian
-    uint64_t image_size;  // Effective Image size, little endian
-    uint64_t flags;       // kernel flags, little endian
-    uint64_t res2;        // reserved
-    uint64_t res3;        // reserved
-    uint64_t res4;        // reserved
-    uint32_t magic;       // Magic number, little endian, "ARM\x64"
-    uint32_t res5;        // reserved (used for PE COFF offset)
-};
-
 uintptr_t linux_setup_images(uintptr_t ram_start,
                              uintptr_t kernel,
                              size_t kernel_size,
