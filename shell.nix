20ece35117516a6251def4e6e0db70bd93bfcfeb
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2023-08-15 12:38:31 +1000

    Add Rust and llvm-ar to Nix shell

--- shell.nix
+++ shell.nix
@@ -1,14 +1,18 @@
 let
-    pkgs = import <nixpkgs> {};
+    rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+    pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+    rust = pkgs.rust-bin.fromRustupToolchainFile ./examples/rust/rust-toolchain.toml;
 in
   pkgs.mkShell {
     buildInputs = with pkgs.buildPackages; [
         zig_0_11
+        rust
         qemu
         gnumake
         dtc
         llvmPackages_16.clang
         llvmPackages_16.lld
+        llvmPackages_16.libllvm
         # expect is only needed for CI testing but we include it for
         # completeness
         expect
