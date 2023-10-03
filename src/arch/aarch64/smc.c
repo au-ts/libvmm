66e6fd1f14b11e5b198fc6af41ec1d16ddfe0359
Ivan Velickovic <i.velickovic@unsw.edu.au>
2023-10-03 17:31:24 +1100

    libvmm: add support for SMC forwarding
    Co-authored-by: Nataliya Korovkina <malus.brandywine@gmail.com>

--- src/arch/aarch64/smc.c
+++ src/arch/aarch64/smc.c
@@ -7,6 +7,7 @@
 
 #include "smc.h"
 #include "psci.h"
+#include "fault.h"
 #include "../../util/util.h"
 
 // Values in this file are taken from:
@@ -90,6 +91,69 @@ static void smc_set_arg(seL4_UserContext *u, size_t arg, size_t val)
     }
 }
 
+static void dump_smc_request(seL4_ARM_SMCContext *request) {
+    LOG_VMM("SMC forward dump request:\n");
+    LOG_VMM("   x0: 0x%lx\n", request->x0);
+    LOG_VMM("   x1: 0x%lx\n", request->x1);
+    LOG_VMM("   x2: 0x%lx\n", request->x2);
+    LOG_VMM("   x3: 0x%lx\n", request->x3);
+    LOG_VMM("   x4: 0x%lx\n", request->x4);
+    LOG_VMM("   x5: 0x%lx\n", request->x5);
+    LOG_VMM("   x6: 0x%lx\n", request->x6);
+    LOG_VMM("   x7: 0x%lx\n", request->x7);
+}
+
+static void dump_smc_response(seL4_ARM_SMCContext *response) {
+    LOG_VMM("SMC forward dump response:\n");
+    LOG_VMM("   x0: 0x%lx\n", response->x0);
+    LOG_VMM("   x1: 0x%lx\n", response->x1);
+    LOG_VMM("   x2: 0x%lx\n", response->x2);
+    LOG_VMM("   x3: 0x%lx\n", response->x3);
+    LOG_VMM("   x4: 0x%lx\n", response->x4);
+    LOG_VMM("   x5: 0x%lx\n", response->x5);
+    LOG_VMM("   x6: 0x%lx\n", response->x6);
+    LOG_VMM("   x7: 0x%lx\n", response->x7);
+}
+
+// #define DEBUG_SMC
+
+#if defined(CONFIG_ALLOW_SMC_CALLS)
+/* Default handler of SiP service calls passes through all the calls
+   to the firmware without applying any policy
+*/
+bool handle_sip(size_t vcpu_id, seL4_UserContext *regs, size_t fn_number)
+{
+    seL4_CPtr smc_cap = SMC_CAP;
+    seL4_ARM_SMCContext request;
+    seL4_ARM_SMCContext response;
+
+    request.x0 = regs->x0; request.x1 = regs->x1;
+    request.x2 = regs->x2; request.x3 = regs->x3;
+    request.x4 = regs->x4; request.x5 = regs->x5;
+    request.x6 = regs->x6; request.x7 = regs->x7;
+
+#if defined(DEBUG_SMC)
+    dump_smc_request(&request);
+#endif
+
+    seL4_ARM_SMC_Call(smc_cap, &request, &response);
+
+#if defined(DEBUG_SMC)
+    dump_smc_response(&response);
+#endif
+
+    regs->x0 = response.x0; regs->x1 = response.x1;
+    regs->x2 = response.x2; regs->x3 = response.x3;
+    regs->x4 = response.x4; regs->x5 = response.x5;
+    regs->x6 = response.x6; regs->x7 = response.x7;
+
+    bool success = fault_advance_vcpu(vcpu_id, regs);
+    assert(success);
+
+    return success;
+}
+#endif
+
 // @ivanv: print out which SMC call as a string we can't handle.
 bool handle_smc(size_t vcpu_id, uint32_t hsr)
 {
@@ -107,9 +171,24 @@ bool handle_smc(size_t vcpu_id, uint32_t hsr)
             if (fn_number < PSCI_MAX) {
                 return handle_psci(vcpu_id, &regs, fn_number, hsr);
             }
-            LOG_VMM_ERR("Unhandled SMC: standard service call %lu\n", fn_number);
+            LOG_VMM_ERR("Failed to handle SMC standard service call %lu\n", fn_number);
             break;
+        case SMC_CALL_SIP_SERVICE:
+            /* If the seL4 configuration does not allow us to forward SMC calls, then we
+             * simply drop through to the default case in the switch statement. */
+#if defined(CONFIG_ALLOW_SMC_CALLS)
+#if defined(DEBUG_SMC)
+            LOG_VMM("Handling SiP call with function number: 0x%lx\n", fn_number);
+#endif
+            if (!handle_sip(vcpu_id, &regs, fn_number)) {
+                LOG_VMM_ERR("Failed to handle SMC SiP service call %lu\n", fn_number);
+                return false;
+            } else {
+                return true;
+            }
+#endif
         default:
+            // @ivanv: print out a string of what service that is unknown
             LOG_VMM_ERR("Unhandled SMC: unknown value service: 0x%lx, function number: 0x%lx\n", service, fn_number);
             break;
     }
