1de8893302ee9fb9c91408f2fc3e0f5709798657
Nataliya Korovkina <malus.brandywine@gmail.com>
2023-10-05 14:26:56 -0400

    Add SMC calls forwarding framework
        Added:
        - a framework for forwarding SMC calls to Secure Monitor,
        - SiP Service entry in the framework.
    
    Signed-off-by: Nataliya Korovkina <malus.brandywine@gmail.com>

--- src/arch/aarch64/smc.h
+++ src/arch/aarch64/smc.h
@@ -23,17 +23,16 @@ uint64_t smc_get_arg(seL4_UserContext *u, uint64_t arg);
 
 #if defined(CONFIG_ALLOW_SMC_CALLS)
 
-/* Set SMC Call Cap to use by the library API */
-bool smc_set_cap(seL4_CPtr smcccap);
-
 /* Default handler of calls to a SMC Service */
 bool smc_default_handle_service(size_t vcpu_id, seL4_UserContext *regs, uint64_t fn_number);
 
 /* Service handler type */
 typedef bool (* smc_handle_service_type)(size_t, seL4_UserContext *, uint64_t);
 
-/* Default handler of SiP service calls */
-bool smc_default_handle_sip(size_t vcpu_id, seL4_UserContext *regs, uint64_t fn_number);
+/* API for user: */
+
+/* Set SMC Call Cap to use by the library API */
+bool smc_set_cap(seL4_CPtr smcccap);
 
 /* 'set_handler_sip_service()' assigns/removes active SiP Service handler */
 bool smc_set_handler_sip_service(smc_handle_service_type handler_func);
