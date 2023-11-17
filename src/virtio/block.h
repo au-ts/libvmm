2149559890925259719981c28bc4acd58922ea52
Eric Chan <eric.chan2@unsw.edu.au>
2023-11-17 15:01:43 +1100

    implementation for in out and flush, barrier and plugged not yet

--- src/virtio/block.h
+++ src/virtio/block.h
@@ -41,8 +41,6 @@
 #define VIRTIO_BLK_NUM_VIRTQ 1
 #define VIRTIO_BLK_VIRTQ_DEFAULT 0
 
-#define VIRTIO_BLK_SECTOR_SIZE 512 // 0x200
-
 #define VIRTIO_BLK_CAPACITY             0x100000
 
 /* Feature bits */
@@ -137,6 +135,17 @@ struct virtio_blk_outhdr {
 #define VIRTIO_BLK_S_OK             0
 #define VIRTIO_BLK_S_IOERR          1
 #define VIRTIO_BLK_S_UNSUPP         2
+
+// /** 
+//  * Callback to be called when a response is received from sDDF layer
+//  * 
+//  * @param virtio_desc
+//  * @param ring_handle
+//  * @param status
+//  * @param sddf_desc
+//  * @param count
+//  */
+// typedef void (*virtio_blk_cmd_cb)(uint16_t, sddf_blk_ring_handle_t, sddf_blk_response_status_t, uint32_t, uint16_t);
 
 void virtio_blk_init(struct virtio_device *dev,
                     struct virtio_queue_handler *vqs, size_t num_vqs,
