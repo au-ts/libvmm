5969f2a3f48aa9eb6768246081993e076cc8ef05
Eric Chan <eric.chan2@unsw.edu.au>
2023-12-18 15:26:37 +1100

    changing iozone results to csv format

--- examples/blk_benchmark/parse.py
+++ examples/blk_benchmark/parse.py
@@ -21,7 +21,7 @@ def parse_iozone_results(input_files, output_file):
 
     # Writing results with raw data and statistics
     with open(output_file, 'w') as file:
-        file.write("Record Length, Write: num1, num2, ..., Avg, Std, Read: num1, num2, ..., Avg, Std\n")
+        file.write("reclen,write_num1,write_num2,write_num3,write_avg,write_std,read_num1,read_num2,read_num3,read_Avg,read_std\n")
         for reclen, data in sorted(aggregated_results.items(), key=lambda x: int(x[0])):
             write_values = data["write"]
             read_values = data["read"]
@@ -34,7 +34,7 @@ def parse_iozone_results(input_files, output_file):
             write_values_str = ', '.join(map(str, write_values))
             read_values_str = ', '.join(map(str, read_values))
 
-            line = f"{reclen}, Write: {write_values_str}, {write_avg}, {write_std}, Read: {read_values_str}, {read_avg}, {read_std}\n"
+            line = f"{reclen},{write_values_str},{write_avg},{write_std},{read_values_str},{read_avg},{read_std}\n"
             file.write(line)
 
 if len(sys.argv) < 3:
