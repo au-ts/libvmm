2a2edae1d5be96a9c8487ee70b1968f2ed4d8614
Eric Chan <erichchan999@gmail.com>
2024-02-06 17:29:09 +1100

    restructuring VM userlevel into tools
    restructuring VM userlevel into tools

---
+++ tools/linux/include/uio_drivers/blk.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2024, UNSW
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+#pragma once
+
+#include <stdint.h>
+
+int driver_init(void **maps, uintptr_t *maps_phys, int num_maps);
+void driver_notified();
