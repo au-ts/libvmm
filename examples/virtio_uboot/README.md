253f301c9030827b4caba5de804723290f0bc7c0
James <z5345944@ad.unsw.edu.au>
2024-04-24 14:33:12 +1000

    copy of virtio folder

---
+++ examples/virtio_uboot/README.md
@@ -0,0 +1,82 @@
+# Using virtIO with multiple Linux guests
+
+This example shows off the virtIO support that libvmm provides using the
+[seL4 Device Driver Framework](https://github.com/au-ts/sddf) to talk to the
+actual hardware.
+
+The example currently works on the following platforms:
+* QEMU ARM virt
+
+## Building
+
+```sh
+make BOARD=<BOARD> MICROKIT_SDK=/path/to/sdk
+```
+
+Where `<BOARD>` is one of:
+* `qemu_arm_virt`
+* `odroidc4`
+
+Other configuration options can be passed to the Makefile such as `CONFIG`
+and `BUILD_DIR`, see the Makefile for details.
+
+If you would like to simulate the QEMU board you can run the following command:
+```sh
+make BOARD=qemu_arm_virt MICROKIT_SDK=/path/to/sdk qemu
+```
+
+This will build the example code as well as run the QEMU command to simulate a
+system running the whole system.
+
+## Running
+
+### virtIO console
+
+This example makes use of the virtIO console device so that neither guest has access
+to any serial device on the platform. The virtIO console support in libvmm talks to
+a serial multiplexor which then talks to a driver for input/output to the physical
+serial device.
+
+When you boot the example, you will see different coloured output for each guest. The Linux logs will be interleaving like so:
+```
+Starting klogd: OKStarting klogd: 
+OK
+Running sysctl: Running sysctl: OK
+OKSaving random seed: 
+Saving random seed: [    4.070358] random: crng init done
+[    4.103992] random: crng init done
+OK
+Starting network: OK
+Starting network: OK
+OK
+
+Welcome to Buildroot
+buildroot login: 
+Welcome to Buildroot
+buildroot login:
+```
+
+Initially all input is defaulted
+to guest 1 in green. To switch to input into the other guest (red), type in `@2`.
+The `@` symbol is used to switch between clients of the serial system, in this case the red guest is client 2.
+
+### virtIO block
+
+Guest 1 and guest 2 also doubles as a client in the block system that talks virtIO to guest 3 that acts as driver with passthrough access to the block device. The requests from both clients are multiplexed through the additional block virtualiser component.
+
+When you boot the example, the block driver VM will boot first. When it is ready, the client VMs will boot together. After the client VMs boot, they will attempt to mount the virtIO block device `/dev/vda` into `/mnt`. The kernel logs from linux will show the virtIO drive initialising for both clients.
+```
+[    5.381885] virtio_blk virtio1: [vda] 2040 512-byte logical blocks (1.04 MB/1020 KiB)
+[    5.325953] virtio_blk virtio1: [vda] 2040 512-byte logical blocks (1.04 MB/1020 KiB)
+```
+
+We can test the virtIO block implementation by using block benchmark programs like `postmark`, which has been included in `/root`, by running it in the `/mnt` directory.
+
+The system expects the storage device to contain an MBR partition table that contains two partitions. Each partition is allocated to a single client. Partitions must have a starting block number that is a multiple of sDDF block's transfer size of 4096 bytes divided by the disk's sector size. Partitions that do not follow this restriction are unsupported.
+
+### QEMU set up
+When running on QEMU, read and writes go to an emulated ramdisk instead of to your local storage device. The ramdisk file supplied to QEMU is formatted during build time to contain a FAT filesystem for both partitions.
+
+### Hardware set up
+When running on the odroidc4, the system expects to read and write from the SD card. You will need to format the SD card yourself on the odroidc4 system.
+
---
+++ examples/virtio_uboot/blk_driver_vmm.c
@@ -0,0 +1,139 @@
+/*
+ * Copyright 2023, UNSW
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+#include <stddef.h>
+#include <stdint.h>
+#include <microkit.h>
+#include <util.h>
+#include <vgic/vgic.h>
+#include <linux.h>
+#include <fault.h>
+#include <guest.h>
+#include <virq.h>
+#include <tcb.h>
+#include <vcpu.h>
+
+// #define DEBUG_BLK_DRIVER_VM
+
+#define GUEST_RAM_SIZE 0x6000000
+
+#if defined(BOARD_qemu_arm_virt)
+#define GUEST_DTB_VADDR 0x47f00000
+#define GUEST_INIT_RAM_DISK_VADDR 0x47000000
+#elif defined(BOARD_odroidc4)
+#define GUEST_DTB_VADDR 0x25f10000
+#define GUEST_INIT_RAM_DISK_VADDR 0x24000000
+#else
+#error Need to define guest kernel image address and DTB address
+#endif
+
+/* Data for the guest's kernel image. */
+extern char _guest_kernel_image[];
+extern char _guest_kernel_image_end[];
+/* Data for the device tree to be passed to the kernel. */
+extern char _guest_dtb_image[];
+extern char _guest_dtb_image_end[];
+/* Data for the initial RAM disk to be passed to the kernel. */
+extern char _guest_initrd_image[];
+extern char _guest_initrd_image_end[];
+/* Microkit will set this variable to the start of the guest RAM memory region. */
+uintptr_t guest_ram_vaddr;
+
+/* sDDF block */
+#define SD_CH 1
+#if defined(BOARD_odroidc4)
+#define SD_IRQ 222
+#elif defined(BOARD_qemu_arm_virt)
+#define SD_IRQ 79
+#endif
+
+// @ericc: autogen
+#define UIO_IRQ 50
+#define UIO_CH 3
+
+void uio_ack(size_t vcpu_id, int irq, void *cookie) {
+    microkit_notify(UIO_CH);
+}
+
+void init(void) {
+    /* Initialise the VMM, the VCPU(s), and start the guest */
+    LOG_VMM("starting \"%s\"\n", microkit_name);
+    /* Place all the binaries in the right locations before starting the guest */
+    size_t kernel_size = _guest_kernel_image_end - _guest_kernel_image;
+    size_t dtb_size = _guest_dtb_image_end - _guest_dtb_image;
+    size_t initrd_size = _guest_initrd_image_end - _guest_initrd_image;
+    uintptr_t kernel_pc = linux_setup_images(guest_ram_vaddr,
+                                      (uintptr_t) _guest_kernel_image,
+                                      kernel_size,
+                                      (uintptr_t) _guest_dtb_image,
+                                      GUEST_DTB_VADDR,
+                                      dtb_size,
+                                      (uintptr_t) _guest_initrd_image,
+                                      GUEST_INIT_RAM_DISK_VADDR,
+                                      initrd_size
+                                      );
+    if (!kernel_pc) {
+        LOG_VMM_ERR("Failed to initialise guest images\n");
+        return;
+    }
+    
+    /* Initialise the virtual GIC driver */
+    bool success = virq_controller_init(GUEST_VCPU_ID);
+    if (!success) {
+        LOG_VMM_ERR("Failed to initialise emulated interrupt controller\n");
+        return;
+    }
+
+    /* Register the UIO IRQ */
+    virq_register(GUEST_VCPU_ID, UIO_IRQ, uio_ack, NULL);
+
+#if defined(BOARD_odroidc4)
+    /* Register the SD card IRQ */
+    virq_register_passthrough(GUEST_VCPU_ID, SD_IRQ, SD_CH);
+#if defined(DEBUG_BLK_DRIVER_VM)
+    virq_register_passthrough(GUEST_VCPU_ID, 225, 10);
+#endif
+#endif
+
+#if defined(BOARD_qemu_arm_virt)
+    /* Register the SD card IRQ */
+    virq_register_passthrough(GUEST_VCPU_ID, SD_IRQ, SD_CH);
+#if defined(DEBUG_BLK_DRIVER_VM)
+    virq_register_passthrough(GUEST_VCPU_ID, 33, 10);
+#endif
+#endif
+
+    /* Finally start the guest */
+    guest_start(GUEST_VCPU_ID, kernel_pc, GUEST_DTB_VADDR, GUEST_INIT_RAM_DISK_VADDR);
+}
+
+void notified(microkit_channel ch) {
+    bool handled = false;
+
+    handled = virq_handle_passthrough(ch);
+
+    if (ch == UIO_CH) {
+        virq_inject(GUEST_VCPU_ID, UIO_IRQ);
+        handled = true;
+    }
+
+    if (!handled) {
+        LOG_VMM_ERR("Unhandled notification on channel %d\n", ch);
+    }
+}
+
+/*
+ * The primary purpose of the VMM after initialisation is to act as a fault-handler,
+ * whenever our guest causes an exception, it gets delivered to this entry point for
+ * the VMM to handle.
+ */
+void fault(microkit_id id, microkit_msginfo msginfo) {
+    bool success = fault_handle(id, msginfo);
+    if (success) {
+        /* Now that we have handled the fault successfully, we reply to it so
+         * that the guest can resume execution. */
+        microkit_fault_reply(microkit_msginfo_new(0, 0));
+    }
+}
