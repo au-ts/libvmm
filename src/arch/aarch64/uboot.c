7bc7f05a72a80f57c2895ac7fd860da6ded3898e
James <z5345944@ad.unsw.edu.au>
2024-04-24 15:52:03 +1000

    Updated uboot files

--- src/arch/aarch64/uboot.c
+++ src/arch/aarch64/uboot.c
@@ -6,6 +6,7 @@
 
 #include "../../util/util.h"
 #include "uboot.h"
+
 uintptr_t uboot_setup_images(uintptr_t ram_start,
                              uintptr_t uboot,
                              size_t uboot_size,
@@ -15,48 +16,13 @@ uintptr_t uboot_setup_images(uintptr_t ram_start,
                              size_t dtb_size
                              )
 {
-    //TODO: need to work out where in RAM does uboot go??? Seems like this would be board specific? Could just try loading it into ram start
-    uintptr_t uboot_dest = ram_start + uboot_offset; // we shift uboot so we can put the devic tree blob at the start of RAM
+    // Shift uboot as it seems to expect DTB is at start of RAM
+    uintptr_t uboot_dest = ram_start + uboot_offset;
     LOG_VMM("Copying u-boot to 0x%x (0x%x bytes)\n", uboot_dest, uboot_size);
     memcpy((char *)uboot_dest, (char *)uboot, uboot_size);
 
-    // Just using the linux dtb here so we have one
-    LOG_VMM("Copying guest DTB to 0x%x (0x%x bytes)\n", dtb_dest, dtb_size); // need to place a device tree here to be picked up by Uboot
+    LOG_VMM("Copying guest DTB to 0x%x (0x%x bytes)\n", dtb_dest, dtb_size);
     memcpy((char *)dtb_dest, (char *)dtb_src, dtb_size);
-
-    return uboot_dest;
-}
-
-uintptr_t uboot_setup_kernel(uintptr_t ram_start,
-                             uintptr_t uboot,
-                             size_t uboot_size,
-                             uintptr_t dtb_src,
-                             uintptr_t dtb_dest,
-                             size_t dtb_size,
-                             uintptr_t image_src,
-                             uintptr_t image_dest,
-                             size_t image_size
-                             )
-{
-    //TODO: need to work out where in RAM does uboot go??? Seems like this would be board specific? Could just try loading it into ram start
-    uintptr_t uboot_dest = ram_start + 0x200000; // we shift uboot so we can put the devic tree blob at the start of RAM
-    LOG_VMM("Copying u-boot to 0x%x (0x%x bytes)\n", uboot_dest, uboot_size);
-    memcpy((char *)uboot_dest, (char *)uboot, uboot_size);
-
-    // Just using the linux dtb here so we have one
-    LOG_VMM("Copying guest DTB to 0x%x (0x%x bytes)\n", dtb_dest, dtb_size); // need to place a device tree here to be picked up by Uboot
-    memcpy((char *)dtb_dest, (char *)dtb_src, dtb_size);
-
-    /* setup and testing for the Linux image */
-    struct linux_image_header *image_header = (struct linux_image_header *) image_src;
-    assert(image_header->magic == LINUX_IMAGE_MAGIC);
-    if (image_header->magic != LINUX_IMAGE_MAGIC) {
-        LOG_VMM_ERR("Linux kernel image magic check failed\n");
-        return 0;
-    }
-
-    LOG_VMM("Copying image to 0x%x (0x%x bytes)\n", image_dest, image_size);
-    memcpy((char *)image_dest, (char *)image_src, image_size);
 
     return uboot_dest;
 }
@@ -67,6 +33,7 @@ bool uboot_start(size_t boot_vcpu_id, uintptr_t pc, uintptr_t dtb) {
      * You will note that this is currently Linux specific as we currently do not support
      * any other kind of guest. However, even though the library is open to supporting other
      * guests, there is no point in prematurely generalising this code.
+     * TODO - not sure if we should update this for UBoot or if we even need to do this???
      */
     seL4_UserContext regs = {0};
     regs.x0 = dtb;
