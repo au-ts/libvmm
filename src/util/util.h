01248f13c2719a4065328bb13d6625720c4f9af3
Eric Chan <eric.chan2@unsw.edu.au>
2024-04-26 13:46:42 +1000

    Make memcpy and memset externally linked
    Signed-off-by: Eric Chan <eric.chan2@unsw.edu.au>

--- src/util/util.h
+++ src/util/util.h
@@ -34,20 +34,8 @@
 #define LOG_VMM(...) do{ printf("%s|INFO: ", microkit_name); printf(__VA_ARGS__); }while(0)
 #define LOG_VMM_ERR(...) do{ printf("%s|ERROR: ", microkit_name); printf(__VA_ARGS__); }while(0)
 
-static void *memcpy(void *restrict dest, const void *restrict src, size_t n)
-{
-    unsigned char *d = dest;
-    const unsigned char *s = src;
-    for (; n; n--) *d++ = *s++;
-    return dest;
-}
-
-static void *memset(void *dest, int c, size_t n)
-{
-    unsigned char *s = dest;
-    for (; n; n--, s++) *s = c;
-    return dest;
-}
+void *memcpy(void *restrict dest, const void *restrict src, size_t n);
+void *memset(void *dest, int c, size_t n);
 
 static void assert_fail(
     const char  *assertion,
