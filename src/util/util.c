c2c351bb76ff36de8f99b56cb13cea8ccc5dff5e
Eric Chan <eric.chan2@unsw.edu.au>
2024-04-26 13:46:42 +1000

    Make memcpy and memset externally linked
    Signed-off-by: Eric Chan <eric.chan2@unsw.edu.au>

--- src/util/util.c
+++ src/util/util.c
@@ -14,3 +14,23 @@ void _putchar(char character)
 {
     microkit_dbg_putc(character);
 }
+
+void *memset(void *dest, int c, size_t n)
+{
+    unsigned char *s = dest;
+    for (; n; n--, s++) {
+        *s = c;
+    }
+    return dest;
+}
+
+void *memcpy(void *restrict dest, const void *restrict src, size_t n)
+{
+    unsigned char *d = dest;
+    const unsigned char *s = src;
+    for (; n; n--) {
+        *d++ = *s++;
+    }
+    return dest;
+}
+
