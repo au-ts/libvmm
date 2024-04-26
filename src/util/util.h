e7ba088428a4c329b799d975350c8d165e997a53
Eric Chan <eric.chan2@unsw.edu.au>
2024-04-26 13:47:05 +1000

    Add printing in hex convenience function
    Signed-off-by: Eric Chan <eric.chan2@unsw.edu.au>

--- src/util/util.h
+++ src/util/util.h
@@ -50,6 +50,9 @@ static void assert_fail(
 #define BIT_LOW(n)  (1ul<<(n))
 #define BIT_HIGH(n) (1ul<<(n - 32 ))
 
+/* Convenience function to print memory region in hex */
+void print_mem_hex(uintptr_t addr, size_t size);
+
 #ifndef CONFIG_DEBUG_BUILD
 
 #define _unused(x) ((void)(x))
