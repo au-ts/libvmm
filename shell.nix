a50cd87f9281654dff962bb3004fa47a791eacba
Ivan Velickovic <i.velickovic@unsw.edu.au>
2024-05-03 14:41:05 +1000

    nix: add aarch64-unknown-linux-gcc
    Needed for virtIO example since it cross-compiles for Linux
    user-space.
    
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- shell.nix
+++ shell.nix
@@ -4,6 +4,9 @@ let
     pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
     rust = pkgs.rust-bin.fromRustupToolchainFile ./examples/rust/rust-toolchain.toml;
     llvm = pkgs.llvmPackages_16;
+    linux_aarch64_cross = import <nixpkgs> {
+        crossSystem = { config = "aarch64-unknown-linux-gnu"; };
+    };
     manual_deps = with pkgs.buildPackages; [
         texliveFull
         pandoc
@@ -24,6 +27,8 @@ in
         # expect is only needed for CI testing but we include it for
         # completeness
         expect
+        # For when we need to build user-space applications for Linux guests
+        linux_aarch64_cross.buildPackages.gcc
     ] ++ manual_deps;
     hardeningDisable = [ "all" ];
     # Need to specify this when using Rust with bindgen
