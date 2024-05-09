25ec2a945a551cf03e0e22e8ad6e2fb1e7359b37
Ivan Velickovic <i.velickovic@unsw.edu.au>
2024-05-09 12:04:39 +1000

    sound: minor style changes
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- tools/linux/include/uio/sound.h
+++ tools/linux/include/uio/sound.h
@@ -1,6 +1,7 @@
 #pragma once
-#define UIO_SND_FAULT_ADDRESS 0x301000
 #include <sddf/sound/sound.h>
+
+#define UIO_SND_FAULT_ADDRESS 0x301000
 
 typedef struct vm_shared_state {
     sound_shared_state_t sound;
