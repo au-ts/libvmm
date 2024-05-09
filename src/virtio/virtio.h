8e930b5fc4021ca83728871918a1ca9e30c0adef
Alex Brown <alexandermichaelbrown@outlook.com>
2024-05-09 12:10:36 +1000

    Add back virtIO 1.2 comment
    Signed-off-by: Alex Brown <alexandermichaelbrown@outlook.com>

--- src/virtio/virtio.h
+++ src/virtio/virtio.h
@@ -3,3 +3,9 @@
 #include "block.h"
 #include "console.h"
 #include "sound.h"
+
+/*
+ * All terminology used and functionality of the virtIO device implementation
+ * adheres with the following specification:
+ * Virtual I/O Device (VIRTIO) Version 1.2
+ */
