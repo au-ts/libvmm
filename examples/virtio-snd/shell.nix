2c984d0051e3bb581ef453947f9ab05db5979d00
Ivan Velickovic <i.velickovic@unsw.edu.au>
2024-06-17 17:29:11 +1000

    Use normal nixpkgs for virtio-snd example
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- examples/virtio-snd/shell.nix
+++ examples/virtio-snd/shell.nix
@@ -1,4 +1,6 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/057f9aecfb71c4437d2b27d3323df7f93c010b7e.tar.gz") {} }:
+let
+  pkgs = import <nixpkgs> {};
+in
   pkgs.mkShell {
     nativeBuildInputs =
     let
@@ -18,3 +20,4 @@
       ];
     in crossInputs ++ nativeInputs;
 }
+
