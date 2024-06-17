2ede5d9f0e0963e2221efd95f2cc252e2ec75df8
Ivan Velickovic <i.velickovic@unsw.edu.au>
2024-06-17 17:51:30 +1000

    Fix for latest Zig
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- examples/simple/build.zig
+++ examples/simple/build.zig
@@ -7,13 +7,13 @@ const MicrokitBoard = enum {
 
 const Target = struct {
     board: MicrokitBoard,
-    zig_target: std.zig.CrossTarget,
+    zig_target: std.Target.Query,
 };
 
 const targets = [_]Target {
     .{
         .board = MicrokitBoard.qemu_arm_virt,
-        .zig_target = std.zig.CrossTarget{
+        .zig_target = std.Target.Query{
             .cpu_arch = .aarch64,
             .cpu_model = .{ .explicit = &std.Target.arm.cpu.cortex_a53 },
             .os_tag = .freestanding,
@@ -22,7 +22,7 @@ const targets = [_]Target {
     },
     .{
         .board = MicrokitBoard.odroidc4,
-        .zig_target = std.zig.CrossTarget{
+        .zig_target = std.Target.Query{
             .cpu_arch = .aarch64,
             .cpu_model = .{ .explicit = &std.Target.arm.cpu.cortex_a55 },
             .os_tag = .freestanding,
@@ -31,7 +31,7 @@ const targets = [_]Target {
     }
 };
 
-fn findTarget(board: MicrokitBoard) std.zig.CrossTarget {
+fn findTarget(board: MicrokitBoard) std.Target.Query {
     for (targets) |target| {
         if (board == target.board) {
             return target.zig_target;
