tharveyy/libvmm-virtio-maaxboard

Add support for Avnet MaaXBoard

examples/simple/build.zig
@@ -2,7 +2,8 @@ const std = @import("std");
 
 const MicrokitBoard = enum {
     qemu_arm_virt,
-    odroidc4
+    odroidc4,
+    maaxboard,
 };
 
 const Target = struct {
@@ -28,7 +29,16 @@ const targets = [_]Target {
             .os_tag = .freestanding,
             .abi = .none,
         },
-    }
+    },
+    .{
+        .board = MicrokitBoard.maaxboard,
+        .zig_target = std.zig.CrossTarget{
+            .cpu_arch = .aarch64,
+            .cpu_model = .{ .explicit = &std.Target.arm.cpu.cortex_a53 },
+            .os_tag = .freestanding,
+            .abi = .none,
+        },
+    },
 };
 
 fn findTarget(board: MicrokitBoard) std.zig.CrossTarget {
@@ -48,7 +58,7 @@ const ConfigOptions = enum {
     benchmark
 };
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
 
     // Getting the path to the Microkit SDK before doing anything else
@@ -99,12 +109,18 @@ pub fn build(b: *std.Build) void {
         .strip = false,
     });
 
+    const base_dts_file = try std.fs.cwd().openFile(b.fmt("board/{s}/linux.dts", .{ microkit_board }), .{});
+    const base_dts = try base_dts_file.reader().readAllAlloc(b.allocator, (try base_dts_file.stat()).size);
+    const overlay_dts_file = try std.fs.cwd().openFile(b.fmt("board/{s}/overlay.dts", .{ microkit_board }), .{});
+    const overlay_dts = try base_dts_file.reader().readAllAlloc(b.allocator, (try overlay_dts_file.stat()).size);
+    const final_dts = try std.mem.concat(b.allocator, u8, &[_][]const u8{ base_dts, overlay_dts });
+
     // For actually compiling the DTS into a DTB
-    const dts_path = b.fmt("board/{s}/linux.dts", .{ microkit_board });
     const dtc_cmd = b.addSystemCommand(&[_][]const u8{
         "dtc", "-q", "-I", "dts", "-O", "dtb"
     });
-    dtc_cmd.addFileArg(.{ .path = dts_path });
+    dtc_cmd.addFileArg(.{ .path = b.getInstallPath(.prefix, "final.dts") });
+    dtc_cmd.step.dependOn(&b.addInstallFileWithDir(final_dts, .prefix, "final.dts").step);
     const dtb = dtc_cmd.captureStdOut();
 
     // Add microkit.h to be used by the API wrapper.

au-ts/libvmm

Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

examples/simple/build.zig
@@ -2,7 +2,8 @@ const std = @import("std");
 
 const MicrokitBoard = enum {
     qemu_arm_virt,
-    odroidc4
+    odroidc4,
+    maaxboard,
 };
 
 const Target = struct {
@@ -28,7 +29,16 @@ const targets = [_]Target {
             .os_tag = .freestanding,
             .abi = .none,
         },
-    }
+    },
+    .{
+        .board = MicrokitBoard.maaxboard,
+        .zig_target = std.Target.Query{
+            .cpu_arch = .aarch64,
+            .cpu_model = .{ .explicit = &std.Target.arm.cpu.cortex_a53 },
+            .os_tag = .freestanding,
+            .abi = .none,
+        },
+    },
 };
 
 fn findTarget(board: MicrokitBoard) std.Target.Query {
@@ -99,12 +109,19 @@ pub fn build(b: *std.Build) void {
         .strip = false,
     });
 
+    const base_dts_path = b.fmt("board/{s}/linux.dts", .{ microkit_board });
+    const overlay = b.fmt("board/{s}/overlay.dts", .{ microkit_board });
+    const dts_cat_cmd = b.addSystemCommand(&[_][]const u8{
+        "sh", "../../tools/dtscat", base_dts_path, overlay
+    });
+    const final_dts = dts_cat_cmd.captureStdOut();
+
     // For actually compiling the DTS into a DTB
-    const dts_path = b.fmt("board/{s}/linux.dts", .{ microkit_board });
     const dtc_cmd = b.addSystemCommand(&[_][]const u8{
         "dtc", "-q", "-I", "dts", "-O", "dtb"
     });
-    dtc_cmd.addFileArg(b.path(dts_path));
+    dtc_cmd.addFileArg(.{ .cwd_relative = b.getInstallPath(.prefix, "final.dts") });
+    dtc_cmd.step.dependOn(&b.addInstallFileWithDir(final_dts, .prefix, "final.dts").step);
     const dtb = dtc_cmd.captureStdOut();
 
     // Add microkit.h to be used by the API wrapper.