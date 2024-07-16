e32bdc5287eb6f0369c11ac154be3dd64338fa8a
Alex Brown <alexandermichaelbrown@outlook.com>
2024-07-16 14:49:38 +1000

    examples/virtio-net: uncomment net stuff
    Signed-off-by: Alex Brown <alexandermichaelbrown@outlook.com>

--- examples/virtio-net/client_vmm.c
+++ examples/virtio-net/client_vmm.c
@@ -56,16 +56,16 @@ char *serial_tx_data;
 
 static struct virtio_console_device virtio_console;
 
-// uintptr_t net_rx_free;
-// uintptr_t net_rx_active;
-// uintptr_t net_tx_free;
-// uintptr_t net_tx_active;
-// uintptr_t net_rx_data;
-// uintptr_t net_tx_data;
+uintptr_t net_rx_free;
+uintptr_t net_rx_active;
+uintptr_t net_tx_free;
+uintptr_t net_tx_active;
+uintptr_t net_rx_data;
+uintptr_t net_tx_data;
 
-// static struct virtio_net_device virtio_net;
-// #define NET_VIRT_RX_CH 3
-// #define NET_VIRT_TX_CH 4
+static struct virtio_net_device virtio_net;
+#define NET_VIRT_RX_CH 3
+#define NET_VIRT_TX_CH 4
 
 #define VIRTIO_NET_IRQ (77)
 #define VIRTIO_NET_BASE (0x190000)
@@ -115,32 +115,25 @@ void init(void)
     LOG_VMM("Initialised virtio-console device\n");
 
     /* Initialise the virtIO net device */
-    // assert(net_rx_data != 0);
-    // assert(net_tx_data != 0);
+    assert(net_rx_data);
+    assert(net_tx_data);
 
-    // net_queue_handle_t rx, tx;
-    // net_cli_queue_init_sys(microkit_name, &rx, net_rx_free, net_rx_active, &tx, net_tx_free, net_tx_active);
-    // // net_queue_init(&rx, (net_queue_t *)net_rx_free, (net_queue_t *)net_rx_active, NUM_ENTRIES);
-    // // net_queue_init(&tx, (net_queue_t *)net_tx_free, (net_queue_t *)net_tx_active, NUM_ENTRIES);
+    net_queue_handle_t net_rxq, net_txq;
+    net_cli_queue_init_sys(microkit_name, &net_rxq, net_rx_free, net_rx_active, &net_txq, net_tx_free, net_tx_active);
 
-    // LOG_VMM("Initialised net queues\n");
+    uint8_t mac[6];
+    net_cli_mac_addr_init_sys(microkit_name, mac);
 
-    // // net_buffers_init(&rx, net_rx_data);
-    // // net_buffers_init(&tx, net_tx_data);
-
-    // // LOG_VMM("Initialised net buffers\n");
-
-    // uint8_t mac[6] = {0x52, 0x54, 0x01, 0x00, 0x00, 0x16};
-    // success = virtio_mmio_net_init(&virtio_net,
-    //                                mac,
-    //                                VIRTIO_NET_BASE,
-    //                                VIRTIO_NET_SIZE,
-    //                                VIRTIO_NET_IRQ,
-    //                                &rx, &tx,
-    //                                net_rx_data, net_tx_data,
-    //                                NET_VIRT_RX_CH,
-    //                                NET_VIRT_TX_CH);
-    // assert(success);
+    success = virtio_mmio_net_init(&virtio_net,
+                                   mac,
+                                   VIRTIO_NET_BASE,
+                                   VIRTIO_NET_SIZE,
+                                   VIRTIO_NET_IRQ,
+                                   &net_rxq, &net_txq,
+                                   net_rx_data, net_tx_data,
+                                   NET_VIRT_RX_CH,
+                                   NET_VIRT_TX_CH);
+    assert(success);
 
     LOG_VMM("Initialised virtio-net device\n");
 
@@ -157,10 +150,10 @@ void notified(microkit_channel ch)
         virtio_console_handle_rx(&virtio_console);
         break;
     }
-    // case NET_VIRT_RX_CH: {
-    //     virtio_net_handle_rx(&virtio_net);
-    //     break;
-    // }
+    case NET_VIRT_RX_CH: {
+        virtio_net_handle_rx(&virtio_net);
+        break;
+    }
     default:
         LOG_VMM_ERR("Unexpected channel, ch: 0x%lx\n", ch);
     }
