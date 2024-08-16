50e3800cceb76fd59f036852ba079bb1ec2e547a
Anuraag <z5342488@ad.unsw.edu.au>
2024-08-16 13:58:47 +1000

    use capacity instead of size
    Signed-off-by: Anuraag <anuraag23.04@gmail.com>

--- tools/linux/uio_drivers/blk/blk.c
+++ tools/linux/uio_drivers/blk/blk.c
@@ -116,7 +116,7 @@ int driver_init(void **maps, uintptr_t *maps_phys, int num_maps, int argc, char 
     blk_config->capacity = size / BLK_TRANSFER_SIZE;
 
     LOG_UIO_BLOCK("Raw block device: read_only=%d, sector_size=%d, size=%ld\n", (int)blk_config->read_only,
-                  blk_config->sector_size, blk_config->size);
+                  blk_config->sector_size, blk_config->capacity);
 
     /* Optimal size */
     /* As far as I know linux does not let you query this from userspace, set as 0 to mean undefined */
