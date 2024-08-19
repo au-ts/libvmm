e35c35c2202dcf640dbc0a098399a5b68316309d
Ivan Velickovic <i.velickovic@unsw.edu.au>
2024-08-19 22:50:02 +1000

    shell.nix: delete unused variable
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- shell.nix
+++ shell.nix
@@ -7,9 +7,6 @@ let
     pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
     rust = pkgs.rust-bin.fromRustupToolchainFile ./examples/rust/rust-toolchain.toml;
     llvm = pkgs.llvmPackages_16;
-    linux_aarch64_cross = import <nixpkgs> {
-        crossSystem = { config = "aarch64-unknown-linux-gnu"; };
-    };
 in
   pkgs.mkShell {
     buildInputs = with pkgs.buildPackages; [
