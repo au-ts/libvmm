38ae41a1c7a1f59b76b9032fa0440946d81b2544
Ivan Velickovic <i.velickovic@unsw.edu.au>
2024-09-03 20:18:38 +1000

    Add support for custom SMC SiP handler
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- include/libvmm/arch/aarch64/smc.h
+++ include/libvmm/arch/aarch64/smc.h
@@ -13,7 +13,24 @@
 #include <microkit.h>
 
 /* SMC vCPU fault handler */
-bool handle_smc(size_t vcpu_id, uint32_t hsr);
+bool smc_handle(size_t vcpu_id, uint32_t hsr);
+
+/*
+ * A custom handler for SMC SiP calls can be registered.
+ * By default SiP calls are not handled.
+ * Note only one handler can be registered at a given time.
+ * smc_register_sip_handler returns false if a handler already exists.
+ */
+typedef bool (*smc_sip_handler_t)(size_t vcpu_id, seL4_UserContext *regs, size_t fn_number);
+bool smc_register_sip_handler(smc_sip_handler_t handler);
+
+#if defined(CONFIG_ALLOW_SMC_CALLS)
+/*
+ * Handle SMC SiP calls simply by forwarding the arguments to seL4 to perform
+ * and placing the response registers into the guest's TCB registers.
+ */
+bool smc_sip_forward(size_t vcpu, seL4_UserContext *regs, size_t fn_number);
+#endif
 
 /* Helper functions */
 void smc_set_return_value(seL4_UserContext *u, uint64_t val);
