01a75e43119c7d01434df7012148d6fe58c334c6
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2024-10-29 16:03:56 +1100

    build.zig: minor fixes for ARM targets
    Signed-off-by: Ivan-Velickovic <i.velickovic@unsw.edu.au>

--- examples/simple/build.zig
+++ examples/simple/build.zig
@@ -19,7 +19,8 @@ const targets = [_]Target {
         .board = MicrokitBoard.qemu_virt_aarch64,
         .zig_target = std.Target.Query{
             .cpu_arch = .aarch64,
-            .cpu_model = .{ .explicit = &std.Target.arm.cpu.cortex_a53 },
+            .cpu_model = .{ .explicit = &std.Target.aarch64.cpu.cortex_a53 },
+            .cpu_features_add = std.Target.aarch64.featureSet(&[_]std.Target.aarch64.Feature{ .strict_align }),
             .os_tag = .freestanding,
             .abi = .none,
         },
@@ -28,7 +29,8 @@ const targets = [_]Target {
         .board = MicrokitBoard.odroidc4,
         .zig_target = std.Target.Query{
             .cpu_arch = .aarch64,
-            .cpu_model = .{ .explicit = &std.Target.arm.cpu.cortex_a55 },
+            .cpu_model = .{ .explicit = &std.Target.aarch64.cpu.cortex_a55 },
+            .cpu_features_add = std.Target.aarch64.featureSet(&[_]std.Target.aarch64.Feature{ .strict_align }),
             .os_tag = .freestanding,
             .abi = .none,
         },
@@ -37,7 +39,8 @@ const targets = [_]Target {
         .board = MicrokitBoard.maaxboard,
         .zig_target = std.Target.Query{
             .cpu_arch = .aarch64,
-            .cpu_model = .{ .explicit = &std.Target.arm.cpu.cortex_a53 },
+            .cpu_model = .{ .explicit = &std.Target.aarch64.cpu.cortex_a53 },
+            .cpu_features_add = std.Target.aarch64.featureSet(&[_]std.Target.aarch64.Feature{ .strict_align }),
             .os_tag = .freestanding,
             .abi = .none,
         },
--- examples/zig/build.zig
+++ examples/zig/build.zig
@@ -14,7 +14,8 @@ pub fn build(b: *std.Build) void {
     // systems programming language.
     const target = b.resolveTargetQuery(.{
         .cpu_arch = .aarch64,
-        .cpu_model = .{ .explicit = &std.Target.arm.cpu.cortex_a53 },
+        .cpu_model = .{ .explicit = &std.Target.aarch64.cpu.cortex_a53 },
+        .cpu_features_add = std.Target.aarch64.featureSet(&[_]std.Target.aarch64.Feature{ .strict_align }),
         .os_tag = .freestanding,
         .abi = .none,
     });
