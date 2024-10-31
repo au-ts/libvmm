008d2448ff7e7ff2bf21db3c04aed3a5512e08a2
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2024-11-01 10:22:31 +1100

    add UIO net driver
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

---
+++ tools/linux/uio_drivers/net/log.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2024, UNSW
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <stdio.h>
+
+#define DEBUG_NET
+
+#if defined(DEBUG_NET)
+#define LOG_NET(...) do{ fprintf(stderr, "UIO(NET): "); fprintf(stderr, __VA_ARGS__); }while(0)
+#else
+#define LOG_NET(...) do{}while(0)
+#endif
+
+#define LOG_NET_ERR(...) do{ fprintf(stderr, "UIO(NET)|ERROR: "); fprintf(stderr, __VA_ARGS__); }while(0)
+#define LOG_NET_WARN(...) do{ fprintf(stderr, "UIO(NET)|WARN: "); fprintf(stderr, __VA_ARGS__); }while(0)
