30ac5c82129d1f5bbcf6d2dadab5fca5b167c7da
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2024-11-01 11:48:22 +1100

    [no ci] examples/simple: add ariane to build.zig
    Signed-off-by: Ivan-Velickovic <i.velickovic@unsw.edu.au>

--- examples/simple/build.zig
+++ examples/simple/build.zig
@@ -6,6 +6,7 @@ const std = @import("std");
 const MicrokitBoard = enum {
     qemu_virt_aarch64,
     qemu_virt_riscv64,
+    ariane,
     odroidc4,
     maaxboard,
 };
@@ -28,6 +29,15 @@ const targets = [_]Target {
     },
    .{
         .board = MicrokitBoard.qemu_virt_riscv64,
+        .zig_target = std.Target.Query{
+            .cpu_arch = .riscv64,
+            .cpu_model = .{ .explicit = &std.Target.riscv.cpu.baseline_rv64 },
+            .os_tag = .freestanding,
+            .abi = .none,
+        },
+    },
+   .{
+        .board = MicrokitBoard.ariane,
         .zig_target = std.Target.Query{
             .cpu_arch = .riscv64,
             .cpu_model = .{ .explicit = &std.Target.riscv.cpu.baseline_rv64 },
@@ -109,7 +119,7 @@ pub fn build(b: *std.Build) !void {
     const arm_vgic_version: usize = switch (microkit_board_option.?) {
         .qemu_virt_aarch64, .odroidc4 => 2,
         .maaxboard => 3,
-        .qemu_virt_riscv64 => 0,
+        .qemu_virt_riscv64, .ariane => 0,
     };
 
     const libvmm_dep = b.dependency("libvmm", .{
