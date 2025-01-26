8d3409bc6d66217bc9748aee4b45be193fb8d283
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2025-01-27 01:42:11 +1100

    config.h: add virtio

--- include/libvmm/config.h
+++ include/libvmm/config.h
@@ -8,11 +8,18 @@
 
 #define VMM_MAX_IRQS 32
 #define VMM_MAX_VCPUS 32
+#define VMM_MAX_VIRTIO_DEVICES 32
 
 typedef struct vmm_config_irq {
     uint8_t id;
     uint32_t irq;
 } vmm_config_irq_t;
+
+typedef struct vmm_config_virtio_device {
+    uint64_t base;
+    uint64_t size;
+    uint32_t irq;
+} vmm_config_virtio_device_t;
 
 typedef struct vmm_config_vcpu {
     uint8_t id;
@@ -27,6 +34,8 @@ typedef struct vmm_config {
     vmm_config_irq_t irqs[VMM_MAX_IRQS];
     uint8_t num_vcpus;
     vmm_config_vcpu_t vcpus[VMM_MAX_VCPUS];
+    uint8_t num_virtio_devices;
+    vmm_config_virtio_device_t virtio_devices[VMM_MAX_VIRTIO_DEVICES];
 } vmm_config_t;
 
 int vmm_config_irq_from_id(vmm_config_t *config, uint8_t id) {
