96e76ea1ddf1fa5e2274c0d8e2444d12a50d481f
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2025-01-31 14:43:07 +1100

    Get virtIO net working within client VM
    Signed-off-by: Ivan-Velickovic <i.velickovic@unsw.edu.au>

--- examples/virtio/meta.py
+++ examples/virtio/meta.py
@@ -20,6 +20,8 @@ class Board:
     paddr_top: int
     serial: str
     blk: str
+    net: str
+    guest_net: str
 
 BOARDS: List[Board] = [
     Board(
@@ -28,17 +30,19 @@ BOARDS: List[Board] = [
         paddr_top=0x6_0000_000,
         serial="pl011@9000000",
         blk="virtio_mmio@a003e00",
+        net="virtio_mmio@a003c00",
+        guest_net="virtio-net@0160000",
     ),
 ]
 
 def generate(sdf_file: str, output_dir: str, dtb: DeviceTree, client_dtb: DeviceTree):
     # Client 1
-    vmm_client1 = ProtectionDomain("CLIENT_VMM-1", "client_vmm.elf", priority=100)
-    vm_client1 = VirtualMachine("client_linux-1", [VirtualMachine.Vcpu(id=0)])
-    client1 = Vmm(sdf, vmm_client1, vm_client1, client_dtb)
-    sdf.add_pd(vmm_client1)
-    assert client1.connect()
-    assert client1.serialise_config(output_dir)
+    vmm_client0 = ProtectionDomain("CLIENT_VMM-1", "client_vmm.elf", priority=100)
+    vm_client0 = VirtualMachine("client_linux-1", [VirtualMachine.Vcpu(id=0)])
+    client0 = Vmm(sdf, vmm_client0, vm_client0, client_dtb)
+    sdf.add_pd(vmm_client0)
+    assert client0.connect()
+    assert client0.serialise_config(output_dir)
 
     # Client 2
     # vmm_client2 = ProtectionDomain("CLIENT_VMM-2", "client_vmm.elf", priority=100)
@@ -58,7 +62,7 @@ def generate(sdf_file: str, output_dir: str, dtb: DeviceTree, client_dtb: Device
 
     serial_system = Sddf.Serial(sdf, serial_node, serial_driver,
                                 serial_virt_tx, virt_rx=serial_virt_rx)
-    serial_system.add_client(vmm_client1)
+    serial_system.add_client(vmm_client0)
     # serial_system.add_client(vmm_client2)
 
     pds = [
@@ -69,6 +73,30 @@ def generate(sdf_file: str, output_dir: str, dtb: DeviceTree, client_dtb: Device
     for pd in pds:
         sdf.add_pd(pd)
 
+    # Net subsystem
+    net_node = dtb.node(board.net)
+    assert net_node is not None
+
+    eth_driver = ProtectionDomain("eth_driver", "eth_driver.elf", priority=101, budget=100, period=400)
+    net_virt_tx = ProtectionDomain("net_virt_tx", "network_virt_tx.elf", priority=100, budget=20000)
+    net_virt_rx = ProtectionDomain("net_virt_rx", "network_virt_rx.elf", priority=99)
+    net_system = Sddf.Net(sdf, net_node, eth_driver, net_virt_tx, net_virt_rx)
+    client0_net_copier = ProtectionDomain("client0_net_copier", "network_copy0.elf", priority=98, budget=20000)
+
+    pds = [
+        eth_driver,
+        net_virt_rx,
+        net_virt_tx,
+        client0_net_copier,
+    ]
+    for pd in pds:
+        sdf.add_pd(pd)
+
+    mac_random_part = random.randint(0, 0xfe)
+    client0_mac_addr = f"52:54:01:00:00:{hex(mac_random_part)[2:]:0>2}"
+
+    client0.add_virtio_mmio_net(client_dtb.node(board.guest_net), net_system, client0_net_copier, mac_addr=client0_mac_addr)
+
     # Block subsystem
     blk_driver = ProtectionDomain("blk_driver", "blk_driver.elf", priority=200)
     blk_virt = ProtectionDomain("blk_virt", "blk_virt.elf", priority=199, stack_size=0x2000)
@@ -77,7 +105,7 @@ def generate(sdf_file: str, output_dir: str, dtb: DeviceTree, client_dtb: Device
     assert blk_node is not None
 
     blk_system = Sddf.Blk(sdf, blk_node, blk_driver, blk_virt)
-    blk_system.add_client(vmm_client1, partition=0)
+    blk_system.add_client(vmm_client0, partition=0)
     # blk_system.add_client(vmm_client2, partition=1)
     pds = [
         blk_driver,
@@ -90,8 +118,9 @@ def generate(sdf_file: str, output_dir: str, dtb: DeviceTree, client_dtb: Device
     assert serial_system.serialise_config(output_dir)
     assert blk_system.connect()
     assert blk_system.serialise_config(output_dir)
+    assert net_system.connect()
+    assert net_system.serialise_config(output_dir)
 
-    # generate XML
     with open(f"{output_dir}/{sdf_file}", "w+") as f:
         f.write(sdf.render())
 
