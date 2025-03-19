b2dc883fde21427a98fe8bd313d141257dfc3a8a
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2025-03-19 12:16:53 +1100

    examples/virtio: transition to storing images outside of repo
    Co-authored-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>
    Signed-off-by: Ivan-Velickovic <i.velickovic@unsw.edu.au>

--- examples/virtio/virtio.mk
+++ examples/virtio/virtio.mk
@@ -6,6 +6,8 @@
 QEMU := qemu-system-aarch64
 PYTHON ?= python3
 
+LIBVMM_DOWNLOADS := https://trustworthy.systems/Downloads/libvmm/images/
+
 LIBVMM_TOOLS := $(LIBVMM)/tools
 MICROKIT_TOOL ?= $(MICROKIT_SDK)/bin/microkit
 SDDF_INCLUDE := $(SDDF)/include/sddf
@@ -124,9 +126,21 @@ $(IMAGE_FILE) $(REPORT_FILE): $(IMAGES) $(SYSTEM_FILE)
 vm_dir:
 	mkdir -p client_vm
 
-client_vm/rootfs.cpio.gz: $(CLIENT_VM)/rootfs.cpio.gz \
+${LINUX}:
+	curl -L ${LIBVMM_DOWNLOADS}/$(LINUX).tar.gz -o $(LINUX).tar.gz
+	mkdir -p linux_download_dir
+	tar -xf $@.tar.gz -C linux_download_dir
+	cp linux_download_dir/${LINUX}/linux ${LINUX}
+
+${INITRD}:
+	curl -L ${LIBVMM_DOWNLOADS}/$(INITRD).tar.gz -o $(INITRD).tar.gz
+	mkdir -p initrd_download_dir
+	tar xf $@.tar.gz -C initrd_download_dir
+	cp initrd_download_dir/${INITRD}/rootfs.cpio.gz ${INITRD}
+
+client_vm/rootfs.cpio.gz: ${INITRD} \
 	$(CLIENT_VM_USERLEVEL_INIT) |client_vm
-	$(LIBVMM)/tools/packrootfs $(CLIENT_VM)/rootfs.cpio.gz \
+	$(LIBVMM)/tools/packrootfs ${INITRD} \
 		client_vm/rootfs_staging -o $@ \
 		--startup $(CLIENT_VM_USERLEVEL_INIT)
 
@@ -144,9 +158,9 @@ client_vm/vmm.o: $(VIRTIO_EXAMPLE)/client_vmm.c $(CHECK_FLAGS_BOARD_MD5) |vm_dir
 	$(CC) $(CFLAGS) -c -o $@ $<
 
 client_vm/images.o: $(LIBVMM)/tools/package_guest_images.S $(CHECK_FLAGS_BOARD_MD5) \
-	$(CLIENT_VM)/linux client_vm/vm.dtb client_vm/rootfs.cpio.gz
+	${LINUX} client_vm/vm.dtb client_vm/rootfs.cpio.gz
 	$(CC) -c -g3 -x assembler-with-cpp \
-					-DGUEST_KERNEL_IMAGE_PATH=\"$(CLIENT_VM)/linux\" \
+					-DGUEST_KERNEL_IMAGE_PATH=\"${LINUX}\" \
 					-DGUEST_DTB_IMAGE_PATH=\"client_vm/vm.dtb\" \
 					-DGUEST_INITRD_IMAGE_PATH=\"client_vm/rootfs.cpio.gz\" \
 					-target $(TARGET) \
@@ -156,10 +170,7 @@ client_vmm.elf: client_vm/vmm.o client_vm/images.o |vm_dir
 	$(LD) $(LDFLAGS) $^ $(LIBS) -o $@
 
 # Stop make from deleting intermediate files
-.PRECIOUS: blk_driver_vm blk_driver_vm/vm.dts \
-	blk_driver_vm/vm.dtb blk_driver_vm/rootfs.cpio.gz \
-	blk_driver_vm/images.o blk_driver_vm/vmm.o \
-	client_vm client_vm/vm.dts client_vm/vm.dtb \
+.PRECIOUS: client_vm client_vm/vm.dts client_vm/vm.dtb \
 	client_vm/rootfs.cpio.gz client_vm/images.o client_vm/vmm.o
 
 qemu: $(IMAGE_FILE) blk_storage
