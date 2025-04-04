dd6c1964a32951f73fc35cc54882cd0263e50c8b
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2025-04-04 18:25:48 +1100

    virtio/socket: style + trailing spaces
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

--- src/virtio/socket.c
+++ src/virtio/socket.c
@@ -22,73 +22,78 @@
 
 #define LOG_VSOCK_ERR(...) do{ printf("%s|VIRTIO(VSOCK)|ERROR: ", microkit_name); printf(__VA_ARGS__); }while(0)
 
-static bool virtio_vsock_valid_cid(uint32_t cid) {
+static bool virtio_vsock_valid_cid(uint32_t cid)
+{
     // Page 159 of 282 VirtIO spec 1.2.
     switch (cid) {
-        case 0:
-        case 1:
-        case 2:
-        case 0xffffffff:
-            return false;
-        default:
-            return true;
+    case 0:
+    case 1:
+    case 2:
+    case 0xffffffff:
+        return false;
+    default:
+        return true;
     }
 }
 
-static void virtio_vsock_features_print(uint32_t features) {
+static void virtio_vsock_features_print(uint32_t features)
+{
     /* Dump the features given in a human-readable format */
     LOG_VSOCK("Dumping features (0x%lx):\n", features);
     LOG_VSOCK("feature VIRTIO_VSOCK_F_STREAM set to %s\n",
-             BIT_LOW(VIRTIO_VSOCK_F_STREAM) & features ? "true" : "false");
+              BIT_LOW(VIRTIO_VSOCK_F_STREAM) & features ? "true" : "false");
     LOG_VSOCK("feature VIRTIO_VSOCK_F_SEQPACKET set to %s\n",
-             BIT_LOW(VIRTIO_VSOCK_F_SEQPACKET) & features ? "true" : "false");
+              BIT_LOW(VIRTIO_VSOCK_F_SEQPACKET) & features ? "true" : "false");
 }
 
-static void virtio_vsock_reset(struct virtio_device *dev) {
+static void virtio_vsock_reset(struct virtio_device *dev)
+{
     LOG_VSOCK("operation: reset\n");
 
-    for (int i = 0; i < dev->num_vqs; i++){
+    for (int i = 0; i < dev->num_vqs; i++) {
         dev->vqs[i].ready = false;
         dev->vqs[i].last_idx = 0;
     }
 }
 
-static bool virtio_vsock_get_device_features(struct virtio_device *dev, uint32_t *features) {
+static bool virtio_vsock_get_device_features(struct virtio_device *dev, uint32_t *features)
+{
     LOG_VSOCK("operation: get device features\n");
 
     switch (dev->data.DeviceFeaturesSel) {
-        case 0:
-            *features = BIT_LOW(VIRTIO_VSOCK_F_STREAM);
-            break;
-        case 1:
-            *features = BIT_HIGH(VIRTIO_F_VERSION_1);
-            break;
-        default:
-            // @ivanv: audit
-            LOG_VSOCK_ERR("driver sets DeviceFeaturesSel to 0x%x, which doesn't make sense\n", dev->data.DeviceFeaturesSel);
-            return false;
+    case 0:
+        *features = BIT_LOW(VIRTIO_VSOCK_F_STREAM);
+        break;
+    case 1:
+        *features = BIT_HIGH(VIRTIO_F_VERSION_1);
+        break;
+    default:
+        // @ivanv: audit
+        LOG_VSOCK_ERR("driver sets DeviceFeaturesSel to 0x%x, which doesn't make sense\n", dev->data.DeviceFeaturesSel);
+        return false;
     }
 
     return true;
 }
 
-static bool virtio_vsock_set_driver_features(struct virtio_device *dev, uint32_t features) {
+static bool virtio_vsock_set_driver_features(struct virtio_device *dev, uint32_t features)
+{
     LOG_VSOCK("operation: set driver features\n");
     virtio_vsock_features_print(features);
 
     bool success = true;
 
     switch (dev->data.DriverFeaturesSel) {
-        // feature bits 0 to 31
-        case 0:
-            break;
-        // features bits 32 to 63
-        case 1:
-            success = (features == BIT_HIGH(VIRTIO_F_VERSION_1));
-            break;
-        default:
-            LOG_VSOCK_ERR("driver sets DriverFeaturesSel to 0x%x, which doesn't make sense\n", dev->data.DriverFeaturesSel);
-            return false;
+    // feature bits 0 to 31
+    case 0:
+        break;
+    // features bits 32 to 63
+    case 1:
+        success = (features == BIT_HIGH(VIRTIO_F_VERSION_1));
+        break;
+    default:
+        LOG_VSOCK_ERR("driver sets DriverFeaturesSel to 0x%x, which doesn't make sense\n", dev->data.DriverFeaturesSel);
+        return false;
     }
 
     if (success) {
@@ -99,33 +104,36 @@ static bool virtio_vsock_set_driver_features(struct virtio_device *dev, uint32_t
     return true;
 }
 
-static bool virtio_vsock_get_device_config(struct virtio_device *dev, uint32_t offset, uint32_t *config) {
+static bool virtio_vsock_get_device_config(struct virtio_device *dev, uint32_t offset, uint32_t *config)
+{
     LOG_VSOCK("operation: get device config at offset 0x%lx\n", offset - REG_VIRTIO_MMIO_CONFIG);
 
     struct virtio_vsock_device *vsock = (struct virtio_vsock_device *)dev->device_data;
 
     switch (offset) {
-        case REG_VIRTIO_MMIO_CONFIG:
-            *config = vsock->guest_cid;
-            break;
-        case REG_VIRTIO_MMIO_CONFIG + 0x4:
-            /* The upper 32-bits of the CID are reserved and zeroed. */
-            *config = 0;
-            break;
-        default:
-            LOG_VSOCK("get config at unknown register offset 0x%x\n", offset);
-            return false;
+    case REG_VIRTIO_MMIO_CONFIG:
+        *config = vsock->guest_cid;
+        break;
+    case REG_VIRTIO_MMIO_CONFIG + 0x4:
+        /* The upper 32-bits of the CID are reserved and zeroed. */
+        *config = 0;
+        break;
+    default:
+        LOG_VSOCK("get config at unknown register offset 0x%x\n", offset);
+        return false;
     }
 
     return true;
 }
 
-static bool virtio_vsock_set_device_config(struct virtio_device *dev, uint32_t offset, uint32_t config) {
+static bool virtio_vsock_set_device_config(struct virtio_device *dev, uint32_t offset, uint32_t config)
+{
     LOG_VSOCK("operation: set device config\n");
     return false;
 }
 
-static void virtio_vsock_handle_tx(struct virtio_device *dev) {
+static void virtio_vsock_handle_tx(struct virtio_device *dev)
+{
     struct virtio_vsock_device *vsock = (struct virtio_vsock_device *) dev->device_data;
     struct virtio_queue_handler *vq = &dev->vqs[VIRTIO_VSOCK_TX_QUEUE];
     struct virtio_vsock_recv_space *peer_buf = (struct virtio_vsock_recv_space *) vsock->buffer_peer;
@@ -164,30 +172,30 @@ static void virtio_vsock_handle_tx(struct virtio_device *dev) {
         assert(packet->hdr.src_cid == vsock->guest_cid);
 
         switch (packet->hdr.op) {
-            case VIRTIO_VSOCK_OP_REQUEST:
-            case VIRTIO_VSOCK_OP_RESPONSE:
-            case VIRTIO_VSOCK_OP_RST:
-            case VIRTIO_VSOCK_OP_SHUTDOWN:
-            case VIRTIO_VSOCK_OP_CREDIT_UPDATE:
-            case VIRTIO_VSOCK_OP_CREDIT_REQUEST:
-                /* These requests don't have any payloads. */
-                assert(packet->hdr.len == 0);
-                break;
-            case VIRTIO_VSOCK_OP_RW: {
-                /* Only this one have a payload because it actually sends data */
-                /* Sanity check that the payload does not exceed the device's buffer. Shouldn't
-                    trip as we've previously informed the other side of our real buffer size. */
-                assert(packet->hdr.len <= vsock->payload_size);
-                break;
-            }
-            default: {
-                LOG_VSOCK_ERR("invalid operation %d\n", packet->hdr.op);
-                // TODO: handle
-                // send reset as per spec
-                // 5.10.6.4.2
-                assert(false);
-                break;
-            }
+        case VIRTIO_VSOCK_OP_REQUEST:
+        case VIRTIO_VSOCK_OP_RESPONSE:
+        case VIRTIO_VSOCK_OP_RST:
+        case VIRTIO_VSOCK_OP_SHUTDOWN:
+        case VIRTIO_VSOCK_OP_CREDIT_UPDATE:
+        case VIRTIO_VSOCK_OP_CREDIT_REQUEST:
+            /* These requests don't have any payloads. */
+            assert(packet->hdr.len == 0);
+            break;
+        case VIRTIO_VSOCK_OP_RW: {
+            /* Only this one have a payload because it actually sends data */
+            /* Sanity check that the payload does not exceed the device's buffer. Shouldn't
+                trip as we've previously informed the other side of our real buffer size. */
+            assert(packet->hdr.len <= vsock->payload_size);
+            break;
+        }
+        default: {
+            LOG_VSOCK_ERR("invalid operation %d\n", packet->hdr.op);
+            // TODO: handle
+            // send reset as per spec
+            // 5.10.6.4.2
+            assert(false);
+            break;
+        }
         }
 
         /* Copy the head to the receiver's RX buffer. */
@@ -195,7 +203,7 @@ static void virtio_vsock_handle_tx(struct virtio_device *dev) {
 
         /* Then doctor the header to tell the receiver the size of our device receive buffer */
         peer_buf->packet.hdr.buf_alloc = vsock->payload_size;
-        
+
         /* Copy the payload if the request is a send */
         if (packet->hdr.op == VIRTIO_VSOCK_OP_RW) {
             uintptr_t payload = (uintptr_t) &packet->data;
@@ -231,7 +239,8 @@ static void virtio_vsock_handle_tx(struct virtio_device *dev) {
     }
 }
 
-bool virtio_vsock_handle_rx(struct virtio_vsock_device *vsock) {
+bool virtio_vsock_handle_rx(struct virtio_vsock_device *vsock)
+{
     struct virtio_device *dev = &vsock->virtio_device;
     struct virtio_vsock_recv_space *our_buf = (struct virtio_vsock_recv_space *) vsock->buffer_our;
     struct virtio_vsock_recv_space *peer_buf = (struct virtio_vsock_recv_space *) vsock->buffer_peer;
@@ -245,7 +254,7 @@ bool virtio_vsock_handle_rx(struct virtio_vsock_device *vsock) {
         /* Normal receive */
         struct virtio_queue_handler *vq = &dev->vqs[VIRTIO_VSOCK_RX_QUEUE];
         struct virtio_vsock_packet *packet = (struct virtio_vsock_packet *) &our_buf->packet;
-    
+
         LOG_VSOCK("======== RECEIVING ========\n");
         LOG_VSOCK("src_cid: 0x%lx\n", packet->hdr.src_cid);
         LOG_VSOCK("dst_cid: 0x%lx\n", packet->hdr.dst_cid);
@@ -257,12 +266,12 @@ bool virtio_vsock_handle_rx(struct virtio_vsock_device *vsock) {
         LOG_VSOCK("flags: 0x%lx\n", packet->hdr.flags);
         LOG_VSOCK("buf_alloc: 0x%lx\n", packet->hdr.buf_alloc);
         LOG_VSOCK("fwd_cnt: 0x%lx\n", packet->hdr.fwd_cnt);
-    
+
         /* Grab an available descriptor and copy over the packet's header */
         uint16_t desc_head = vq->virtq.avail->ring[vq->last_idx % vq->virtq.num];
         struct virtq_desc desc = vq->virtq.desc[desc_head];
         memcpy((void *) desc.addr, &packet->hdr, sizeof(struct virtio_vsock_hdr));
-    
+
         if (packet->hdr.op == VIRTIO_VSOCK_OP_RW) {
             void *payload_dest = (void *)((uintptr_t) desc.addr + sizeof(struct virtio_vsock_hdr));
             /* Some version of Linux likes to chain RX descriptor for zero-copy RX, we have to handle that
@@ -275,15 +284,15 @@ bool virtio_vsock_handle_rx(struct virtio_vsock_device *vsock) {
             }
             memcpy(payload_dest, packet->data, packet->hdr.len);
         }
-    
+
         struct virtq_used_elem used_hdr_elem = {desc_head, sizeof(struct virtio_vsock_hdr) + packet->hdr.len};
         vq->virtq.used->ring[vq->virtq.used->idx % vq->virtq.num] = used_hdr_elem;
         vq->virtq.used->idx++;
         vq->last_idx++;
-    
+
         /* Initiate to our peer that we are ready to accept the next packet. */
         our_buf->metadata.dirty = false;
-    
+
         /* The peer might have stopped sending because our buffer was dirty,
            or we might have stopped sending because the peer buffer was dirty
            check if we have anything to send and notify them that we are ready
@@ -303,7 +312,8 @@ bool virtio_vsock_handle_rx(struct virtio_vsock_device *vsock) {
     return true;
 }
 
-static bool virtio_vsock_handle_queue_notify(struct virtio_device *dev) {
+static bool virtio_vsock_handle_queue_notify(struct virtio_device *dev)
+{
     LOG_VSOCK("operation: handle queue_notify on %d\n", dev->data.QueueNotify);
 
     size_t vq_idx = dev->data.QueueNotify;
@@ -312,7 +322,9 @@ static bool virtio_vsock_handle_queue_notify(struct virtio_device *dev) {
         return false;
     }
 
-    if (vq_idx != VIRTIO_VSOCK_TX_QUEUE) return true;
+    if (vq_idx != VIRTIO_VSOCK_TX_QUEUE) {
+        return true;
+    }
     virtio_vsock_handle_tx(dev);
 
     /* As of virtIO v1.2, the event vq isn't important in our device implementation
@@ -361,7 +373,8 @@ bool virtio_mmio_vsock_init(struct virtio_vsock_device *vsock,
 
     vsock->guest_cid = guest_cid;
     vsock->shared_buffer_size = shared_buffer_size;
-    vsock->payload_size = shared_buffer_size - sizeof(struct virtio_vsock_recv_space_metadata) - sizeof(struct virtio_vsock_hdr);
+    vsock->payload_size = shared_buffer_size - sizeof(struct virtio_vsock_recv_space_metadata) - sizeof(
+                              struct virtio_vsock_hdr);
     vsock->buffer_our = buffer_our;
     vsock->buffer_peer = buffer_peer;
     vsock->peer_ch = peer_ch;
