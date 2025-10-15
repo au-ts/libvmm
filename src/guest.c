751b7788cfc3c4319a1cb1b38d8f35e84cd70796
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2025-10-15 20:09:59 +1100

    src/guest.c: initial vmenter loop
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

--- src/guest.c
+++ src/guest.c
@@ -17,14 +17,13 @@ bool guest_start(uintptr_t kernel_pc, uintptr_t dtb, uintptr_t initrd)
      * any other kind of guest. However, even though the library is open to supporting other
      * guests, there is no point in prematurely generalising this code.
      */
-    seL4_UserContext regs = {0};
 #if defined(CONFIG_ARCH_AARCH64)
+    seL4_UserContext regs = {0};
     regs.x0 = dtb;
     regs.spsr = 5; // PMODE_EL1h
     regs.pc = kernel_pc;
 #elif defined(CONFIG_ARCH_X86_64)
-    regs.rip = kernel_pc;
-    regs.rsp = 0x99000;
+
 #else
 #error "Unsupported guest architecture"
 #endif
@@ -33,6 +32,7 @@ bool guest_start(uintptr_t kernel_pc, uintptr_t dtb, uintptr_t initrd)
     // @billn the following is probably not correct for x86?
     // should just be seL4_VMEnter() revisit
 
+#if defined(CONFIG_ARCH_AARCH64)
     seL4_Word err = seL4_TCB_WriteRegisters(
         BASE_VM_TCB_CAP + GUEST_BOOT_VCPU_ID,
         false, // We'll explcitly start the guest below rather than in this call
@@ -51,6 +51,48 @@ bool guest_start(uintptr_t kernel_pc, uintptr_t dtb, uintptr_t initrd)
     // vcpu_set_on(GUEST_BOOT_VCPU_ID, true);
     /* Restart the boot vCPU to the program counter of the TCB associated with it */
     microkit_vcpu_restart(GUEST_BOOT_VCPU_ID, kernel_pc);
+
+#elif defined(CONFIG_ARCH_X86_64)
+    LOG_VMM("starting guest at 0x%lx, DTB at 0x%lx, initial RAM disk at 0x%lx\n", kernel_pc, dtb, initrd);
+
+    // @billn explain
+    // SEL4_VMENTER_CALL_EIP_MR
+    microkit_mr_set(0, kernel_pc);
+    // SEL4_VMENTER_CALL_CONTROL_PPC_MR
+    microkit_mr_set(1, (1u<<31) | (1u<<28) | (1u<<25) | (1u<<23) | (1u<<7) | (1u<<3));
+    // SEL4_VMENTER_CALL_CONTROL_ENTRY_MR
+    microkit_mr_set(2, (1u<<9) | (1u<<14) | (1u<<15));
+
+    while (true) {
+        seL4_Word badge;
+        seL4_Word ret = seL4_VMEnter(&badge);
+
+        if (ret == 0) {
+            LOG_VMM("notif\n");
+        } else if (ret == 1) {
+            LOG_VMM("fault\n");
+            seL4_Word rip = microkit_mr_get(0);
+            LOG_VMM("ip = 0x%x\n", rip);
+            seL4_Word cppc = microkit_mr_get(1); // SEL4_VMENTER_CALL_CONTROL_PPC_MR
+            LOG_VMM("vm exec control = 0x%x\n", cppc);
+            seL4_Word vmec = microkit_mr_get(2); // SEL4_VMENTER_CALL_CONTROL_ENTRY_MR
+            LOG_VMM("vm entry control = 0x%x\n", vmec);
+            seL4_Word f_reason = microkit_mr_get(3); // SEL4_VMENTER_FAULT_REASON_MR
+            LOG_VMM("fault reason = 0x%x\n", f_reason);
+            seL4_Word f_qual = microkit_mr_get(4); // SEL4_VMENTER_FAULT_QUALIFICATION_MR
+            // reasons: /Users/dreamliner787-9/TS/microkit-capdl-dev/seL4/include/arch/x86/arch/object/vcpu.h
+            LOG_VMM("fault qualification = 0x%x\n", f_qual);
+            seL4_Word ins_len = microkit_mr_get(5); // SEL4_VMENTER_FAULT_INSTRUCTION_LEN_MR
+            LOG_VMM("instruction length = 0x%x\n", ins_len);
+            seL4_Word g_p_addr = microkit_mr_get(6); // SEL4_VMENTER_FAULT_GUEST_PHYSICAL_MR
+            LOG_VMM("guest physical addr = 0x%x\n", g_p_addr);
+        }
+
+        // @billn, doing a vmenter after a fault without handling the fault first causes seL4 to crash. issue?
+        break;
+    }
+    LOG_VMM("done\n");
+#endif
 
     return true;
 }
