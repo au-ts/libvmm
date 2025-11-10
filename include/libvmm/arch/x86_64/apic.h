124509c3bcfcdd981a708af3cdb8d5c239025ca3
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2025-11-10 20:18:26 +1100

    checkpoint: Linux now sees I/O APIC and correct number of GSIs, faulting on some I/O Ports now
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

--- include/libvmm/arch/x86_64/apic.h
+++ include/libvmm/arch/x86_64/apic.h
@@ -11,3 +11,4 @@
 #include <libvmm/arch/x86_64/instruction.h>
 
 bool lapic_fault_handle(seL4_VCPUContext *vctx, uint64_t offset, seL4_Word qualification, memory_instruction_data_t decoded_mem_ins);
+bool ioapic_fault_handle(seL4_VCPUContext *vctx, uint64_t offset, seL4_Word qualification, memory_instruction_data_t decoded_mem_ins);
