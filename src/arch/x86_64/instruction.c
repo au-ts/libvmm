3164075b98128a9fbd4a91d6166b880abfed0d50
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2025-12-03 15:41:41 +1100

    fix more bugs in instruction parsing, add 64-bit operand detection
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

--- src/arch/x86_64/instruction.c
+++ src/arch/x86_64/instruction.c
@@ -39,7 +39,12 @@ seL4_Word pte_to_gpa(seL4_Word pte)
     return pte & 0xffffffffff000;
 }
 
-bool pt_page_present(seL4_Word pte)
+bool pte_present(seL4_Word pte)
+{
+    return pte & BIT(0);
+}
+
+bool pt_page_size(seL4_Word pte)
 {
     return pte & BIT(7);
 }
@@ -81,7 +86,7 @@ static register_idx_t modrm_reg_to_vctx_idx(uint8_t reg, bool rex_r)
     case 15:
         return R15_IDX;
     default:
-        LOG_VMM_ERR("unknown register idx: 0x%x, reg: 0x%x, REX.W: %d\n", idx, reg, rex_r);
+        LOG_VMM_ERR("unknown register idx: 0x%x, reg: 0x%x, REX.R: %d\n", idx, reg, rex_r);
         assert(false);
     }
 }
@@ -93,19 +98,34 @@ decoded_instruction_ret_t decode_instruction(size_t vcpu_id, seL4_Word rip, seL4
     seL4_Word pml4_gpa = microkit_vcpu_x86_read_vmcs(vcpu_id, VMX_GUEST_CR3) & ~0xfff;
     seL4_Word *pml4 = gpa_to_vaddr(pml4_gpa);
     seL4_Word pml4_idx = (rip >> (12 + (9 * 3))) & 0x1ff;
+    seL4_Word pml4_pte = pml4[pml4_idx];
+    if (!pte_present(pml4_pte)) {
+        LOG_VMM_ERR("PML4 PTE not present when decoding instruction at 0x%lx\n", rip);
+        assert(false);
+    }
 
-    seL4_Word pdpt_gpa = pte_to_gpa(pml4[pml4_idx]);
+    seL4_Word pdpt_gpa = pte_to_gpa(pml4_pte);
     uint64_t *pdpt = gpa_to_vaddr(pdpt_gpa);
     seL4_Word pdpt_idx = (rip >> (12 + (9 * 2))) & 0x1ff;
+    uint64_t pdpt_pte = pdpt[pdpt_idx];
+    if (!pte_present(pdpt_pte)) {
+        LOG_VMM_ERR("PDPT PTE not present when decoding instruction at 0x%lx\n", rip);
+        assert(false);
+    }
 
-    seL4_Word pd_gpa = pte_to_gpa(pdpt[pdpt_idx]);
+    seL4_Word pd_gpa = pte_to_gpa(pdpt_pte);
     uint64_t *pd = gpa_to_vaddr(pd_gpa);
     seL4_Word pd_idx = (rip >> (12 + (9 * 1))) & 0x1ff;
+    seL4_Word pd_pte = pd[pd_idx];
+    if (!pte_present(pd_pte)) {
+        LOG_VMM_ERR("PD PTE not present when decoding instruction at 0x%lx\n", rip);
+        assert(false);
+    }
 
     uint64_t *page;
-    if (pt_page_present(pd[pd_idx] & BIT(7))) {
+    if (pt_page_size(pd_pte) && pte_present(pd_pte)) {
         // 2MiB page
-        seL4_Word page_gpa = pte_to_gpa((pd[pd_idx])) | (rip & 0x1fffff);
+        seL4_Word page_gpa = pte_to_gpa(pd_pte) | (rip & 0x1fffff);
         page = gpa_to_vaddr(page_gpa);
 
         // @billn todo
@@ -113,11 +133,16 @@ decoded_instruction_ret_t decode_instruction(size_t vcpu_id, seL4_Word rip, seL4
         assert(rip + X86_MAX_INSTRUCTION_LENGTH <= ROUND_UP(rip, 0x200000));
     } else {
         // 4k page
-        seL4_Word pt_gpa = pte_to_gpa(pd[pd_idx]);
+        seL4_Word pt_gpa = pte_to_gpa(pd_pte);
         uint64_t *pt = gpa_to_vaddr(pt_gpa);
         seL4_Word pt_idx = (rip >> (12)) & 0x1ff;
+        seL4_Word pt_pte = pt[pt_idx];
+        if (!pte_present(pt_pte)) {
+            LOG_VMM_ERR("PT PTE not present when decoding instruction at 0x%lx\n", rip);
+            assert(false);
+        }
 
-        seL4_Word page_gpa = pte_to_gpa((pt[pt_idx])) | (rip & 0xfff);
+        seL4_Word page_gpa = pte_to_gpa(pt_pte) | (rip & 0xfff);
         page = gpa_to_vaddr(page_gpa);
 
         // @billn todo
@@ -140,21 +165,20 @@ decoded_instruction_ret_t decode_instruction(size_t vcpu_id, seL4_Word rip, seL4
 
     bool opcode_valid = false;
     int parsed_byte = 0;
+    bool rex_w = false; // 64-bit operand size
     bool rex_r = false; // 4-bit operand, rather than 3-bit
+    bool rex_x = false; // 4-bit SIB.index
+    bool rex_b = false; // 4-bit MODRM.rm field or the SIB.base field
 
     // scan for REX byte, which is always 0b0100WRXB
     if (instruction_buf[0] >> 4 == 4) {
         uint8_t rex_byte = instruction_buf[parsed_byte];
         parsed_byte += 1;
-        switch (rex_byte & 0xf) {
-        case 4:
-            rex_r = true;
-            break;
 
-        default:
-            LOG_VMM_ERR("unimplemented REX prefix parsing: 0x%x\n", rex_byte);
-            break;
-        }
+        rex_w = (rex_byte & BIT(3)) != 0;
+        rex_r = (rex_byte & BIT(2)) != 0;
+        rex_x = (rex_byte & BIT(1)) != 0;
+        rex_b = (rex_byte & BIT(0)) != 0;
     }
 
     // match the opcode against a list of known opcodes that we provide decoding logic for.
@@ -171,18 +195,21 @@ decoded_instruction_ret_t decode_instruction(size_t vcpu_id, seL4_Word rip, seL4
             // uint8_t rm = modrm & 0x7;
 
             ret.type = INSTRUCTION_MEMORY;
-            switch (opcode) {
-            case OPCODE_MOV_BYTE_TO_MEM:
-            case OPCODE_MOV_BYTE_FROM_MEM:
-                ret.decoded.memory_instruction.access_width = BYTE_ACCESS_WIDTH;
-                break;
-            case OPCODE_MOV_WORD_TO_MEM:
-            case OPCODE_MOV_WORD_FROM_MEM:
-                // @billn revisit 16-byte access decoding
-                ret.decoded.memory_instruction.access_width = DWORD_ACCESS_WIDTH;
-                break;
+            if (rex_w) {
+                ret.decoded.memory_instruction.access_width = QWORD_ACCESS_WIDTH;
+            } else {
+                switch (opcode) {
+                case OPCODE_MOV_BYTE_TO_MEM:
+                case OPCODE_MOV_BYTE_FROM_MEM:
+                    ret.decoded.memory_instruction.access_width = BYTE_ACCESS_WIDTH;
+                    break;
+                case OPCODE_MOV_WORD_TO_MEM:
+                case OPCODE_MOV_WORD_FROM_MEM:
+                    // @billn revisit 16-byte access decoding
+                    ret.decoded.memory_instruction.access_width = DWORD_ACCESS_WIDTH;
+                    break;
+                }
             }
-
             ret.decoded.memory_instruction.target_reg = modrm_reg_to_vctx_idx(reg, rex_r);
 
             break;
