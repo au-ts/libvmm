306b169397e2a6da397521321647ee887563d6d2
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2025-12-09 13:15:04 +1100

    expose PCI ISA bridge
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

--- src/arch/x86_64/pci.c
+++ src/arch/x86_64/pci.c
@@ -22,41 +22,45 @@
 #define LOG_PCI(...) do{}while(0)
 #endif
 
-struct pci_host_bridge {
+struct pci {
     uint32_t address_reg;
 
-    struct pci_config_space self_config_space;
+    struct pci_config_space host_bridge;
+    struct pci_config_space isa_bridge;
 };
 
-static struct pci_host_bridge pci_host_bridge = { .self_config_space = { .device_id = 0x1234,
-                                                                         .vendor_id = 0x5e14,
-                                                                         .class_code = 0x6,
-                                                                         .subclass = 0,
-                                                                         .header_type = 0 } };
+static struct pci pci_state = {
+    .host_bridge = { .device_id = 0x1237, .vendor_id = 0x8086, .class_code = 0x6, .subclass = 0, .header_type = 0 },
+    .isa_bridge = { .device_id = 0x7000,
+                    .vendor_id = 0x8086,
+                    .class_code = 0x6,
+                    .subclass = 1,
+                    .header_type = 0, },
+};
 
 static bool pci_host_bridge_addr_reg_enable(void)
 {
-    return !!(pci_host_bridge.address_reg >> 31);
+    return !!(pci_state.address_reg >> 31);
 }
 
 static uint8_t pci_host_bridge_addr_reg_bus(void)
 {
-    return (pci_host_bridge.address_reg >> 16) & 0x7f;
+    return (pci_state.address_reg >> 16) & 0x7f;
 }
 
 static uint8_t pci_host_bridge_addr_reg_dev(void)
 {
-    return (pci_host_bridge.address_reg >> 11) & 0x1f;
+    return (pci_state.address_reg >> 11) & 0x1f;
 }
 
 static uint8_t pci_host_bridge_addr_reg_func(void)
 {
-    return (pci_host_bridge.address_reg >> 8) & 0x7;
+    return (pci_state.address_reg >> 8) & 0x7;
 }
 
 static uint8_t pci_host_bridge_addr_reg_offset(void)
 {
-    return pci_host_bridge.address_reg & 0xff;
+    return pci_state.address_reg & 0xff;
 }
 
 static void pci_host_bridge_invalid_read(seL4_VCPUContext *vctx)
@@ -78,30 +82,39 @@ bool is_pci_config_space_access_mech_1(uint16_t port_addr)
 
 static bool is_host_bridge_access(void)
 {
-    return pci_host_bridge_addr_reg_enable() && pci_host_bridge_addr_reg_bus() == 0 && pci_host_bridge_addr_reg_dev() == 0
-        && pci_host_bridge_addr_reg_func() == 0;
+    return pci_host_bridge_addr_reg_enable() && pci_host_bridge_addr_reg_bus() == 0
+        && pci_host_bridge_addr_reg_dev() == 0 && pci_host_bridge_addr_reg_func() == 0;
 }
 
-bool emulate_pci_host_bridge_access(seL4_VCPUContext *vctx, uint16_t port_addr, bool is_read,
-                                    ioport_access_width_t access_width)
+static bool is_isa_bridge_access(void)
+{
+    return pci_host_bridge_addr_reg_enable() && pci_host_bridge_addr_reg_bus() == 0
+        && pci_host_bridge_addr_reg_dev() == 1 && pci_host_bridge_addr_reg_func() == 0;
+}
+
+bool emulate_pci_config_space_access_pio(seL4_VCPUContext *vctx, uint16_t port_addr, bool is_read,
+                                         ioport_access_width_t access_width, struct pci_config_space *config_space)
 {
     bool success = true;
+
+    // @billn doesn't support write yet
     if (!is_read) {
         return success;
     }
 
     uint64_t *vctx_raw = (uint64_t *)vctx;
+    uint8_t *config_bytes = (uint8_t *)config_space;
     int port_offset = port_addr - PCI_CONFIG_DATA_START_PORT;
 
+    // caller must catch!
     assert(pci_host_bridge_addr_reg_enable());
 
-    uint8_t *config_space_bytes = (uint8_t *)(&pci_host_bridge.self_config_space);
     int bytes_to_copy = ioports_access_width_to_bytes(access_width);
     assert(bytes_to_copy);
+    memcpy(&vctx_raw[RAX_IDX], &config_bytes[pci_host_bridge_addr_reg_offset() + port_offset], bytes_to_copy);
 
-    memcpy(&vctx_raw[RAX_IDX], &config_space_bytes[pci_host_bridge_addr_reg_offset() + port_offset], bytes_to_copy);
-
-    LOG_PCI("read host bridge, offset 0x%x, val 0x%x, read width: %d bytes\n", pci_host_bridge_addr_reg_offset() + port_offset, vctx_raw[RAX_IDX], bytes_to_copy);
+    LOG_PCI("read host bridge, offset 0x%x, val 0x%x, read width: %d bytes\n",
+            pci_host_bridge_addr_reg_offset() + port_offset, vctx_raw[RAX_IDX], bytes_to_copy);
 
     return success;
 }
@@ -122,12 +135,12 @@ bool emulate_pci_config_space_access_mech_1(seL4_VCPUContext *vctx, uint16_t por
     if (port_addr >= PCI_CONFIG_ADDRESS_START_PORT && port_addr < PCI_CONFIG_ADDRESS_END_PORT) {
         if (is_read) {
             assert(port_addr == PCI_CONFIG_ADDRESS_START_PORT);
-            vctx_raw[RAX_IDX] = pci_host_bridge.address_reg;
+            vctx_raw[RAX_IDX] = pci_state.address_reg;
         } else {
             uint32_t value = vctx_raw[RAX_IDX];
             if (value >> 31) {
                 assert(port_addr == PCI_CONFIG_ADDRESS_START_PORT);
-                pci_host_bridge.address_reg = value;
+                pci_state.address_reg = value;
 
                 LOG_PCI("selecting bus %d, device %d, func %d, reg_offset 0x%x\n", pci_host_bridge_addr_reg_bus(),
                         pci_host_bridge_addr_reg_dev(), pci_host_bridge_addr_reg_func(),
@@ -140,7 +153,11 @@ bool emulate_pci_config_space_access_mech_1(seL4_VCPUContext *vctx, uint16_t por
             pci_host_bridge_invalid_read(vctx);
         } else {
             if (is_host_bridge_access()) {
-                success = emulate_pci_host_bridge_access(vctx, port_addr, is_read, access_width);
+                success = emulate_pci_config_space_access_pio(vctx, port_addr, is_read, access_width,
+                                                              &pci_state.host_bridge);
+            } else if (is_isa_bridge_access()) {
+                success = emulate_pci_config_space_access_pio(vctx, port_addr, is_read, access_width,
+                                                              &pci_state.isa_bridge);
             } else {
                 if (is_read) {
                     pci_host_bridge_invalid_read(vctx);
