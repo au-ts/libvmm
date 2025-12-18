ec9214f32739dec3964afe7a6de470870678bb0a
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2025-12-18 13:04:05 +1100

    explain cr4 mask
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

--- src/arch/x86_64/vcpu.c
+++ src/arch/x86_64/vcpu.c
@@ -42,6 +42,8 @@ void vcpu_set_up_long_mode(uint64_t cr3, uint64_t gdt_gpa, uint64_t gdt_limit) {
 
     // @billn explain
     // @billn todo add other important bits
+
+    // prevent the guest from turning of VMX mode
     microkit_vcpu_x86_write_vmcs(GUEST_BOOT_VCPU_ID, VMX_CONTROL_CR4_MASK, 1 << 13);
     // @billn todo add other registers with mask
 
