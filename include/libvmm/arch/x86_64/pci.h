13749e3c9077c1af050a3f9f609546183a01ebe7
Ivan Velickovic <i.velickovic@unsw.edu.au>
2026-01-06 12:29:50 +1100

    start emulating qemu fw-cfg
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- include/libvmm/arch/x86_64/pci.h
+++ include/libvmm/arch/x86_64/pci.h
@@ -7,6 +7,8 @@
 #include <stdbool.h>
 #include <libvmm/util/util.h>
 #include <sddf/util/util.h>
+// TODO: need pci_config_space from virtio pci.h, should probably extract into common header
+#include <libvmm/virtio/pci.h>
 #include <libvmm/arch/x86_64/ioports.h>
 
 #define PCI_CONFIG_ADDRESS_START_PORT 0xCF8
@@ -14,6 +16,33 @@
 
 #define PCI_CONFIG_DATA_START_PORT 0xCFC
 #define PCI_CONFIG_DATA_END_PORT (PCI_CONFIG_DATA_START_PORT + 4)
+
+struct pci_device_address {
+    uint8_t dev;
+    uint8_t bus;
+    uint8_t func;
+};
+
+// function 3 of isa bridge
+// https://www.intel.com/Assets/PDF/datasheet/290562.pdf
+#define PMBA_OFFSET 0x40
+#define PMREGMISC_OFFSET 0x80
+
+struct isa_bridge_power_mgmt_regs {
+    uint32_t pmba;
+    uint8_t pmregmisc;
+};
+
+struct pci_bus {
+    uint32_t address_reg;
+
+    struct pci_config_space host_bridge;
+    struct pci_config_space isa_bridge;
+    struct isa_bridge_power_mgmt_regs isa_bridge_power_mgmt_regs;
+
+    bool ide_controller_enable;
+    struct pci_device_address native_ide_con_addr;
+};
 
 bool is_pci_config_space_access_mech_1(uint16_t port_addr);
 bool emulate_pci_config_space_access_mech_1(seL4_VCPUContext *vctx, uint16_t port_addr, bool is_read,
