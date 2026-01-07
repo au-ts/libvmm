00d0abebad2f9dd7f8a7da2cc0a1fd4bc0cb44e5
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2026-01-07 16:26:24 +1100

    port qemu's bios linker loader command making facility
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

--- include/libvmm/arch/x86_64/qemu/README.md
+++ include/libvmm/arch/x86_64/qemu/README.md
@@ -2,4 +2,4 @@ This folder contains files copied from QEMU's source tree v9.1.1.
 
 - `bios_linker_loader.h`:
 Source path: `hw/acpi/bios-linker-loader.c`
-Purpose: Data structure for entries in the `etc/table-loader` fw cfg file.
+Purpose: Data structure and creating functions for entries in the `etc/table-loader` fw cfg file.
--- src/arch/x86_64/uefi.c
+++ src/arch/x86_64/uefi.c
@@ -17,17 +17,43 @@ extern struct fw_cfg_blobs fw_cfg_blobs;
 bool uefi_setup_images(uintptr_t ram_start, size_t ram_size, uintptr_t flash_start, size_t flash_size, char *firm,
                        size_t firm_size, void *dsdt_blob, uint64_t dsdt_blob_size)
 {
-    /* Place the UEFI firmware at the reset vector.
-     * Assumes that (GPA of flash_start) + flash_size == 0x1_0000_0000!!!!!! */
+    // Place the UEFI firmware at the reset vector.
+    // Assumes that (GPA of flash_start) + flash_size == 0x1_0000_0000!!!!!!
     memcpy((void *)(flash_start + (flash_size - firm_size)), firm, firm_size);
 
-    /* Build the various fw cfg blobs that will be needed by TianoCore OVMF */
-
+    // Build the various fw cfg blobs that will be needed by TianoCore OVMF
+    // First thing is the E820 table.
     fw_cfg_blobs.fw_cfg_e820_map.entries[0].addr = 0;
     fw_cfg_blobs.fw_cfg_e820_map.entries[0].size = ram_size;
     fw_cfg_blobs.fw_cfg_e820_map.entries[0].type = E820_RAM;
 
-    /* Finish by populating File Dir with everything we built */
+    // Then the ACPI XSDP table
+    // The GPA of the XSDT will be filled in by a table loader command later
+    xsdp_build(&fw_cfg_blobs.fw_xsdp, 0);
+
+    // Then the rest of the ACPI tables, all GPAs will be zero
+    madt_build(&fw_cfg_blobs.fw_acpi_tables.madt);
+    hpet_build(&fw_cfg_blobs.fw_acpi_tables.hpet);
+    fadt_build(&fw_cfg_blobs.fw_acpi_tables.fadt, 0);
+    uint64_t dummy_gpas[XSDT_ENTRIES] = { 0 };
+    xsdt_build(&fw_cfg_blobs.fw_acpi_tables.xsdt, dummy_gpas, XSDT_ENTRIES);
+
+    if (dsdt_blob_size > DSDT_MAX_SIZE) {
+        LOG_VMM_ERR("DSDT blob size is greater than max size in fw cfg (%d > %d), please increase DSDT_MAX_SIZE\n",
+                    dsdt_blob_size, DSDT_MAX_SIZE);
+        return false;
+    }
+    memcpy(&fw_cfg_blobs.fw_acpi_tables.dsdt, dsdt_blob, dsdt_blob_size);
+
+    // Now build the list of table loader commands, which:
+    // 1. Load all the tables into memory.
+    // 2. Patch all the required GPAs.
+    // 3. Recompute all table checksums
+    int num_cmd = 0;
+
+    
+
+    // Finish by populating File Dir with everything we built
     fw_cfg_blobs.fw_cfg_file_dir =
         (struct fw_cfg_file_dir) { .num_files = __builtin_bswap32(NUM_FW_CFG_FILES),
                                    .file_entries[0] = {
