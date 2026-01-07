744e8c2c5f9e6281b45a461bea935d6f32dfc40d
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2026-01-07 15:21:41 +1100

    move fw cfg blob making logic to uefi.c
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

--- include/libvmm/arch/x86_64/qemu_fw_cfg.h
+++ include/libvmm/arch/x86_64/qemu_fw_cfg.h
@@ -12,6 +12,54 @@
 #include <libvmm/arch/x86_64/ioports.h>
 #include <libvmm/arch/x86_64/qemu/bios_linker_loader.h>
 
+/* selector key values for "well-known" fw_cfg entries */
+#define FW_CFG_SIGNATURE        0x00
+#define FW_CFG_ID               0x01
+#define FW_CFG_UUID             0x02
+#define FW_CFG_RAM_SIZE         0x03
+#define FW_CFG_NOGRAPHIC        0x04
+#define FW_CFG_NB_CPUS          0x05
+#define FW_CFG_MACHINE_ID       0x06
+#define FW_CFG_KERNEL_ADDR      0x07
+#define FW_CFG_KERNEL_SIZE      0x08
+#define FW_CFG_KERNEL_CMDLINE   0x09
+#define FW_CFG_INITRD_ADDR      0x0a
+#define FW_CFG_INITRD_SIZE      0x0b
+#define FW_CFG_BOOT_DEVICE      0x0c
+#define FW_CFG_NUMA             0x0d
+#define FW_CFG_BOOT_MENU        0x0e
+#define FW_CFG_MAX_CPUS         0x0f
+#define FW_CFG_KERNEL_ENTRY     0x10
+#define FW_CFG_KERNEL_DATA      0x11
+#define FW_CFG_INITRD_DATA      0x12
+#define FW_CFG_CMDLINE_ADDR     0x13
+#define FW_CFG_CMDLINE_SIZE     0x14
+#define FW_CFG_CMDLINE_DATA     0x15
+#define FW_CFG_SETUP_ADDR       0x16
+#define FW_CFG_SETUP_SIZE       0x17
+#define FW_CFG_SETUP_DATA       0x18
+#define FW_CFG_FILE_DIR         0x19
+
+#define FW_CFG_E820        0x20
+
+/* an individual file entry, 64 bytes total */
+struct FWCfgFile {
+    /* size of referenced fw_cfg item, big-endian */
+    uint32_t size;
+    /* selector key of fw_cfg item, big-endian */
+    uint16_t select;
+    uint16_t reserved;
+    /* fw_cfg item name, NUL-terminated ascii */
+    char name[56];
+}  __attribute__((packed));
+
+/* Structure of FW_CFG_FILE_DIR */
+#define NUM_FW_CFG_FILES 5
+struct fw_cfg_file_dir {
+    uint32_t num_files; // Big endian!
+    struct FWCfgFile file_entries[NUM_FW_CFG_FILES];
+};
+
 #define NUM_FW_E820_ENTRIES 1
 struct fw_cfg_e820_map {
     struct boot_e820_entry entries[NUM_FW_E820_ENTRIES];
@@ -28,9 +76,15 @@ struct fw_cfg_acpi_tables {
 } __attribute__((packed));
 
 #define NUM_BIOS_LINKER_LOADER_CMD 16
+#define E820_FWCFG_FILE "etc/e820"
+#define ACPI_BUILD_TABLE_FILE "etc/acpi/tables"
+#define ACPI_BUILD_RSDP_FILE "etc/acpi/rsdp"
+#define ACPI_BUILD_LOADER_FILE "etc/table-loader"
 
 // Useful data that will be served to the guest firmware via the fw cfg interface.
 struct fw_cfg_blobs {
+    // File Dir
+    struct fw_cfg_file_dir fw_cfg_file_dir;
     // "etc/e820"
     struct fw_cfg_e820_map fw_cfg_e820_map;
     // "etc/acpi/tables"
