9f0d553c8b6a2da7620dc2173a548e693af9129a
Ivan Velickovic <i.velickovic@unsw.edu.au>
2026-01-08 14:55:45 +1100

    ramfb dma fixes

--- include/libvmm/arch/x86_64/qemu/ramfb.h
+++ include/libvmm/arch/x86_64/qemu/ramfb.h
@@ -2,6 +2,11 @@
 
 #include <stdint.h>
 
+/* Pixel format types. */
+#define FORMAT_RGB888 875710290 /**< 24 bits, red=8, green=8, blue=8 */
+#define FORMAT_XRGB8888 875713112 /**< 32 bits, alpha (transparency)=8, red=8, green=8, blue=8 */
+#define FORMAT_RGB565 909199186 /**< 16 bits, red=5, green=6, blue=5 */
+
 struct QemuRamFBCfg {
     uint64_t address;   /**< The address of the framebuffer. */
     uint32_t fourcc;    /**< The four character code representing the pixel format (use FORMAT_ defines). */
