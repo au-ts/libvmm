c607ae40bb66d8683f437cea43069dec36f0a42f
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2026-01-08 14:43:32 +1100

    properly emulate COM2, which seems to be mirroring COM1 output by uefi
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

--- examples/uefi/vmm_x86_64.c
+++ examples/uefi/vmm_x86_64.c
@@ -81,7 +81,6 @@ void init(void)
 
     /* Pass through COM1 serial port and IDE disk controller */
     microkit_vcpu_x86_enable_ioport(GUEST_BOOT_VCPU_ID, com1_ioport_id, com1_ioport_addr, com1_ioport_size);
-    microkit_vcpu_x86_enable_ioport(GUEST_BOOT_VCPU_ID, com2_ioport_id, com2_ioport_addr, com2_ioport_size);
     passthrough_ide_controller(primary_ata_cmd_pio_id, primary_ata_cmd_pio_addr, primary_ata_ctrl_pio_id,
                                primary_ata_ctrl_pio_addr, second_ata_cmd_pio_id, second_ata_cmd_pio_addr,
                                second_ata_ctrl_pio_id, second_ata_ctrl_pio_addr);
