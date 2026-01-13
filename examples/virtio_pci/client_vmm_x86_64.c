335e62f62ed3c2cf915aca8842c03ffe7bdbd7de
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2026-01-13 13:47:40 +1100

    temporarily disable net and block in virtio PCI example, make example work on both arm and x86
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

---
+++ examples/virtio_pci/client_vmm_x86_64.c
@@ -0,0 +1,177 @@
+/*
+ * Copyright 2025, UNSW
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <microkit.h>
+#include <libvmm/guest.h>
+#include <libvmm/config.h>
+#include <libvmm/util/util.h>
+#include <sddf/timer/client.h>
+#include <sddf/blk/config.h>
+#include <sddf/timer/config.h>
+#include <sddf/serial/config.h>
+#include <sddf/network/config.h>
+
+#include <libvmm/arch/x86_64/linux.h>
+#include <libvmm/arch/x86_64/fault.h>
+#include <libvmm/arch/x86_64/apic.h>
+#include <libvmm/arch/x86_64/hpet.h>
+#include <libvmm/arch/x86_64/pci.h>
+#include <libvmm/arch/x86_64/pit.h>
+#include <libvmm/arch/x86_64/virq.h>
+#include <libvmm/arch/x86_64/vcpu.h>
+
+__attribute__((__section__(".serial_client_config"))) serial_client_config_t serial_config;
+__attribute__((__section__(".blk_client_config"))) blk_client_config_t blk_config;
+__attribute__((__section__(".net_client_config"))) net_client_config_t net_config;
+__attribute__((__section__(".vmm_config"))) vmm_config_t vmm_config;
+
+// @billn sus, use package asm script
+#include "client_vm/x86_64/simple_dsdt.hex"
+
+uint64_t com1_ioport_id;
+uint64_t com1_ioport_addr;
+uint64_t com1_ioport_size = 8;
+
+uint64_t primary_ata_cmd_pio_id;
+uint64_t primary_ata_cmd_pio_addr;
+
+uint64_t primary_ata_ctrl_pio_id;
+uint64_t primary_ata_ctrl_pio_addr;
+
+uint64_t second_ata_cmd_pio_id;
+uint64_t second_ata_cmd_pio_addr;
+
+uint64_t second_ata_ctrl_pio_id;
+uint64_t second_ata_ctrl_pio_addr;
+
+uint64_t pci_conf_addr_pio_id;
+uint64_t pci_conf_addr_pio_addr;
+
+uint64_t pci_conf_data_pio_id;
+uint64_t pci_conf_data_pio_addr;
+
+#define COM1_IRQ_CH 0
+#define PRIM_ATA_IRQ_CH 1
+#define SECD_ATA_IRQ_CH 2
+
+#define GUEST_CMDLINE "earlyprintk=serial,0x3f8,115200 debug console=ttyS0,115200 earlycon=serial,0x3f8,115200 loglevel=8"
+
+/* Data for the guest's kernel image. */
+extern char _guest_kernel_image[];
+extern char _guest_kernel_image_end[];
+/* Data for the initial RAM disk to be passed to the kernel. */
+extern char _guest_initrd_image[];
+extern char _guest_initrd_image_end[];
+/* Microkit will set this variable to the start of the guest RAM memory region. */
+uintptr_t guest_ram_vaddr;
+
+// @billn unused, but have to leave it here otherwise linker complains, revisit
+uint64_t guest_ram_size;
+uintptr_t guest_flash_vaddr;
+uint64_t guest_flash_size;
+
+
+bool tsc_calibrating = true;
+linux_x86_setup_ret_t linux_setup;
+uint64_t tsc_pre, tsc_post, measured_tsc_hz;
+
+void init(void)
+{
+    /* Initialise the VMM, the VCPU(s), and start the guest */
+    LOG_VMM("starting \"%s\"\n", microkit_name);
+    /* Place all the binaries in the right locations before starting the guest */
+    size_t kernel_size = _guest_kernel_image_end - _guest_kernel_image;
+
+    size_t initrd_size = _guest_initrd_image_end - _guest_initrd_image;
+    if (!linux_setup_images(guest_ram_vaddr, 0x10000000, (uintptr_t)_guest_kernel_image, kernel_size,
+                            (uintptr_t)_guest_initrd_image, initrd_size, simple_dsdt_aml_code,
+                            sizeof(simple_dsdt_aml_code), GUEST_CMDLINE, &linux_setup)) {
+        LOG_VMM_ERR("Failed to initialise guest images\n");
+        return;
+    }
+
+    vcpu_set_up_long_mode(linux_setup.pml4_gpa, linux_setup.gdt_gpa, linux_setup.gdt_limit);
+
+    /* Pass through COM1 serial port and IDE disk controller */
+    microkit_vcpu_x86_enable_ioport(GUEST_BOOT_VCPU_ID, com1_ioport_id, com1_ioport_addr, com1_ioport_size);
+    passthrough_ide_controller(primary_ata_cmd_pio_id, primary_ata_cmd_pio_addr, primary_ata_ctrl_pio_id,
+                               primary_ata_ctrl_pio_addr, second_ata_cmd_pio_id, second_ata_cmd_pio_addr,
+                               second_ata_ctrl_pio_id, second_ata_ctrl_pio_addr);
+
+    microkit_irq_ack(COM1_IRQ_CH);
+    microkit_irq_ack(PRIM_ATA_IRQ_CH);
+    microkit_irq_ack(SECD_ATA_IRQ_CH);
+
+    LOG_VMM("Measuring TSC frequency...\n");
+    sddf_timer_set_timeout(TIMER_DRV_CH_FOR_LAPIC, NS_IN_S);
+    tsc_pre = rdtsc();
+}
+
+void notified(microkit_channel ch)
+{
+    switch (ch) {
+    case TIMER_DRV_CH_FOR_LAPIC: {
+        if (tsc_calibrating) {
+            tsc_post = rdtsc();
+            measured_tsc_hz = tsc_post - tsc_pre;
+            LOG_VMM("TSC frequency is %lu Hz\n", measured_tsc_hz);
+            tsc_calibrating = false;
+
+            /* Initialise the virtual APIC */
+            bool success = virq_controller_init(measured_tsc_hz);
+            if (!success) {
+                LOG_VMM_ERR("Failed to initialise virtual IRQ controller\n");
+                return;
+            }
+
+            /* Pass through IDE disk controller IRQs */
+            assert(virq_ioapic_register_passthrough(0, 14, PRIM_ATA_IRQ_CH));
+            assert(virq_ioapic_register_passthrough(0, 15, SECD_ATA_IRQ_CH));
+
+            /* Pass through serial IRQs */
+            assert(virq_ioapic_register_passthrough(0, 4, COM1_IRQ_CH));
+
+            guest_start(linux_setup.kernel_entry_gpa, 0, 0);
+        } else {
+            handle_lapic_timer_nftn(GUEST_BOOT_VCPU_ID);
+        }
+        break;
+    }
+    case TIMER_DRV_CH_FOR_PIT:
+        pit_handle_timer_ntfn();
+        break;
+    case TIMER_DRV_CH_FOR_HPET_CH0:
+    case TIMER_DRV_CH_FOR_HPET_CH1:
+    case TIMER_DRV_CH_FOR_HPET_CH2:
+        hpet_handle_timer_ntfn(ch);
+        break;
+    default:
+        // printf("Unexpected channel, ch: 0x%lx\n", ch);
+        virq_ioapic_handle_passthrough(ch);
+    }
+}
+
+/*
+ * The primary purpose of the VMM after initialisation is to act as a fault-handler.
+ * Whenever our guest causes an exception, it gets delivered to this entry point for
+ * the VMM to handle.
+ */
+// seL4_Bool fault(microkit_child child, microkit_msginfo msginfo, microkit_msginfo *reply_msginfo)
+// {
+// #if defined(CONFIG_ARCH_AARCH64)
+//     bool success = fault_handle(child, msginfo);
+//     if (success) {
+//         /* Now that we have handled the fault successfully, we reply to it so
+//          * that the guest can resume execution. */
+//         *reply_msginfo = microkit_msginfo_new(0, 0);
+//         return seL4_True;
+//     }
+// #endif
+
+//     return seL4_False;
+// }
--- examples/virtio_pci/virtio_pci.mk
+++ examples/virtio_pci/virtio_pci.mk
@@ -28,18 +28,50 @@ IMAGE_FILE := loader.img
 REPORT_FILE := report.txt
 DTS_FILE := $(SDDF)/dts/$(MICROKIT_BOARD).dts
 DTB_FILE := $(MICROKIT_BOARD).dtb
-CLIENT_VM := $(VIRTIO_EXAMPLE)/client_vm
 CLIENT_DTB := client_vm/vm.dtb
 METAPROGRAM := $(VIRTIO_EXAMPLE)/meta.py
+SYSTEM_DIR := $(VIRTIO_EXAMPLE)/client_vm/$(ARCH)
 
 SDDF_CUSTOM_LIBC := 1
 
 CLIENT_VM_USERLEVEL_INIT := blk_client_init net_client_init
 
+ifeq ($(ARCH),aarch64)
+	LINUX ?= 85000f3f42a882e4476e57003d53f2bbec8262b0-linux
+	INITRD := b6a276df6a0e39f76bc8950e975daa2888ad83df-rootfs.cpio.gz
+	ARCH_FLAGS := -target aarch64-none-elf -mstrict-align
+	VMM_NAME := client_vmm_aarch64
+	IMAGES = $(VMM_NAME).elf
+
+	QEMU_ARCH_ARGS := -machine virt,virtualization=on,secure=off \
+					  -cpu cortex-a53 \
+					  -device loader,file=$(IMAGE_FILE),addr=0x70000000,cpu-num=0 \
+					  -device virtio-blk-device,drive=drive0,id=virtblk0,num-queues=1 \
+					  -device virtio-net-device,netdev=netdev0 \
+
+else ifeq ($(ARCH),x86_64)
+	LINUX ?= $(SYSTEM_DIR)/bzImage
+	INITRD ?= $(SYSTEM_DIR)/rootfs.cpio.gz
+
+	ARCH_FLAGS := -target x86_64-unknown-elf
+	VMM_NAME := client_vmm_x86_64
+	IMAGES = $(VMM_NAME).elf timer_driver.elf
+
+	KERNEL = sel4.elf
+	KERNEL32 := sel4_32.elf
+
+	QEMU_ARCH_ARGS := -accel kvm -cpu host,+fsgsbase,+pdpe1gb,+xsaveopt,+xsave,+vmx,+vme \
+					  -kernel $(KERNEL32) -initrd $(IMAGE_FILE) \
+					  -device virtio-blk-pci-device,drive=drive0,id=virtblk0,num-queues=1 \
+					  -device virtio-net-pci-device,netdev=netdev0 \
+
+else
+$(error Unsupported ARCH given)
+endif
+
 vpath %.c $(SDDF) $(LIBVMM) $(VIRTIO_EXAMPLE) $(NETWORK_COMPONENTS)
 
 CFLAGS := \
-	  -mstrict-align \
 	  -ffreestanding \
 	  -g3 -O3 -Wall \
 	  -Wno-unused-function \
@@ -52,7 +84,7 @@ CFLAGS := \
 	  -I$(VIRTIO_EXAMPLE)/include \
 	  -MD \
 	  -MP \
-	  -target $(TARGET)
+	  $(ARCH_FLAGS)
 
 LDFLAGS := -L$(BOARD_DIR)/lib
 LIBS := --start-group -lmicrokit -Tmicrokit.ld libsddf_util_debug.a libvmm.a --end-group
@@ -70,7 +102,7 @@ include $(LIBVMM_TOOLS)/linux/uio/uio.mk
 include $(LIBVMM_TOOLS)/linux/blk/blk_init.mk
 include $(LIBVMM_TOOLS)/linux/net/net_init.mk
 
-IMAGES := client_vmm.elf timer_driver.elf blk_driver.elf blk_virt.elf serial_driver.elf serial_virt_tx.elf serial_virt_rx.elf \
+IMAGES += client_vmm.elf timer_driver.elf blk_driver.elf blk_virt.elf serial_driver.elf serial_virt_tx.elf serial_virt_rx.elf \
 	network_virt_rx.elf network_virt_tx.elf eth_driver.elf network_copy.elf
 
 CHECK_FLAGS_BOARD_MD5 := .board_cflags-$(shell echo -- $(CFLAGS) $(BOARD) $(MICROKIT_CONFIG) | shasum | sed 's/ *-//')
@@ -88,29 +120,38 @@ $(IMAGES): libsddf_util_debug.a libvmm.a
 $(DTB_FILE): $(DTS_FILE)
 	$(DTC) -q -I dts -O dtb $< > $@
 
+ifeq ($(ARCH),x86_64)
+$(SYSTEM_FILE): $(METAPROGRAM) $(IMAGES)
+else
 $(SYSTEM_FILE): $(METAPROGRAM) $(IMAGES) $(DTB_FILE) $(CLIENT_DTB)
+endif
+
+ifeq ($(ARCH),x86_64)
+	$(PYTHON) $(METAPROGRAM) --sddf $(SDDF) --board $(MICROKIT_BOARD) --output . --sdf $(SYSTEM_FILE) $(PARTITION_ARG)
+else
 	$(PYTHON) $(METAPROGRAM) --sddf $(SDDF) --board $(MICROKIT_BOARD) --dtb $(DTB_FILE) --client-dtb $(CLIENT_DTB) --output . --sdf $(SYSTEM_FILE) $(PARTITION_ARG)
+endif
 
 ifeq ($(MICROKIT_BOARD), maaxboard)
 	$(OBJCOPY) --update-section .device_resources=timer_driver_device_resources.data timer_driver.elf
 	$(OBJCOPY) --update-section .timer_client_config=timer_client_blk_driver.data blk_driver.elf
 endif
-	$(OBJCOPY) --update-section .device_resources=blk_driver_device_resources.data blk_driver.elf
-	$(OBJCOPY) --update-section .blk_driver_config=blk_driver.data blk_driver.elf
-	$(OBJCOPY) --update-section .blk_virt_config=blk_virt.data blk_virt.elf
-	$(OBJCOPY) --update-section .blk_client_config=blk_client_CLIENT_VMM.data client_vmm.elf
+# 	$(OBJCOPY) --update-section .device_resources=blk_driver_device_resources.data blk_driver.elf
+# 	$(OBJCOPY) --update-section .blk_driver_config=blk_driver.data blk_driver.elf
+# 	$(OBJCOPY) --update-section .blk_virt_config=blk_virt.data blk_virt.elf
+# 	$(OBJCOPY) --update-section .blk_client_config=blk_client_CLIENT_VMM.data client_vmm.elf
 	$(OBJCOPY) --update-section .device_resources=serial_driver_device_resources.data serial_driver.elf
 	$(OBJCOPY) --update-section .serial_driver_config=serial_driver_config.data serial_driver.elf
 	$(OBJCOPY) --update-section .serial_virt_rx_config=serial_virt_rx.data serial_virt_rx.elf
 	$(OBJCOPY) --update-section .serial_virt_tx_config=serial_virt_tx.data serial_virt_tx.elf
 	$(OBJCOPY) --update-section .serial_client_config=serial_client_CLIENT_VMM.data client_vmm.elf
 	$(OBJCOPY) --update-section .vmm_config=vmm_CLIENT_VMM.data client_vmm.elf
-	$(OBJCOPY) --update-section .device_resources=eth_driver_device_resources.data eth_driver.elf
-	$(OBJCOPY) --update-section .net_driver_config=net_driver.data eth_driver.elf
-	$(OBJCOPY) --update-section .net_virt_rx_config=net_virt_rx.data network_virt_rx.elf
-	$(OBJCOPY) --update-section .net_virt_tx_config=net_virt_tx.data network_virt_tx.elf
-	$(OBJCOPY) --update-section .net_copy_config=net_copy_client0_net_copier.data network_copy.elf network_copy.elf
-	$(OBJCOPY) --update-section .net_client_config=net_client_CLIENT_VMM.data client_vmm.elf
+# 	$(OBJCOPY) --update-section .device_resources=eth_driver_device_resources.data eth_driver.elf
+# 	$(OBJCOPY) --update-section .net_driver_config=net_driver.data eth_driver.elf
+# 	$(OBJCOPY) --update-section .net_virt_rx_config=net_virt_rx.data network_virt_rx.elf
+# 	$(OBJCOPY) --update-section .net_virt_tx_config=net_virt_tx.data network_virt_tx.elf
+# 	$(OBJCOPY) --update-section .net_copy_config=net_copy_client0_net_copier.data network_copy.elf network_copy.elf
+# 	$(OBJCOPY) --update-section .net_client_config=net_client_CLIENT_VMM.data client_vmm.elf
 
 $(IMAGE_FILE) $(REPORT_FILE): $(IMAGES) $(SYSTEM_FILE)
 	$(MICROKIT_TOOL) $(SYSTEM_FILE) --search-path $(BUILD_DIR) --board $(MICROKIT_BOARD) \
@@ -141,15 +182,23 @@ client_vm/rootfs.cpio.gz: ${INITRD} \
 blk_storage:
 	$(LIBVMM_TOOLS)/mkvirtdisk $@ $(BLK_NUM_PART) $(BLK_SIZE) $(BLK_MEM)
 
-client_vm/vm.dts: $(CLIENT_VM)/linux.dts $(CLIENT_VM)/$(GIC_DT_OVERLAY) \
+client_vm/vmm.o: $(VIRTIO_EXAMPLE)/$(VMM_NAME).c $(CHECK_FLAGS_BOARD_MD5) |vm_dir
+	$(CC) $(CFLAGS) -c -o $@ $<
+
+ifeq ($(ARCH),x86_64)
+client_vm/images.o: $(LIBVMM)/tools/package_guest_images.S $(LINUX) $(INITRD)
+	$(CC) -c -g3 -x assembler-with-cpp \
+					-DGUEST_KERNEL_IMAGE_PATH=\"${LINUX}\" \
+					-DGUEST_INITRD_IMAGE_PATH=\"${INITRD}\" \
+					$(ARCH_FLAGS) \
+					$(LIBVMM)/tools/package_guest_images.S -o $@
+else
+client_vm/vm.dts: $(SYSTEM_DIR)/linux.dts $(SYSTEM_DIR)/$(GIC_DT_OVERLAY) \
 	$(CHECK_FLAGS_BOARD_MD5) |vm_dir
 	$(LIBVMM)/tools/dtscat $^ > $@
 
 client_vm/vm.dtb: client_vm/vm.dts
 	$(DTC) -q -I dts -O dtb $< > $@
-
-client_vm/vmm.o: $(VIRTIO_EXAMPLE)/client_vmm.c $(CHECK_FLAGS_BOARD_MD5) |vm_dir
-	$(CC) $(CFLAGS) -c -o $@ $<
 
 client_vm/images.o: $(LIBVMM)/tools/package_guest_images.S $(CHECK_FLAGS_BOARD_MD5) \
 	${LINUX} client_vm/vm.dtb client_vm/rootfs.cpio.gz
@@ -157,8 +206,9 @@ client_vm/images.o: $(LIBVMM)/tools/package_guest_images.S $(CHECK_FLAGS_BOARD_M
 					-DGUEST_KERNEL_IMAGE_PATH=\"${LINUX}\" \
 					-DGUEST_DTB_IMAGE_PATH=\"client_vm/vm.dtb\" \
 					-DGUEST_INITRD_IMAGE_PATH=\"client_vm/rootfs.cpio.gz\" \
-					-target $(TARGET) \
+					$(ARCH_FLAGS) \
 					$(LIBVMM)/tools/package_guest_images.S -o $@
+endif
 
 client_vmm.elf: client_vm/vmm.o client_vm/images.o |vm_dir
 	$(LD) $(LDFLAGS) $^ $(LIBS) -o $@
@@ -169,17 +219,12 @@ client_vmm.elf: client_vm/vmm.o client_vm/images.o |vm_dir
 
 qemu: $(IMAGE_FILE) blk_storage
 	[ ${MICROKIT_BOARD} = qemu_virt_aarch64 ]
-	$(QEMU) -machine virt,virtualization=on,secure=off \
-			-cpu cortex-a53 \
-			-serial mon:stdio \
-			-device loader,file=$(IMAGE_FILE),addr=0x70000000,cpu-num=0 \
-			-m size=2G \
-			-nographic \
-			-global virtio-mmio.force-legacy=false \
-			-drive file=blk_storage,format=raw,if=none,id=drive0 \
-			-device virtio-blk-device,drive=drive0,id=virtblk0,num-queues=1 \
-			-device virtio-net-device,netdev=netdev0 \
-			-netdev user,id=netdev0,hostfwd=tcp::1236-:1236,hostfwd=tcp::1237-:1237,hostfwd=udp::1235-:1235 \
+	$(QEMU) $(QEMU_ARCH_ARGS) -serial mon:stdio \
+							  -m size=2G \
+							  -nographic \
+							  -global virtio-mmio.force-legacy=false \
+							  -drive file=blk_storage,format=raw,if=none,id=drive0 \
+							  -netdev user,id=netdev0,hostfwd=tcp::1236-:1236,hostfwd=tcp::1237-:1237,hostfwd=udp::1235-:1235 \
 
 clean::
 	$(RM) -f *.elf .depend* $
