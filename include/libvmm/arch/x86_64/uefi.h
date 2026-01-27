93d13ef4244427ff64a869b7ba4586f680b1f7e4
Bill Nguyen <bill.nguyen@unsw.edu.au>
2026-01-27 14:00:37 +1100

    checkpoint: UEFI high mem clean up, NixOS graphical (Plasma) works, GNOME doesnt work

--- include/libvmm/arch/x86_64/uefi.h
+++ include/libvmm/arch/x86_64/uefi.h
@@ -6,5 +6,7 @@
 
 #include <libvmm/util/util.h>
 
-bool uefi_setup_images(uintptr_t ram_start, size_t ram_size, uintptr_t flash_start, size_t flash_size, char *firm,
-                       size_t firm_size, void *dsdt_blob, uint64_t dsdt_blob_size);
+bool uefi_setup_images(uintptr_t ram_start_vmm, uint64_t ram_start_gpa, size_t ram_size, uintptr_t high_ram_start_vmm,
+                       uint64_t high_ram_start_gpa, size_t high_ram_size, uintptr_t flash_start_vmm,
+                       uintptr_t flash_start_gpa, size_t flash_size, char *firm, size_t firm_size, void *dsdt_blob,
+                       uint64_t dsdt_blob_size);
