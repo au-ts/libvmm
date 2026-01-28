ba81cba95fd7fd9057adddb732f82a80602b164d
Bill Nguyen <bill.nguyen@unsw.edu.au>
2026-01-29 10:13:28 +1100

    iso_to_gpt increase bs

--- examples/uefi/iso_to_gpt_img.sh
+++ examples/uefi/iso_to_gpt_img.sh
@@ -1,10 +1,10 @@
 set -e
 # usage: ./iso_to_gpt_img.sh destination.img source.iso
 
-truncate -s 65G "$1"
+truncate -s 72G "$1"
 sfdisk --no-reread --no-tell-kernel "$1" <<EOF
 label: dos
 
-start=2048,size=64G
+start=2048,size=71G
 EOF
-dd if="$2" of="$1" conv=notrunc seek=2048
+dd if="$2" of="$1" bs=16M conv=notrunc,sync seek=2048 status=progress
