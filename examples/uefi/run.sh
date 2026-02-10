972a521025590693f61c6475f2870b8a2e5fda55
Bill Nguyen <bill.nguyen@unsw.edu.au>
2026-02-11 09:48:16 +1100

    minor uefi changes

--- examples/uefi/run.sh
+++ examples/uefi/run.sh
@@ -10,3 +10,11 @@ qemu-system-x86_64 -accel kvm -cpu host,+sse,+sse2,+fsgsbase,+pdpe1gb,+xsaveopt,
 # 					  -device virtio-blk-pci,drive=drive0,id=virtblk0,num-queues=1,addr=0x3.0 \
 
 							  # -drive file=/dev/loop12,format=raw,if=virtio,id=drive0 \
+
+
+
+qemu-system-x86_64 -machine q35,hpet-msi=off -cpu qemu64,+fsgsbase,+pdpe1gb,+xsaveopt,+xsave \
+ -kernel /home/billn/ts/libvmm/examples/simple/board/x86_64_generic_vtx/bzImage\
+ -initrd /home/billn/ts/libvmm/examples/simple/board/x86_64_generic_vtx/rootfs.cpio.gz \
+ -serial mon:stdio --nographic -d guest_errors \
+  -append 'nokaslr earlyprintk=serial,0x3f8,115200 debug console=ttyS0,115200 earlycon=serial,0x3f8,115200 loglevel=8 pci=nomsi clocksource=hpet'
