aa02f7f74dc38d2dd65689ebe8ea3340cb5b5e09
Bill Nguyen <bill.nguyen@unsw.edu.au>
2026-02-20 12:04:56 +1100

    wip (broken) - handling COM access with string out instructions

--- include/libvmm/arch/x86_64/com.h
+++ include/libvmm/arch/x86_64/com.h
@@ -1,4 +1,5 @@
-#include <libvmm/util/util.h>
 #include <stdbool.h>
+#include <libvmm/util/util.h>
+#include <libvmm/arch/x86_64/ioports.h>
 
-void emulate_com(seL4_VCPUContext *vctx, size_t idx, size_t reg_offset, bool is_read);
+void emulate_com(seL4_VCPUContext *vctx, size_t idx, size_t reg_offset, bool is_read, bool is_rep, bool is_string, ioport_access_width_t access_width);
