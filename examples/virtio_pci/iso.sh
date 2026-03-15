ab27e49ce440706697b1c431d393667f3dd7b135
Ivan Velickovic <i.velickovic@unsw.edu.au>
2026-03-15 23:07:48 +1100

    iso.sh script
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

---
+++ examples/virtio_pci/iso.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+# usage: ./iso_to_gpt_img.sh destination.img source.iso
+
+if ! file $2 | grep -v "QCOW"; then
+        echo "Naughty, tried to use QCOW"
+        exit 1
+fi
+
+rm -f "$1"
+truncate -s 8G "$1"
+sfdisk --no-reread --no-tell-kernel "$1" <<EOF
+label: dos
+
+start=2048,size=7G
+EOF
+
+# it is extremely important that bs=512 and seek=2048, as seek is in unit of `bs`.
+dd if="$2" of="$1" bs=4096 conv=notrunc,sync,sparse seek=256 status=progress
+
