aba4524ddc061556c308734217e0867654e06a6b
Bill Nguyen <bill.nguyen@unsw.edu.au>
2026-04-07 13:19:34 +1000

    ci: moved virtio backend function to common.py
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>

--- ci/tests/virtio.py
+++ ci/tests/virtio.py
@@ -3,12 +3,8 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
 import asyncio
-import os
 from pathlib import Path
-import subprocess
 import sys
-import tempfile
-import types
 
 from ts_ci import (
     HardwareBackend,
@@ -19,55 +15,7 @@ from ts_ci import (
 
 sys.path.insert(1, Path(__file__).parents[2].as_posix())
 from ci import common, matrix
-from ci.common import TestConfig, run_tests
-
-LIBVMM = Path(__file__).parents[2]
-mkvirtdisk = (LIBVMM / "dep" / "sddf" / "tools" / "mkvirtdisk").resolve()
-
-
-def backend_fn(test_config: TestConfig, loader_img: Path) -> HardwareBackend:
-    backend = common.backend_fn(test_config, loader_img)
-
-    if isinstance(backend, QemuBackend):
-        tmpdir = tempfile.TemporaryDirectory(suffix="libvmm_blk_disks")
-
-        fd, disk_path = tempfile.mkstemp(dir=tmpdir.name)
-        os.close(fd)
-
-        subprocess.run(
-            [mkvirtdisk, disk_path, "1", "512", "16777216", "GPT"],
-            check=True,
-            capture_output=True,
-        )
-
-        if test_config.board == "x86_64_generic":
-            virtio_blk_device = "virtio-blk-pci,drive=hd,addr=0x3.0"
-            virtio_net_device = "virtio-net-pci,netdev=netdev0,addr=0x2.0"
-        else:
-            virtio_blk_device = "virtio-blk-device,drive=hd,bus=virtio-mmio-bus.1"
-            virtio_net_device = "virtio-net-device,netdev=netdev0,bus=virtio-mmio-bus.0"
-
-        # fmt: off
-        backend.invocation_args.extend([
-            "-global", "virtio-mmio.force-legacy=false",
-            "-drive", "file={},if=none,format=raw,id=hd".format(disk_path),
-            "-device", virtio_blk_device,
-            "-device", virtio_net_device,
-            "-netdev", "user,id=netdev0,"
-        ])
-        # fmt: on
-
-        orig_stop = backend.stop
-
-        async def stop_with_cleanup(self):
-            try:
-                await orig_stop()
-            finally:
-                tmpdir.cleanup()
-
-        backend.stop = types.MethodType(stop_with_cleanup, backend)
-
-    return backend
+from ci.common import virtio_backend_fn, run_tests
 
 
 async def test(backend: HardwareBackend, test_config: common.TestConfig):
@@ -84,7 +32,7 @@ TEST_CASES = matrix.generate_example_test_cases(
     "virtio",
     ["virtio", "virtio_pci"],
     test_fn=test,
-    backend_fn=backend_fn,
+    backend_fn=virtio_backend_fn,
     no_output_timeout_s=matrix.NO_OUTPUT_DEFAULT_TIMEOUT_S,
 )
 
