a576e56cf143a8958c4834e754788977da81bd66
Bill Nguyen <bill.nguyen@unsw.edu.au>
2026-04-07 18:47:59 +1000

    ci/tests: add network tests
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>

---
+++ ci/tests/virtio_net_wget.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# Copyright 2026, UNSW
+# SPDX-License-Identifier: BSD-2-Clause
+
+import asyncio
+from pathlib import Path
+import sys
+
+from ts_ci import (
+    HardwareBackend,
+    send_input,
+    wait_for_output,
+)
+
+sys.path.insert(1, Path(__file__).parents[2].as_posix())
+from ci import common, matrix
+from ci.common import virtio_backend_fn, run_tests
+
+
+async def test_virtio_net_wget(
+    backend: HardwareBackend, test_config: common.TestConfig
+):
+    async with asyncio.timeout(30):
+        await wait_for_output(backend, b"buildroot login: ")
+        # Sleep should not be necessary, see https://github.com/au-ts/libvmm/issues/195
+        await asyncio.sleep(1)
+        await send_input(backend, b"root\n")
+        await wait_for_output(backend, b"# ")
+        await send_input(backend, b"wget httpbin.org/html\n")
+        await wait_for_output(backend, b"'html' saved")
+        await send_input(backend, b"cat html\n")
+        await wait_for_output(backend, b"<!DOCTYPE html>")
+        await wait_for_output(backend, b"and all of them a care-killing competency.")
+
+
+# export
+TEST_CASES = matrix.generate_example_test_cases(
+    "virtio_network_download",
+    ["virtio", "virtio_pci"],
+    test_fn=test_virtio_net_wget,
+    backend_fn=common.virtio_backend_fn,
+    no_output_timeout_s=matrix.NO_OUTPUT_DEFAULT_TIMEOUT_S,
+)
+
+
+if __name__ == "__main__":
+    run_tests(TEST_CASES)
