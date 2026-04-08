8ffa8de2e659fc6e4efa119deccef75cfd657d63
Bill Nguyen <bill.nguyen@unsw.edu.au>
2026-04-08 19:59:04 +1000

    virtio: add util funcs for manipulating virtqueue
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>

--- include/libvmm/virtio/virtio.h
+++ include/libvmm/virtio/virtio.h
@@ -63,3 +63,28 @@ static inline struct virtq *get_current_virtq_by_handler(virtio_device_t *dev)
  * and virtual IRQ associated with the device has been registered.
  */
 bool virtio_mmio_register_device(virtio_device_t *dev, uintptr_t region_base, uintptr_t region_size, size_t virq);
+
+/* Given a descriptor head, walk the descriptor chain and compute the sum of the length of all the desciptor chain's buffers */
+uint64_t virtio_desc_chain_payload_len(virtio_queue_handler_t *vq_handler, uint16_t desc_head);
+
+/* Given a scatter gather list of a virtio request in the form of a descriptor head, copy a chunk of data from the list,
+ * starting from `read_off` into `data`. For example, if you want the body of a virtio block read or write request,
+ * `read_off` should be `sizeof(struct virtio_blk_outhdr)`.
+ * Return true if the operation completed successfully, false if the amount of data overflows the
+ * scatter gather list. */
+bool virtio_read_data_from_desc_chain(virtio_queue_handler_t *vq_handler, uint16_t desc_head, uint64_t bytes_to_read,
+                                      uint64_t read_off, char *data);
+
+/* Same idea as above but we are now writing to the scatter gather buffers in guest RAM */
+bool virtio_write_data_to_desc_chain(virtio_queue_handler_t *vq_handler, uint16_t desc_head, uint64_t bytes_to_write,
+                                     uint64_t write_off, char *data);
+
+/* Peek an available descriptor head from a virtq */
+bool virtio_virtq_peek_avail(virtio_queue_handler_t *vq_handler, uint16_t *ret);
+
+/* Pop an available descriptor head from a virtq */
+bool virtio_virtq_pop_avail(virtio_queue_handler_t *vq_handler, uint16_t *ret);
+
+/* Given a descriptor head and the number of bytes that were written to it by the VMM, place it
+ * in the used queue */
+void virtio_virtq_add_used(virtio_queue_handler_t *vq_handler, uint16_t desc_head, uint32_t bytes_written);
