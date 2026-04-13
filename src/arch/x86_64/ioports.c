6145b8f864b67fc2a66cb876a538ee0aa60d5b72
Bill Nguyen <bill.nguyen@unsw.edu.au>
2026-04-13 11:22:25 +1000

    x86_64: addressed various TODOs
    Mainly deduplicating port I/O fault handling code
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>

--- src/arch/x86_64/ioports.c
+++ src/arch/x86_64/ioports.c
@@ -13,17 +13,33 @@
 #include <libvmm/arch/x86_64/instruction.h>
 #include <libvmm/arch/x86_64/vmcs.h>
 #include <libvmm/arch/x86_64/fault.h>
-#include <libvmm/arch/x86_64/com.h>
 #include <libvmm/guest.h>
 
-extern struct pci_bus pci_bus_state;
+/* Documents referenced:
+ * 1. Intel® 64 and IA-32 Architectures Software Developer’s Manual
+ *    Combined Volumes: 1, 2A, 2B, 2C, 2D, 3A, 3B, 3C, 3D, and 4
+ *    Order Number: 325462-088US June 2025
+ */
 
-// TODO: hack
+// @billn do properly: hack for ACPI PM timer
 #define TIMER_DRV_CH_FOR_LAPIC 11
 
-int ioports_access_width_to_bytes(ioport_access_width_t access_width)
+/* Table 29-5. "Exit Qualification for I/O Instructions" */
+#define PIO_VIOLATION_READ_DIRECTION_BIT BIT(3)
+#define PIO_VIOLATION_STRING_OP_BIT BIT(4)
+#define PIO_VIOLATION_ADDR_SHIFT 16
+#define PIO_VIOLATION_ADDR_MASK 0xffff
+#define PIO_VIOLATION_ACC_WIDTH_MASK 0x7
+
+typedef enum ioport_access_width_qualification {
+    IOPORT_BYTE_ACCESS_QUAL = 0,
+    IOPORT_WORD_ACCESS_QUAL = 1, // 2-byte
+    IOPORT_DWORD_ACCESS_QUAL = 3, // 4-byte
+} ioport_access_width_t;
+
+int pio_fault_to_access_width_bytes(seL4_Word qualification)
 {
-    switch (access_width) {
+    switch (qualification & PIO_VIOLATION_ACC_WIDTH_MASK) {
     case IOPORT_BYTE_ACCESS_QUAL:
         return 1;
     case IOPORT_WORD_ACCESS_QUAL:
@@ -31,15 +47,37 @@ int ioports_access_width_to_bytes(ioport_access_width_t access_width)
     case IOPORT_DWORD_ACCESS_QUAL:
         return 4;
     default:
+        /* Hardware bug or wrong type of qualification */
+        assert(false);
         return 0;
     }
 }
 
+bool pio_fault_is_read(seL4_Word qualification)
+{
+    return !!(qualification & PIO_VIOLATION_READ_DIRECTION_BIT);
+}
+
+bool pio_fault_is_write(seL4_Word qualification)
+{
+    return !pio_fault_is_read(qualification);
+}
+
+bool pio_fault_is_string_op(seL4_Word qualification)
+{
+    return !!(qualification & PIO_VIOLATION_STRING_OP_BIT);
+}
+
+uint16_t pio_fault_addr(seL4_Word qualification)
+{
+    return (qualification >> PIO_VIOLATION_ADDR_SHIFT) & PIO_VIOLATION_ADDR_MASK;
+}
+
 bool emulate_ioports(seL4_VCPUContext *vctx, uint64_t f_qualification)
 {
-    uint64_t is_read = f_qualification & BIT(3);
-    uint64_t is_string = f_qualification & BIT(4);
-    uint16_t port_addr = (f_qualification >> 16) & 0xffff;
+    uint64_t is_read = pio_fault_is_read(f_qualification);
+    uint64_t is_string = pio_fault_is_string_op(f_qualification);
+    uint16_t port_addr = pio_fault_addr(f_qualification);
 
     assert(!is_string);
 
