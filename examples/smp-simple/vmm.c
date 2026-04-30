7e872bb8696722ab2c00746a371288e942d86d99
Bill Nguyen <bill.nguyen@unsw.edu.au>
2026-04-30 16:34:40 +0700

    examples/smp-simple: carbon copy from simple
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>

---
+++ examples/smp-simple/vmm.c
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2023, UNSW
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <microkit.h>
+#include <libvmm/libvmm.h>
+
+/*
+ * As this is just an example, for simplicity we just make the size of the
+ * guest's "RAM" the same for all platforms. For just booting Linux with a
+ * simple user-space, 0x10000000 bytes (256MB) is plenty.
+ */
+#define GUEST_RAM_SIZE 0x10000000
+
+#if defined(BOARD_qemu_virt_aarch64)
+#define GUEST_DTB_VADDR 0x4f000000
+#define GUEST_INIT_RAM_DISK_VADDR 0x4d000000
+#elif defined(BOARD_odroidc4)
+#define GUEST_DTB_VADDR 0x2f000000
+#define GUEST_INIT_RAM_DISK_VADDR 0x2d700000
+#elif defined(BOARD_maaxboard)
+#define GUEST_DTB_VADDR 0x4f000000
+#define GUEST_INIT_RAM_DISK_VADDR 0x4c000000
+#else
+#error Need to define guest kernel image address and DTB address
+#endif
+
+/* For simplicity we just enforce the serial IRQ channel number to be the same
+ * across platforms. */
+#define SERIAL_IRQ_CH 1
+
+#if defined(BOARD_qemu_virt_aarch64)
+#define SERIAL_IRQ 33
+#elif defined(BOARD_odroidc4)
+#define SERIAL_IRQ 225
+#elif defined(BOARD_maaxboard)
+#define SERIAL_IRQ 58
+#else
+#error Need to define serial interrupt
+#endif
+
+/* Data for the guest's kernel image. */
+extern char _guest_kernel_image[];
+extern char _guest_kernel_image_end[];
+/* Data for the device tree to be passed to the kernel. */
+extern char _guest_dtb_image[];
+extern char _guest_dtb_image_end[];
+/* Data for the initial RAM disk to be passed to the kernel. */
+extern char _guest_initrd_image[];
+extern char _guest_initrd_image_end[];
+/* Microkit will set this variable to the start of the guest RAM memory region. */
+uintptr_t guest_ram_vaddr;
+
+void init(void)
+{
+    /* Initialise the VMM, the VCPU(s), and start the guest */
+    LOG_VMM("starting \"%s\"\n", microkit_name);
+    /* Place all the binaries in the right locations before starting the guest */
+    size_t kernel_size = _guest_kernel_image_end - _guest_kernel_image;
+    size_t dtb_size = _guest_dtb_image_end - _guest_dtb_image;
+    size_t initrd_size = _guest_initrd_image_end - _guest_initrd_image;
+    uintptr_t kernel_pc = linux_setup_images(guest_ram_vaddr, (uintptr_t)_guest_kernel_image, kernel_size,
+                                             (uintptr_t)_guest_dtb_image, GUEST_DTB_VADDR, dtb_size,
+                                             (uintptr_t)_guest_initrd_image, GUEST_INIT_RAM_DISK_VADDR, initrd_size);
+    if (!kernel_pc) {
+        LOG_VMM_ERR("Failed to initialise guest images\n");
+        return;
+    }
+    /* Initialise the virtual GIC driver */
+    bool success = virq_controller_init();
+    if (!success) {
+        LOG_VMM_ERR("Failed to initialise emulated interrupt controller\n");
+        return;
+    }
+    success = virq_register_passthrough(GUEST_BOOT_VCPU_ID, SERIAL_IRQ, SERIAL_IRQ_CH);
+    assert(success);
+    /* Finally start the guest */
+    guest_start(kernel_pc, GUEST_DTB_VADDR, GUEST_INIT_RAM_DISK_VADDR);
+}
+
+void notified(microkit_channel ch)
+{
+    switch (ch) {
+    case SERIAL_IRQ_CH: {
+        bool success = virq_handle_passthrough(ch);
+        if (!success) {
+            LOG_VMM_ERR("IRQ %d dropped\n", SERIAL_IRQ);
+        }
+        break;
+    }
+    default:
+        printf("Unexpected channel, ch: 0x%lx\n", ch);
+    }
+}
+
+/*
+ * The primary purpose of the VMM after initialisation is to act as a fault-handler.
+ * Whenever our guest causes an exception, it gets delivered to this entry point for
+ * the VMM to handle.
+ */
+seL4_Bool fault(microkit_child child, microkit_msginfo msginfo, microkit_msginfo *reply_msginfo)
+{
+    bool success = fault_handle(child, msginfo);
+    if (success) {
+        /* Now that we have handled the fault successfully, we reply to it so
+         * that the guest can resume execution. */
+        *reply_msginfo = microkit_msginfo_new(0, 0);
+        return seL4_True;
+    }
+
+    return seL4_False;
+}
