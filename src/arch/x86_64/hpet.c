5dd4eeffba124c71c77a29d05364901f2858d0d3
Bill Nguyen <bill.nguyen@unsw.edu.au>
2026-05-18 10:46:35 +1000

    hpet: disable bug check, it's too sensitive
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>

--- src/arch/x86_64/hpet.c
+++ src/arch/x86_64/hpet.c
@@ -207,25 +207,25 @@ uint64_t timer_n_compute_timeout_ns(int n, uint64_t main_counter_val)
 
 bool bug_check_irq_at_correct_time(int comparator, uint64_t main_counter_val)
 {
-    uint64_t expected_counter_val = hpet_regs.comparators[comparator].current_comparator;
-    uint64_t difference_units;
-    uint64_t tolerance_units = (NS_IN_MS / 100);
+    // uint64_t expected_counter_val = hpet_regs.comparators[comparator].current_comparator;
+    // uint64_t difference_units;
+    // uint64_t tolerance_units = (NS_IN_MS / 100);
 
-    if (expected_counter_val > main_counter_val) {
-        difference_units = expected_counter_val - main_counter_val;
-        if (difference_units > tolerance_units) {
-            LOG_VMM_ERR("HPET timer irq too early!!! comp %d, counter %lu, comparator %lu, diff %lu > margin %lu\n", comparator,
-                        main_counter_val, hpet_regs.comparators[comparator].current_comparator, difference_units, tolerance_units);
-            return false;
-        }
-    } else {
-        difference_units = main_counter_val - expected_counter_val;
-        if (difference_units > tolerance_units) {
-            LOG_VMM_ERR("HPET timer irq too late!!! comp %d, counter %lu, comparator %lu, diff %lu > margin %lu\n", comparator,
-                        main_counter_val, hpet_regs.comparators[comparator].current_comparator, difference_units, tolerance_units);
-            return false;
-        }
-    }
+    // if (expected_counter_val > main_counter_val) {
+    //     difference_units = expected_counter_val - main_counter_val;
+    //     if (difference_units > tolerance_units) {
+    //         LOG_VMM_ERR("HPET timer irq too early!!! comp %d, counter %lu, comparator %lu, diff %lu > margin %lu\n", comparator,
+    //                     main_counter_val, hpet_regs.comparators[comparator].current_comparator, difference_units, tolerance_units);
+    //         return false;
+    //     }
+    // } else {
+    //     difference_units = main_counter_val - expected_counter_val;
+    //     if (difference_units > tolerance_units) {
+    //         LOG_VMM_ERR("HPET timer irq too late!!! comp %d, counter %lu, comparator %lu, diff %lu > margin %lu\n", comparator,
+    //                     main_counter_val, hpet_regs.comparators[comparator].current_comparator, difference_units, tolerance_units);
+    //         return false;
+    //     }
+    // }
 
     return true;
 }
