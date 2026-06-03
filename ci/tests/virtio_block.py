d42983aa5f425b13243a1f88ceedf0635bb3acca
Bill Nguyen <bill.nguyen@unsw.edu.au>
2026-06-03 15:04:13 +1000

    ci: remove sleep hack, issue fixed by #257
    This should not be necessary anymore since the underlying issue have
    been fixed in https://github.com/au-ts/libvmm/pull/257
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>

--- ci/tests/virtio_block.py
+++ ci/tests/virtio_block.py
@@ -20,8 +20,6 @@ from ci.common import virtio_backend_fn, run_tests
 async def test_virtio_block(backend: HardwareBackend, test_config: common.TestConfig):
     async with asyncio.timeout(180):
         await wait_for_output(backend, b"buildroot login: ")
-        # Sleep should not be necessary, see https://github.com/au-ts/libvmm/issues/195
-        await asyncio.sleep(1)
         await send_input(backend, b"root\n")
         await wait_for_output(backend, b"# ")
         # Unconditionally unmount and make a fresh file system before running the tests
