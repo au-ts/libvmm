d043938bd60617dee2341fb7206b2358b9d4e180
Bill Nguyen <bill.nguyen@unsw.edu.au>
2026-06-22 11:17:49 +1000

    examples/rust: update rust-sel4 to latest mainline
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>

--- examples/rust/src/vmm.rs
+++ examples/rust/src/vmm.rs
@@ -5,10 +5,12 @@
 #![no_main]
 #![feature(never_type)]
 
-use core::{include_bytes};
 use core::ffi::c_void;
+use core::include_bytes;
 
-use sel4_microkit::{protection_domain, MessageInfo, Channel, Child, Handler, debug_println};
+use sel4_microkit::{
+    debug_println, protection_domain, Channel, ChannelSet, Child, Handler, MessageInfo,
+};
 
 const GUEST_RAM_VMM_VADDR: usize = 0x40000000;
 /* For ARM, these constants depends on what's defined in your DTB. */
@@ -38,10 +40,17 @@ const UART_CH: Channel = Channel::new(1);
 #[link(name = "microkit", kind = "static")]
 extern "C" {
     fn guest_init(init_args: arch_guest_init) -> bool;
-    fn linux_setup_images(ram_start: usize,
-                          kernel: usize, kernel_size: usize,
-                          dtb_src: usize, dtb_dest: usize, dtb_size: usize,
-                          initrd_src: usize, initrd_dest: usize, initrd_size: usize) -> usize;
+    fn linux_setup_images(
+        ram_start: usize,
+        kernel: usize,
+        kernel_size: usize,
+        dtb_src: usize,
+        dtb_dest: usize,
+        dtb_size: usize,
+        initrd_src: usize,
+        initrd_dest: usize,
+        initrd_size: usize,
+    ) -> usize;
     fn virq_register_passthrough(vcpu_id: usize, irq: i32, irq_ch: u32) -> bool;
     fn virq_handle_passthrough(irq_ch: u32) -> bool;
     fn guest_start(kernel_pc: usize, dtb: usize, initrd: usize) -> bool;
@@ -91,14 +100,21 @@ fn init() -> VmmHandler {
         });
         assert!(success);
 
-        let guest_pc = linux_setup_images(GUEST_RAM_START_GPA,
-                                            linux_addr, linux.len(),
-                                            dtb_addr, GUEST_DTB_GPA, dtb.len(),
-                                            initrd_addr, GUEST_INIT_RAM_DISK_GPA, initrd.len()
-                                         );
+        let guest_pc = linux_setup_images(
+            GUEST_RAM_START_GPA,
+            linux_addr,
+            linux.len(),
+            dtb_addr,
+            GUEST_DTB_GPA,
+            dtb.len(),
+            initrd_addr,
+            GUEST_INIT_RAM_DISK_GPA,
+            initrd.len(),
+        );
         assert!(guest_pc != 0);
 
-        let success = virq_register_passthrough(GUEST_BOOT_VCPU_ID, UART_IRQ as i32, UART_CH.index() as u32);
+        let success =
+            virq_register_passthrough(GUEST_BOOT_VCPU_ID, UART_IRQ as i32, UART_CH.index() as u32);
         assert!(success);
         guest_start(guest_pc, GUEST_DTB_GPA, GUEST_INIT_RAM_DISK_GPA);
     }
@@ -111,24 +127,25 @@ struct VmmHandler {}
 impl Handler for VmmHandler {
     type Error = !;
 
-    fn notified(&mut self, channel: Channel) -> Result<(), Self::Error> {
-        match channel {
-            UART_CH => {
-                unsafe {
-                    let success = virq_handle_passthrough(UART_CH.index() as u32);
-                    if !success {
-                        debug_println!("VMM|ERROR: could not inject UART IRQ");
-                    }
+    fn notified(&mut self, channels: ChannelSet) -> Result<(), Self::Error> {
+        if channels.contains(UART_CH) {
+            unsafe {
+                let success = virq_handle_passthrough(UART_CH.index() as u32);
+                if !success {
+                    debug_println!("VMM|ERROR: could not inject UART IRQ");
                 }
             }
-            _ => unreachable!()
         }
         Ok(())
     }
 
-    fn fault(&mut self, id: Child, msg_info: MessageInfo) -> Result<Option<MessageInfo>, Self::Error> {
+    fn fault(
+        &mut self,
+        child: Child,
+        msg_info: MessageInfo,
+    ) -> Result<Option<MessageInfo>, Self::Error> {
         unsafe {
-            if fault_handle(id.index(), msg_info) {
+            if fault_handle(child.index(), msg_info) {
                 Ok(Some(MessageInfo::new(0, 0)))
             } else {
                 unreachable!()
