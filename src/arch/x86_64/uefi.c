79e58c1409bcb2d0bed7f225a177a055c2247509
Bill Nguyen <bill.nguyen@unsw.edu.au>
2026-07-16 11:44:49 +1000

    x86_64/uefi: build E820 table, checkpoint OVMF parsing E820 correctly
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>

--- src/arch/x86_64/uefi.c
+++ src/arch/x86_64/uefi.c
@@ -18,6 +18,11 @@
 /* Document referenced:
  * https://github.com/tianocore/edk2/blob/edk2-stable202605/OvmfPkg/README */
 
+/* Backing storage for the E820 table we need to provide to the firmware, static since
+ * the fw_cfg code expects the lifetime of the buffer to be equal to the lifetime of the VMM.
+ * Since the guest can access the fw cfg at any time. */
+struct boot_e820_entry uefi_e820[GUEST_MAX_RAM_REGIONS];
+
 bool uefi_setup_images(uintptr_t firm_src, size_t firm_size, uint64_t flash_gpa, size_t flash_size)
 {
     if (!firm_size) {
@@ -54,6 +59,22 @@ bool uefi_setup_images(uintptr_t firm_src, size_t firm_size, uint64_t flash_gpa,
     /* Initialise the fw cfg device to give the E820 memory map, ACPI tables and other important
      * things to OVMF. */
     if (!initialise_fw_cfg()) {
+        return false;
+    }
+
+    int num_guest_ram_regions;
+    struct guest_ram_region *guest_ram_regions = guest_ram_get_regions(&num_guest_ram_regions);
+    assert(num_guest_ram_regions); // impossible, should have been checked earlier
+
+    for (int i = 0; i < num_guest_ram_regions; i++) {
+        uefi_e820[i].addr = guest_ram_regions[i].gpa_start;
+        uefi_e820[i].size = guest_ram_regions[i].size;
+        uefi_e820[i].type = E820_RAM;
+    }
+
+    if (!fw_cfg_add_named_file(E820_FWCFG_FILENAME, strlen(E820_FWCFG_FILENAME), (uint8_t *)&uefi_e820,
+                               sizeof(struct boot_e820_entry) * num_guest_ram_regions)) {
+        LOG_VMM_ERR("Failed to add '%s' file to fw cfg\n", E820_FWCFG_FILENAME);
         return false;
     }
 
