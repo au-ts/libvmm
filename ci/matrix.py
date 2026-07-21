cc4859623eda2ca458fdeeeb0bb57080a497d757
Bill Nguyen <bill.nguyen@unsw.edu.au>
2026-07-21 11:06:17 +1000

    ci: re-enable rust example
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>

--- ci/matrix.py
+++ ci/matrix.py
@@ -75,11 +75,7 @@ EXAMPLES: dict[str, _ExampleMatrixType] = {
         "boards": [
             "qemu_virt_aarch64",
         ],
-        # disabled due to https://github.com/seL4/microkit/pull/532 breaking rust-sel4
-        "tests_exclude": [
-            { "config": "debug" },
-            { "config": "release" },
-        ],
+        "tests_exclude": [],
     },
     "simple": {
         "configs": ["debug", "release"],
