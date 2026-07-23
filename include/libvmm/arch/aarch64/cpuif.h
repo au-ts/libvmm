e539de3ce84b590530bb3750610773402dd5838f
Bill Nguyen <bill.nguyen@unsw.edu.au>
2026-07-23 11:51:42 +1000

    aarch64/vgic: handle ICC_SRE_EL1 read
    This is required by a recent kernel update.
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>

--- include/libvmm/arch/aarch64/cpuif.h
+++ include/libvmm/arch/aarch64/cpuif.h
@@ -12,7 +12,7 @@
 #include <libvmm/util/util.h>
 
 typedef bool (*sysreg_access_fn_t)(size_t vcpu_id, seL4_UserContext *regs, bool is_read);
-typedef bool (*sysreg_read_exception_handler_t)(size_t vcpu_id, seL4_UserContext *regs);
+typedef bool (*sysreg_read_exception_handler_t)(size_t vcpu_id, seL4_UserContext *regs, uint64_t *data);
 typedef bool (*sysreg_write_exception_handler_t)(size_t vcpu_id, seL4_UserContext *regs, uint64_t data);
 
 /* Handles exception from MSR, MRS, or System instruction execution in AArch64 state (EC class 0x18). */
--- include/libvmm/arch/aarch64/vgic/vgic_v3_cpuif.h
+++ include/libvmm/arch/aarch64/vgic/vgic_v3_cpuif.h
@@ -11,5 +11,6 @@
 #include <libvmm/util/util.h>
 #include <libvmm/arch/aarch64/cpuif.h>
 
-bool icc_sgi1r_el1_access(size_t vcpu_id, seL4_UserContext *regs, bool is_read);
 bool icc_sgi1r_el1_write(size_t vcpu_id, seL4_UserContext *regs, uint64_t data);
+
+bool icc_sre_el1_read(size_t vcpu_id, seL4_UserContext *regs, uint64_t *data);
