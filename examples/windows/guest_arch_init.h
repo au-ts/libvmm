5f9b13f7aaebcc6f459118928e2e29d69860d7f0
Bill Nguyen <bill.nguyen@unsw.edu.au>
2026-08-06 12:41:37 +1000

    examples/windows: init squashed
    cp -r examples/virtio_pci examples/windows
    
    Except for ARM stuff, Linux and Buildroot config files.
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: renamed and purged stuff from virtio_pci
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: +QEMU RAM, fix virtio regs moving, +bochs-display
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: rm serial subsystem + boot UEFI
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: add bochs-display passthrough
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: bump guest RAM to 11G
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: passthrough kb + mouse
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: handle all IRQ passthrough
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: fix BSOD inaccessible boot device
    
    Turns out Windows is very strict on ACPI DSDT parsing. The PCI devices
    need an interrupt link device. The Link devices must have all the
    methods even if they are dummy, else the Windows' resource arbiter will
    not accept the interrupt and fail the viostor driver binding process.
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: purge linux
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: fix PS/2 KB+M not working on Windows
    
    We need to tell Windows that our virtual PCI bridge decodes I/O Ports
    range that these legacy devices live in.
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    HACK: enable Intel APICv operation
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    checkpoint windows 10+11 working on QEMU, just need APICv kernel patch
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: metaprogram clean ups
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: add debugging guide
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: lower guest RAM to 8GB, lower reso to 1080p
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: add install instructions
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: use OVMF with LionsOS logo
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: add UEFI repro instruction
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>

---
+++ examples/windows/guest_arch_init.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2026, UNSW
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#define SERIAL_IRQ_CH 51
+
+/* Windows example specific helpers to hide architecture specific details from
+ * the high level VMM source file. */
+bool guest_arch_init(void);
+
+bool virtio_arch_init(void);
+
+bool guest_arch_start(void);
