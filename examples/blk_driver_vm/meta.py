0ae36de9e432840bfb284fba3cc5d31076f95d5c
Joshua Shim <joshua.shim2004@gmail.com>
2026-08-15 12:09:45 +1000

    init blk benchmarks

---
+++ examples/blk_driver_vm/meta.py
@@ -0,0 +1,522 @@
+# Copyright 2025, UNSW
+# SPDX-License-Identifier: BSD-2-Clause
+import argparse
+import struct
+import random
+from dataclasses import dataclass
+from typing import List, Tuple
+from sdfgen import SystemDescription, Sddf, DeviceTree, Vmm
+from importlib.metadata import version
+
+import sys, os
+
+import json
+import subprocess
+import shutil
+from typing import Callable, Optional
+from board import BOARDS
+
+ProtectionDomain = SystemDescription.ProtectionDomain
+MemoryRegion = SystemDescription.MemoryRegion
+Map = SystemDescription.Map
+Channel = SystemDescription.Channel
+VirtualMachine = SystemDescription.VirtualMachine
+Irq = SystemDescription.Irq
+
+
+# @dataclass
+# class Board:
+#     name: str
+#     arch: SystemDescription.Arch
+#     paddr_top: int
+#     serial: str
+#     guest_serial: str
+#     timer: str
+#     ethernet: str
+#     passthrough: list[str]
+
+
+# BOARDS: List[Board] = [
+#     Board(
+#         name="odroidc4",
+#         arch=SystemDescription.Arch.AARCH64,
+#         paddr_top=0x60000000,
+#         serial="soc/bus@ff800000/serial@3000",
+#         guest_serial="virtio-console@100000",
+#         timer="soc/bus@ffd00000/watchdog@f0d0",
+#         ethernet="soc/ethernet@ff3f0000",
+#         passthrough=[
+#             "soc/bus@ff600000/bus@34400",   # pinmux
+#             "soc/bus@ff600000/bus@3c000",   # power controller
+#             "soc/bus@ff600000/mdio-multiplexer@4c000", # ethernet PHY
+#             "soc/bus@ff800000/sys-ctrl@0",  # pinmux
+#         ],
+#     ),
+# ]
+
+"""
+Below are classes to serialise into custom configuration for the benchmarking component.
+All serialised definitions are little endian and pointers are 64-bit integers.
+Structs are serialised to match 64-bit alignment.
+"""
+
+
+class BenchmarkIdleConfig:
+    def __init__(self, cycle_counters: int, ch_init: int):
+        self.cycle_counters = cycle_counters
+        self.ch_init = ch_init
+
+    """
+        Matches struct definition:
+        {
+            void *;
+            uint8_t;
+        }
+    """
+
+    def serialise(self) -> bytes:
+        return struct.pack(
+            "<qc", self.cycle_counters, self.ch_init.to_bytes(1, "little")
+        )
+
+class BenchmarkClientConfig:
+    def __init__(self, ch_start: int, ch_stop: int, cycle_counters: List[int]):
+        self.cycle_counters = cycle_counters
+        self.ch_start = ch_start
+        self.ch_stop = ch_stop
+
+    """
+        Matches struct definition:
+        {
+            uint8_t;
+            uint8_t;
+            uint8_t;
+            void * [];
+        }
+    """
+
+    def serialise(self) -> bytes:
+        # Padded for 64 bit alignment
+        pack_str = "<BBBxxxxx" + "q" * len(self.cycle_counters)
+        return struct.pack(
+            pack_str,
+            self.ch_start,
+            self.ch_stop,
+            len(self.cycle_counters),
+            *self.cycle_counters,
+        )
+
+class BenchmarkConfig:
+    def __init__(
+        self,
+        ch_rx_start: int,
+        ch_tx_start: int,
+        ch_rx_stop: int,
+        ch_tx_stop: int,
+        ch_init: int,
+        core: int,
+        last_core: bool,
+        children: List[Tuple[int, str]],
+        pmu_events: List[int],
+    ):
+        self.ch_rx_start = ch_rx_start
+        self.ch_tx_start = ch_tx_start
+        self.ch_rx_stop = ch_rx_stop
+        self.ch_tx_stop = ch_tx_stop
+        self.ch_init = ch_init
+        self.core = core
+        self.last_core = last_core
+        self.children = children
+        self.pmu_events = pmu_events
+
+    """
+        Matches struct definition:
+        {
+            uint8_t;
+            uint8_t;
+            uint8_t;
+            uint8_t;
+            uint8_t;
+            uint8_t;
+            bool;
+            uint8_t;
+            struct {
+                char [64];
+                uint8_t;
+            } [64];
+            uint8_t [6];
+            uint8_t;
+        }
+    """
+
+    def serialise(self) -> bytes:
+        child_config_format = "c" * 65
+        pack_str = "<BBBBBB?B" + child_config_format * 64 + "BBBBBBB"
+        child_bytes = bytearray()
+        for child in self.children:
+            c_name = child[1].encode("utf-8")
+            c_name_padded = c_name.ljust(64, b"\0")
+            assert len(c_name_padded) == 64
+            child_bytes.extend(c_name_padded)
+            child_bytes.extend(child[0].to_bytes(1, "little"))
+
+        child_bytes = child_bytes.ljust(64 * 65, b"\0")
+
+        child_bytes_list = [x.to_bytes(1, "little") for x in child_bytes]
+
+        num_pmu_events = len(self.pmu_events)
+        assert num_pmu_events <= 6
+        self.pmu_events.extend(0 for i in range(6 - num_pmu_events))
+
+        return struct.pack(
+            pack_str,
+            self.ch_rx_start,
+            self.ch_tx_start,
+            self.ch_rx_stop,
+            self.ch_tx_stop,
+            self.ch_init,
+            self.core,
+            self.last_core,
+            len(self.children),
+            *child_bytes_list,
+            *self.pmu_events,
+            num_pmu_events,
+        )
+
+BENCH_VM_MAGIC = b"BVMC\x01"
+
+class BenchmarkVmConfig:
+    """
+        Matches struct definition in benchmark.c:
+        {
+            uint64_t results_vaddr;
+            char magic[5];
+            uint8_t ch_start;
+            uint8_t ch_stop;
+            uint8_t ch_done;
+            uint8_t vcpu_id;
+            char vm_name[64];
+            /* 7 bytes tail padding to 8-byte alignment */
+        }
+    """
+
+    def __init__(self, results_vaddr: int, ch_start: int, ch_stop: int,
+                 ch_done: int, vcpu_id: int, vm_name: str):
+        self.results_vaddr = results_vaddr
+        self.ch_start = ch_start
+        self.ch_stop = ch_stop
+        self.ch_done = ch_done
+        self.vcpu_id = vcpu_id
+        self.vm_name = vm_name
+
+    def serialise(self) -> bytes:
+        name = self.vm_name.encode("utf-8")
+        assert len(name) < 64, "vm_name too long for 64-byte field"
+        # Pack the struct using https://docs.python.org/3/library/struct.html
+        data = struct.pack(
+            "<Q5sBBBB64s7x",
+            self.results_vaddr,
+            BENCH_VM_MAGIC,
+            self.ch_start,
+            self.ch_stop,
+            self.ch_done,
+            self.vcpu_id,
+            name,
+        )
+        assert len(data) == 88
+        return data
+
+
+class BenchmarkVmmConfig:
+    """
+        Matches struct definition in eth_driver_vmm.c
+        {
+            uint64_t results_vaddr;
+            char magic[5];
+            uint8_t ch_start;
+            uint8_t ch_stop;
+            uint8_t ch_done;
+            uint8_t vcpu_id;
+            /* 7 bytes tail padding to 8-byte alignment */
+        }
+    """
+
+    def __init__(self, results_vaddr: int, ch_start: int, ch_stop: int,
+                 ch_done: int, vcpu_id: int):
+        self.results_vaddr = results_vaddr
+        self.ch_start = ch_start
+        self.ch_stop = ch_stop
+        self.ch_done = ch_done
+        self.vcpu_id = vcpu_id
+
+    def serialise(self) -> bytes:
+        data = struct.pack(
+            "<Q5sBBBB7x",
+            self.results_vaddr,
+            BENCH_VM_MAGIC,
+            self.ch_start,
+            self.ch_stop,
+            self.ch_done,
+            self.vcpu_id,
+        )
+        assert len(data) == 24
+        return data
+
+def generate(sdf_file: str, output_dir: str, dtb: DeviceTree, driver_vm_dtb: DeviceTree, pmu_event_ids: List[int],):
+    uart_node = dtb.node(board.serial)
+    assert uart_node is not None
+    guest_serial_node = driver_vm_dtb.node(board.guest_serial)
+    assert guest_serial_node is not None
+    ethernet_node = dtb.node(board.ethernet)
+    assert ethernet_node is not None
+    timer_node = dtb.node(board.timer)
+    assert timer_node is not None
+
+    timer_driver = ProtectionDomain("timer_driver", "timer_driver.elf", priority=110, cpu=0)
+    timer_system = Sddf.Timer(sdf, timer_node, timer_driver)
+
+    uart_driver = ProtectionDomain("serial_driver", "serial_driver.elf", priority=109,cpu=0)
+    serial_virt_tx = ProtectionDomain("serial_virt_tx", "serial_virt_tx.elf", priority=108, cpu=0)
+    serial_virt_rx = ProtectionDomain("serial_virt_rx", "serial_virt_rx.elf",
+                                      priority=107, stack_size=0x2000,cpu=0)
+    serial_system = Sddf.Serial(sdf, uart_node, uart_driver, serial_virt_tx, virt_rx=serial_virt_rx, enable_color=False)
+
+    blk_driver_vmm_pd = ProtectionDomain("blk_driver_vm", "blk_driver_vmm.elf", priority=106, cpu=0)
+    blk_driver_vm = VirtualMachine("linux", [VirtualMachine.Vcpu(id=0)], priority=105)
+    blk_driver_vmm = Vmm(sdf, blk_driver_vmm_pd, blk_driver_vm, driver_vm_dtb, one_to_one_ram=True)
+
+
+    timer_system.add_client(blk_driver_vmm_pd)
+
+    blk_driver_vmm.add_virtio_mmio_console(guest_serial_node, serial_system)
+
+    for device_path in board.passthrough:
+        node = dtb.node(device_path)
+        assert node is not None
+        blk_driver_vmm.add_passthrough_device(node)
+
+    net_virt_tx = ProtectionDomain(
+        "net_virt_tx", "network_virt_tx.elf", priority=100, budget=20000,cpu=0
+    )
+    net_virt_rx = ProtectionDomain("net_virt_rx", "network_virt_rx.elf", priority=99,cpu=0)
+    net_system = Sddf.Net(sdf, ethernet_node, eth_driver_vmm_pd, net_virt_tx, net_virt_rx, driver_vm_system=eth_driver_vmm)
+
+    client0 = ProtectionDomain("client0", "echo0.elf", priority=97, budget=20000,cpu=0)
+    client0_net_copier = ProtectionDomain(
+        "client0_net_copier", "network_copy0.elf", priority=98, budget=20000,cpu=0
+    )
+    client1 = ProtectionDomain("client1", "echo1.elf", priority=97, budget=20000,cpu=0)
+    client1_net_copier = ProtectionDomain(
+        "client1_net_copier", "network_copy1.elf", priority=98, budget=20000,cpu=0
+    )
+
+    serial_system.add_client(client0)
+    serial_system.add_client(client1)
+    timer_system.add_client(client0)
+    timer_system.add_client(client1)
+    net_system.add_client_with_copier(client0, client0_net_copier)
+    net_system.add_client_with_copier(client1, client1_net_copier)
+
+    client0_lib_sddf_lwip = Sddf.Lwip(sdf, net_system, client0)
+    client1_lib_sddf_lwip = Sddf.Lwip(sdf, net_system, client1)
+
+    # Benchmark specific resources
+    bench_idle = ProtectionDomain("bench_idle", "idle.elf", priority=1,cpu=0)
+    bench = ProtectionDomain("bench", "benchmark.elf", priority=253,cpu=0)
+
+    serial_system.add_client(bench)
+
+    benchmark_pds = [
+        uart_driver,
+        serial_virt_tx,
+        serial_virt_rx,
+        eth_driver_vmm_pd,
+        net_virt_tx,
+        net_virt_rx,
+        client0,
+        client0_net_copier,
+        client1,
+        client1_net_copier,
+        timer_driver,
+    ]
+    pds = [
+        bench_idle,
+        bench,
+    ]
+    bench_children = []
+    for pd in benchmark_pds:
+        child_id = bench.add_child_pd(pd)
+        bench_children.append((child_id, pd.name))
+    for pd in pds:
+        sdf.add_pd(pd)
+
+    # Benchmark START channel
+    bench_start_ch = Channel(client0, bench)
+    sdf.add_channel(bench_start_ch)
+    # Benchmark STOP channel
+    bench_stop_ch = Channel(client0, bench)
+    sdf.add_channel(bench_stop_ch)
+
+    bench_idle_ch = Channel(bench_idle, bench)
+    sdf.add_channel(bench_idle_ch)
+
+    cycle_counters_mr = MemoryRegion(sdf, "cycle_counters", 0x1000)
+    sdf.add_mr(cycle_counters_mr)
+
+    bench_idle.add_map(Map(cycle_counters_mr, 0x5_000_000, perms="rw"))
+    client0.add_map(Map(cycle_counters_mr, 0x20_000_000, perms="rw"))
+    bench_idle_config = BenchmarkIdleConfig(0x5_000_000, bench_idle_ch.pd_a_id)
+
+    bench_client_config = BenchmarkClientConfig(
+        bench_start_ch.pd_a_id, bench_stop_ch.pd_a_id, [0x20_000_000]
+    )
+
+    benchmark_config = BenchmarkConfig(
+        bench_start_ch.pd_b_id,  
+        0,                       
+        bench_stop_ch.pd_b_id,   
+        0,                       
+        bench_idle_ch.pd_b_id,   
+        0,                        
+        True,                     
+        bench_children,
+        pmu_event_ids,                       
+    )
+
+    # Vm benchmark resources
+    bench_vm_start_ch = Channel(bench, eth_driver_vmm_pd)
+    bench_vm_stop_ch  = Channel(bench, eth_driver_vmm_pd)
+    bench_vm_done_ch  = Channel(bench, eth_driver_vmm_pd)
+    for ch in (bench_vm_start_ch, bench_vm_stop_ch, bench_vm_done_ch):
+        sdf.add_channel(ch)
+
+    vm_results_mr = MemoryRegion(sdf, "bench_vm_results", 0x1000)
+    sdf.add_mr(vm_results_mr)
+
+    # vmm_vaddr = eth_driver_vmm_pd.get_map_vaddr(vm_results_mr)
+    # net_driver_vm's guest RAM occupies 0x20000000..0x30000000 but get map vaddr
+    # doesnt know this as eth_driver_vmm.serialise_config(output_dir) hasnt run
+    vmm_vaddr = 0x6_000_000
+    eth_driver_vmm_pd.add_map(Map(vm_results_mr, vmm_vaddr, perms="rw"))
+
+    bench_vaddr = bench.get_map_vaddr(vm_results_mr)
+    bench.add_map(Map(vm_results_mr, bench_vaddr, perms="r"))
+
+    
+    bench_vm_config = BenchmarkVmConfig(
+        bench_vaddr,
+        bench_vm_start_ch.pd_a_id,
+        bench_vm_stop_ch.pd_a_id,
+        bench_vm_done_ch.pd_a_id,
+        0,
+        "linux",
+    )
+
+    bench_vmm_config = BenchmarkVmmConfig(
+        vmm_vaddr,
+        bench_vm_start_ch.pd_b_id,
+        bench_vm_stop_ch.pd_b_id,
+        bench_vm_done_ch.pd_b_id,
+        0,
+    )
+
+    assert eth_driver_vmm.connect()
+    assert serial_system.connect()
+    assert net_system.connect()
+    assert timer_system.connect()
+    assert client0_lib_sddf_lwip.connect()
+    assert client1_lib_sddf_lwip.connect()
+    assert eth_driver_vmm.serialise_config(output_dir)
+    assert serial_system.serialise_config(output_dir)
+    assert net_system.serialise_config(output_dir)
+    assert timer_system.serialise_config(output_dir)
+    assert client0_lib_sddf_lwip.serialise_config(output_dir)
+    assert client1_lib_sddf_lwip.serialise_config(output_dir)
+
+    with open(f"{output_dir}/benchmark_vm_bench.data", "wb+") as f:
+        f.write(bench_vm_config.serialise())
+
+    with open(f"{output_dir}/benchmark_vm_vmm.data", "wb+") as f:
+        f.write(bench_vmm_config.serialise())
+
+    with open(f"{output_dir}/benchmark_config.data", "wb+") as f:
+        f.write(benchmark_config.serialise())
+
+    with open(f"{output_dir}/benchmark_idle_config.data", "wb+") as f:
+        f.write(bench_idle_config.serialise())
+
+    with open(f"{output_dir}/benchmark_client_config.data", "wb+") as f:
+        f.write(bench_client_config.serialise())
+
+    with open(f"{output_dir}/{sdf_file}", "w+") as f:
+        f.write(sdf.render())
+
+bench_pmu_events = {
+    "CACHE_L1I_MISS": (0, []),
+    "CACHE_L1D_MISS": (1, []),
+    "TLB_L1I_MISS": (2, []),
+    "TLB_L1D_MISS": (3, []),
+    "EXECUTE_INSTRUCTION": (4, []),
+    "BRANCH_MISPREDICT": (5, []),
+    "CPU_CYCLES": (6, []),
+    "MEM_ACCESS": (7, []),
+    "CHAIN": (8, []),
+}
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dtb_native", required=True)
+    parser.add_argument("--dtb_guest", required=True)
+    parser.add_argument("--sddf", required=True)
+    parser.add_argument("--board", required=True, choices=[b.name for b in BOARDS])
+    parser.add_argument("--output", required=True)
+    parser.add_argument("--sdf", required=True)
+    parser.add_argument("--bench_pmu_events", required=False)
+
+    args = parser.parse_args()
+
+    board = next(filter(lambda b: b.name == args.board, BOARDS))
+
+    sdf = SystemDescription(board.arch, board.paddr_top)
+    sddf = Sddf(args.sddf)
+
+    with open(args.dtb_native, "rb") as f:
+        dtb_native = DeviceTree(f.read())
+
+    with open(args.dtb_guest, "rb") as f:
+        dtb_guest = DeviceTree(f.read())
+
+    if args.bench_pmu_events:
+        pmu_events = args.bench_pmu_events.split(",")
+    else:
+        # If benchmarking PMU events are not provided, we use these default events
+        pmu_events = [
+            "EXECUTE_INSTRUCTION",
+            "CHAIN",
+            "MEM_ACCESS",
+            "CHAIN",
+            "CACHE_L1D_MISS",
+            "CHAIN",
+        ]
+
+    assert (
+        len(pmu_events) <= 6
+    ), "Supplied more than 6 benchmarking PMU events to track!"
+    pmu_event_ids = []
+    for i in range(len(pmu_events)):
+        if not i % 2:
+            assert (
+                pmu_events[i] != "CHAIN"
+            ), f"Chaining (overflow counting) can only be used by odd counters (selected counter {i})!"
+
+        assert (
+            pmu_events[i] in bench_pmu_events
+        ), f"Selected PMU event {i} ({pmu_events[i]}) is not supported!"
+
+        assert (
+            args.board not in bench_pmu_events[pmu_events[i]][1]
+        ), f"Selected PMU event {i} ({pmu_events[i]}) is not supported by board {args.board}!"
+
+        pmu_event_ids.append(bench_pmu_events[pmu_events[i]][0])
+
+    generate(args.sdf, args.output, dtb_native, dtb_guest, pmu_event_ids)
