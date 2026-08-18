315f866fa8dc2035d2881d5e3507309bd05269f6
Bill Nguyen <bill.nguyen@unsw.edu.au>
2026-08-18 21:50:45 +1000

    examples/windows: init squashed
    cp -r examples/virtio_pci examples/windows
    
    Except for ARM stuff, Linux and Buildroot config files.
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: renamed and purged stuff from virtio_pci
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: +QEMU RAM, fix virtio regs moving, +bochs-display
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: rm serial subsystem + boot UEFI
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: add bochs-display passthrough
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: bump guest RAM to 11G
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: passthrough kb + mouse
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: handle all IRQ passthrough
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: fix BSOD inaccessible boot device
    
    Turns out Windows is very strict on ACPI DSDT parsing. The PCI devices
    need an interrupt link device. The Link devices must have all the
    methods even if they are dummy, else the Windows' resource arbiter will
    not accept the interrupt and fail the viostor driver binding process.
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: purge linux
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: fix PS/2 KB+M not working on Windows
    
    We need to tell Windows that our virtual PCI bridge decodes I/O Ports
    range that these legacy devices live in.
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    HACK: enable Intel APICv operation
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    checkpoint windows 10+11 working on QEMU, just need APICv kernel patch
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: metaprogram clean ups
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: add debugging guide
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: lower guest RAM to 8GB, lower reso to 1080p
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: add install instructions
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: use OVMF with LionsOS logo
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: add UEFI repro instruction
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>
    
    examples/windows: add steps for custom SDK
    
    Signed-off-by: Bill Nguyen <bill.nguyen@unsw.edu.au>

---
+++ examples/windows/client_vmm.c
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2025, UNSW
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+#include <stddef.h>
+#include <stdint.h>
+#include <microkit.h>
+#include <libvmm/libvmm.h>
+#include <sddf/serial/queue.h>
+#include <sddf/serial/config.h>
+#include <sddf/blk/queue.h>
+#include <sddf/blk/config.h>
+#include <sddf/network/queue.h>
+#include <sddf/network/config.h>
+#include <sddf/util/printf.h>
+#include "guest_arch_init.h"
+
+/* Data from sdfgen */
+__attribute__((__section__(".blk_client_config"))) blk_client_config_t blk_config;
+__attribute__((__section__(".net_client_config"))) net_client_config_t net_config;
+__attribute__((__section__(".vmm_config"))) vmm_config_t vmm_config;
+
+blk_queue_handle_t blk_queue;
+
+net_queue_handle_t net_rx_queue;
+net_queue_handle_t net_tx_queue;
+
+/* Bookkeeping structures for virtio devices */
+struct virtio_blk_device virtio_blk;
+struct virtio_net_device virtio_net;
+
+void init(void)
+{
+    assert(blk_config_check_magic(&blk_config));
+    assert(vmm_config_check_magic(&vmm_config));
+    assert(net_config_check_magic(&net_config));
+
+    /* Initialise the VMM and the VCPU */
+    LOG_VMM("starting \"%s\"\n", microkit_name);
+
+    bool success = guest_arch_init();
+    if (!success) {
+        LOG_VMM_ERR("Failed to initialise guest\n");
+        return;
+    }
+
+    /* Initialise sDDF Block subsystem. */
+    blk_queue_init(&blk_queue, blk_config.virt.req_queue.vaddr, blk_config.virt.resp_queue.vaddr,
+                   blk_config.virt.num_buffers);
+    /* Busy wait until blk device is ready */
+    blk_storage_info_t *storage_info = blk_config.virt.storage_info.vaddr;
+    while (!blk_storage_is_ready(storage_info));
+
+    /* Initialise sDDF Network subsystem. */
+    net_queue_init(&net_rx_queue, net_config.rx.free_queue.vaddr, net_config.rx.active_queue.vaddr,
+                   net_config.rx.num_buffers);
+    net_queue_init(&net_tx_queue, net_config.tx.free_queue.vaddr, net_config.tx.active_queue.vaddr,
+                   net_config.tx.num_buffers);
+    net_buffers_init(&net_tx_queue, 0);
+
+    if (!virtio_arch_init()) {
+        LOG_VMM_ERR("Failed to initialise virtIO devices\n");
+        return;
+    }
+
+    /* Finally start the guest */
+    if (!guest_arch_start()) {
+        LOG_VMM_ERR("Failed to start guest\n");
+        return;
+    }
+    LOG_VMM("%s is ready\n", microkit_name);
+}
+
+void notified(microkit_channel ch)
+{
+    if (ch == blk_config.virt.id) {
+        virtio_blk_handle_resp(&virtio_blk);
+    } else if (ch == net_config.rx.id) {
+        virtio_net_handle_rx(&virtio_net);
+    } else if (ch == net_config.tx.id) {
+        /* Nothing to do */
+    } else {
+        virq_handle_passthrough(ch);
+    }
+}
+
+seL4_Bool fault(microkit_child child, microkit_msginfo msginfo, microkit_msginfo *reply_msginfo)
+{
+    bool success = fault_handle(child, msginfo);
+    if (success) {
+        /* Now that we have handled the fault successfully, we reply to it so
+         * that the guest can resume execution. */
+        *reply_msginfo = microkit_msginfo_new(0, 0);
+        return seL4_True;
+    }
+
+    return seL4_False;
+}
